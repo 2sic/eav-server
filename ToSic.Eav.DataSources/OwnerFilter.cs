b52a2e19e48e9b9f5a6007e6e63924b641f4f017
unknown <2dm@NBK-DEV-01.2sic.com>
2016-03-02 23:23:28 +0100

    Created a new OwnerFilter

---
+++ ToSic.Eav.DataSources/OwnerFilter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ToSic.Eav.DataSources
+{
+	/// <summary>
+	/// Filter entities to show Drafts or only Published Entities
+	/// </summary>
+	[PipelineDesigner]
+	public class OwnerFilter : BaseDataSource
+	{
+		#region Configuration-properties
+		private const string UserIdKey = "UserId";
+
+	    // private const string DefaultUnlikelyUserId = "TW6O0YAjPgA8DYt9GjfB"; // use a very random sequence
+
+        /// <summary>
+        /// Indicates whether to show drafts or only Published Entities
+        /// </summary>
+        public string UserId
+		{
+			get { return Configuration[UserIdKey]; }
+			set { Configuration[UserIdKey] = value; }
+		}
+		#endregion
+
+		/// <summary>
+		/// Constructs a new PublishingFilter
+		/// </summary>
+		public OwnerFilter()
+		{
+			Out.Add(Constants.DefaultStreamName, new DataStream(this, Constants.DefaultStreamName, null, GetList));
+			Configuration.Add(UserIdKey, "[Settings:UserId]"); 
+
+            CacheRelevantConfigurations = new[] { UserIdKey };
+        }
+
+        //private IDictionary<int, IEntity> GetEntities()
+        //{
+        //    return DataStream().List;
+        //}
+
+        private IEnumerable<IEntity> GetList()
+        {
+            EnsureConfigurationIsLoaded();
+
+            if (string.IsNullOrWhiteSpace(UserId))
+                return new List<IEntity>();
+
+            return In["Default"].LightList.Where(e => e.Owner == UserId);
+        }
+
+	}
+}
