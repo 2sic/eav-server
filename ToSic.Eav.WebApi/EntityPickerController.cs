ac7355fab5d649d4c3d3e4c8ea3242e4282d0209
iJungleboy <daniel.mettler@2sic.com>
2017-01-06 18:18:15 +0100

    Part of https://github.com/2sic/2sxc/issues/1083

--- ToSic.Eav.WebApi/EntityPickerController.cs
+++ ToSic.Eav.WebApi/EntityPickerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using static System.String;
 
 namespace ToSic.Eav.WebApi
 {
@@ -11,22 +12,35 @@ namespace ToSic.Eav.WebApi
         /// Returns a list of entities, optionally filtered by AttributeSetId.
         /// </summary>
         [HttpGet]
-        public IEnumerable<dynamic> GetAvailableEntities(int appId, string contentTypeName = null, int? dimensionId = null)
+        [HttpPost]
+        public IEnumerable<dynamic> GetAvailableEntities([FromUri]int appId, [FromBody] string[] items, [FromUri] string contentTypeName = null, [FromUri] int? dimensionId = null)
         {
             var dimensionIds = (dimensionId.HasValue ? dimensionId : 0).Value;
 
             IContentType contentType = null;
-            if (!String.IsNullOrEmpty(contentTypeName))
+            if (!IsNullOrEmpty(contentTypeName))
                 contentType = DataSource.GetCache(null, appId).GetContentType(contentTypeName);
 
             var dsrc = DataSource.GetInitialDataSource(null, appId);
-            var entities = (from l in dsrc["Default"].List
-                           where contentType == null || l.Value.Type == contentType
+            var temp = dsrc["Default"].LightList;
+
+            // optionally filter by type
+            if (contentType != null)
+                temp = temp.Where(l => l.Type == contentType);
+
+            // optionally filter by IDs
+            if (items != null && items.Length > 0)
+            {
+                var guids = items.Select(Guid.Parse);
+                temp = temp.Where(e => guids.Contains(e.EntityGuid));
+            }
+
+            var entities = (from l in temp
                            select new
                            {
-                               Id = l.Value.EntityId,
-                               Value = l.Value.EntityGuid,
-                               Text = l.Value.Title == null || l.Value.Title[dimensionIds] == null || string.IsNullOrWhiteSpace(l.Value.Title[dimensionIds].ToString()) ? "(no Title, " + l.Key + ")" : l.Value.Title[dimensionIds]
+                               Id = l.EntityId,
+                               Value = l.EntityGuid,
+                               Text = l.Title?[dimensionIds] == null || IsNullOrWhiteSpace(l.Title[dimensionIds].ToString()) ? "(no Title, " + l.EntityId + ")" : l.Title[dimensionIds]
                            }).OrderBy(l => l.Text.ToString()).ToList();
 
             return entities;
