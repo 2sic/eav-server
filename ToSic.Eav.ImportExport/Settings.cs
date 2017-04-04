30ac9d971cc42cec5786a3e456566bbc1a9264c0
iJungleboy <daniel.mettler@2sic.com>
2017-04-04 14:13:34 +0200

    Extracted all the export to eav

---
+++ ToSic.Eav.ImportExport/Settings.cs
@@ -0,0 +1,22 @@
+﻿namespace ToSic.Eav.ImportExport
+{
+    public class Settings
+    {
+        public static string FileVersion = "07.00.00";
+        public static string MinimumRequiredVersion = "07.00.00";
+
+
+        public static string[] ExcludeFolders =
+        {
+            ".git",
+            "node_modules",
+            "bower_components",
+            ".vs",
+            ".data"
+        };
+
+        public static readonly string TemporaryDirectory = "~/DesktopModules/ToSIC_SexyContent/_";
+        public static readonly string ToSexyDirectory = "~/DesktopModules/ToSIC_SexyContent";
+        public const string TemplateContentType = "2SexyContent-Template";
+    }
+}
