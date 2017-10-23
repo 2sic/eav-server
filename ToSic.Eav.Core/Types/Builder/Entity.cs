1b194ac064eebfa84590a2661911c2f11556cdc1
iJungleboy <daniel.mettler@2sic.com>
2017-10-23 23:40:06 +0200

    add entity-metadata type builder

---
+++ ToSic.Eav.Core/Types/Builder/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ToSic.Eav.Data;
+
+namespace ToSic.Eav.Types.Builder
+{
+    public static class Entity
+    {
+        public static AttributeDefinition EntityDefault(this AttributeDefinition attDef, string entityType, 
+            bool allowMulti = false, bool enableAdd = true, bool enableCreate = true, bool enableEdit = true, bool enableRemove = true, bool enableDelete = false)
+        {
+            attDef.AddMetadata("@entity-default", new Dictionary<string, object>
+            {
+                {"EntityType", entityType},
+                {"AllowMultiValue", allowMulti},
+                {"EnableCreate", enableCreate},
+                {"EnableEdit", enableEdit},
+                {"EnableAddExisting", enableAdd},
+                {"EnableRemove", enableRemove},
+                {"EnableDelete", enableDelete}
+            });
+            return attDef; // for chaining...
+        }
+    }
+}
