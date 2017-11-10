807f52acc0c2fe9b9d4f610a4d4d189386d3b676
iJungleboy <daniel.mettler@2sic.com>
2017-11-10 16:24:58 +0100

    fixed shuffle DS and added icon + content-type + docs

--- ToSic.Eav.DataSources/Shuffle.cs
+++ ToSic.Eav.DataSources/Shuffle.cs
@@ -11,7 +11,10 @@ namespace ToSic.Eav.DataSources
 	/// A DataSource that filters Entities by Ids
 	/// </summary>
 	[PipelineDesigner]
-    [DataSourceProperties(Type = DataSourceType.Sort, DynamicOut = false)]
+    [DataSourceProperties(Type = DataSourceType.Sort, DynamicOut = false, EnableConfig = true, 
+        ExpectsDataOfType = "38e7822b-1049-4539-bb3f-f99949b1b1d1",
+        Icon = "shuffle",
+        HelpLink = "https://github.com/2sic/2sxc/wiki/DotNet-DataSource-Shuffle")]
 	public sealed class Shuffle: BaseDataSource
 	{
         #region Configuration-properties (no config)
@@ -56,8 +59,6 @@ namespace ToSic.Eav.DataSources
 
 	        Log.Add($"will shuffle and take:{Take}");
             return ShuffleInternal(In["Default"].LightList, Take);
-
-	        //   return In["Default"].LightList.Skip(itemsToSkip).Take(PageSize).ToList();
 	    }
 
         #region Experiment based on http://stackoverflow.com/questions/375351/most-efficient-way-to-randomly-sort-shuffle-a-list-of-integers-in-c-sharp/375446#375446
@@ -66,18 +67,20 @@ namespace ToSic.Eav.DataSources
         private static IEnumerable<T> ShuffleInternal<T>(IEnumerable<T> sequence, int take)
         {
             var retArray = sequence.ToArray();
-
-            // go through array, starting at the last-index
-            for (var i = retArray.Length - 1; i > take; i--)
+            var maxIndex = retArray.Length; // not Length -1, as the random-generator will always be below this
+            var maxTake = retArray.Length;
+            if (take > 0 && maxTake > take) maxTake = take;
+            maxTake = maxTake - 1; // either length of array, or take, but -1 as zero-based
+            // go through array, shuffling the items - but only for as many times as we want to take
+            for (var i = 0; i < maxTake; i++)
             {
-                var swapIndex = Generator.Next(0, i);   // get num between 0 and index
-                if (swapIndex == i) continue;           // don't replace with itself
+                var swapIndex = Generator.Next(i, maxIndex);   // get num between index and max
                 var temp = retArray[i];                 // get item at index i...
                 retArray[i] = retArray[swapIndex];      // set index i to new item
                 retArray[swapIndex] = temp;             // place temp-item to swap-slot
             }
 
-            return retArray;
+            return retArray.Take(maxTake + 1);
         }
         #endregion
 
