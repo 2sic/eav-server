975f9efec831654fd3ceb891344edc722bfd7a2a
iJungleboy <daniel.mettler@2sic.com>
2017-11-30 22:34:07 +0100

    Created new DS for Zones, Apps, Attributes

---
+++ ToSic.Eav.DataSources/Apps.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ToSic.Eav.DataSources.Caches;
+using ToSic.Eav.DataSources.VisualQuery;
+using ToSic.Eav.Interfaces;
+
+namespace ToSic.Eav.DataSources
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// A DataSource that returns the attributes of a content-type
+	/// </summary>
+	[VisualQuery(Type = DataSourceType.Source, 
+        DynamicOut = false,
+        Difficulty = DifficultyBeta.Advanced,
+        EnableConfig = true,
+        ExpectsDataOfType = "fabc849e-b426-42ea-8e1c-c04e69facd9b",
+        HelpLink = "https://github.com/2sic/2sxc/wiki/DotNet-DataSource-Apps")]
+
+    public sealed class Apps: BaseDataSource
+	{
+        #region Configuration-properties (no config)
+	    public override string LogId => "DS.EavAps";
+
+        private const string ZoneKey = "Zone";
+        private const string ZoneIdField = "ZoneId";
+        private const int DefZone = 0;
+        private const string AppsContentTypeName = "EAV_Apps";
+
+        // 2dm: this is for a later feature...
+	    private const string AppsCtGuid = "11001010-251c-eafe-2792-000000000002";
+
+
+        private enum AppsType
+	    {
+	        Id,
+	        Name,
+            IsDefault
+	    }
+	    /// <summary>
+	    /// The attribute whoose value will be filtered
+	    /// </summary>
+	    public int ZoneNumber
+	    {
+	        get => int.Parse(Configuration[ZoneKey]);
+	        set => Configuration[ZoneKey] = value.ToString();
+	    }
+
+        #endregion
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructs a new Attributes DS
+        /// </summary>
+        public Apps()
+		{
+			Out.Add(Constants.DefaultStreamName, new DataStream(this, Constants.DefaultStreamName, GetList));
+		    Configuration.Add(ZoneKey, $"[Settings:{ZoneIdField}||{DefZone}]");
+
+            CacheRelevantConfigurations = new [] { ZoneKey };
+		}
+
+	    private IEnumerable<IEntity> GetList()
+	    {
+            EnsureConfigurationIsLoaded();
+
+            // try to load the content-type - if it fails, return empty list
+	        var cache = (BaseCache)DataSource.GetCache(ZoneId, AppId);
+	        if (!cache.ZoneApps.ContainsKey(ZoneNumber)) return new List<IEntity>();
+	        var zone = cache.ZoneApps[ZoneNumber];
+
+	        var list = zone.Apps.OrderBy(a => a.Key).Select(app =>
+	        {
+               // Assemble the entities
+	            var appEnt = new Dictionary<string, object>
+	            {
+	                {AppsType.Id.ToString(), app.Key},
+	                {AppsType.Name.ToString(), app.Value},
+	                {AppsType.IsDefault.ToString(), app.Key == zone.DefaultAppId}
+	            };
+
+                return new Data.Entity(AppId, app.Key, AppsContentTypeName, appEnt, AppsType.Name.ToString());
+            });
+
+            return list;
+        }
+
+	}
+}
---
+++ ToSic.Eav.DataSources/Zones.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ToSic.Eav.DataSources.Caches;
+using ToSic.Eav.DataSources.VisualQuery;
+using ToSic.Eav.Interfaces;
+
+namespace ToSic.Eav.DataSources
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// A DataSource that returns the attributes of a content-type
+	/// </summary>
+	[VisualQuery(Type = DataSourceType.Source,
+	    Difficulty = DifficultyBeta.Advanced, 
+        DynamicOut = false,
+        EnableConfig = false,
+	    HelpLink = "https://github.com/2sic/2sxc/wiki/DotNet-DataSource-Zones")]
+
+    public sealed class Zones: BaseDataSource
+	{
+        #region Configuration-properties (no config)
+	    public override string LogId => "DS.EavZns";
+
+        // private const string ZonesKey = "ContentType";
+	    // private const string DefContentType = "";
+	    private const string ZoneContentTypeName = "EAV_Zones";
+
+	    // 2dm: this is for a later feature...
+        private const string ZoneCtGuid = "11001010-251c-eafe-2792-000000000001";
+
+
+        private enum ZoneType
+	    {
+	        Id,
+	        Name,
+	        IsCurrent,
+            DefaultAppId,
+	        AppCount
+	    }
+
+        
+		#endregion
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructs a new Attributes DS
+        /// </summary>
+		public Zones()
+		{
+			Out.Add(Constants.DefaultStreamName, new DataStream(this, Constants.DefaultStreamName, GetList));
+
+            CacheRelevantConfigurations = new string[0];
+		}
+
+	    private IEnumerable<IEntity> GetList()
+	    {
+            //EnsureConfigurationIsLoaded();
+
+            // Get cache, which manages a list of zones
+	        var cache = (BaseCache)DataSource.GetCache(ZoneId, AppId);
+
+	        var list = cache.ZoneApps.Values.OrderBy(z => z.ZoneId).Select(zone =>
+	        {
+	            // Assemble the entities
+	            var znData = new Dictionary<string, object>
+	            {
+                    {ZoneType.Id.ToString(), zone.ZoneId},
+                    {ZoneType.Name.ToString(), $"Zone {zone.ZoneId}" },
+                    {ZoneType.DefaultAppId.ToString(), zone.DefaultAppId },
+                    {ZoneType.IsCurrent.ToString(), zone.ZoneId == ZoneId },
+                    {ZoneType.AppCount.ToString(), zone.Apps.Count }
+	            };
+
+	            return new Data.Entity(AppId, zone.ZoneId, ZoneContentTypeName, znData, ZoneType.Name.ToString());
+	        });
+
+            return list;
+        }
+
+	}
+}
