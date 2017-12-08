3dab1ee9b6e781eb4d29888bf8029330d8434407
iJungleboy <daniel.mettler@2sic.com>
2017-12-08 08:59:20 +0100

    Clean up tests - had some bugs, but all just issues with the tests themselves

--- ToSic.Eav.DataSources.Tests/DataSourceTests.cs
+++ ToSic.Eav.DataSources.Tests/DataSourceTests.cs
@@ -10,7 +10,7 @@ namespace ToSic.Eav.DataSources.Tests
 
         public const int StandardInstalledPipeLineDS =
              //16; // without sql
-             19; // this is with some json-types...
+             21; // this is with some json-types...
         public const string SqlFullName = "ToSic.Eav.DataSources.SqlDataSource";
         public const string DeferredFullName = "ToSic.Eav.DataSources.DeferredPipelineQuery";
 
