c206229f4cdff04cbf9f882751aa7a09eafe26ab
iJungleboy <daniel.mettler@2sic.com>
2018-02-03 09:41:50 +0100

    wip - separating adam from dnn

---
+++ ToSic.Eav.Core/Identity/Mapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ToSic.Eav.Identity
+{
+    public static class Mapper
+    {
+        public static string GuidCompress(Guid newGuid)
+        {
+            var modifiedBase64 = Convert.ToBase64String(newGuid.ToByteArray())
+                .Replace('+', '-').Replace('/', '_')    // avoid invalid URL characters
+                .Substring(0, 22);                      // truncate trailing "==" characters
+            return modifiedBase64;
+        }
+
+        public static Guid GuidRestore(string shortGuid)
+        {
+            var base64 = shortGuid.Replace('-', '+').Replace('_', '/') + "==";
+            var bytes = Convert.FromBase64String(base64);
+            return new Guid(bytes);
+        }
+
+    }
+}
