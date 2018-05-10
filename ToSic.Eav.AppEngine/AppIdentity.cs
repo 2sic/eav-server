bdfe1c24d028c35cbf699f1c12a0b52764d7dc14
iJungleboy <daniel.mettler@2sic.com>
2018-05-10 14:48:41 +0200

    extend permission system to use full app-identity and not just app-id - part of https://github.com/2sic/2sxc/issues/1559

---
+++ ToSic.Eav.AppEngine/AppIdentity.cs
@@ -0,0 +1,17 @@
+﻿using ToSic.Eav.Apps.Interfaces;
+
+namespace ToSic.Eav.Apps
+{
+    public class AppIdentity: IAppIdentity
+    {
+        public int ZoneId { get; }
+
+        public int AppId { get; }
+
+        public AppIdentity(int zoneId, int appId)
+        {
+            ZoneId = zoneId;
+            AppId = appId;
+        }
+    }
+}
--- ToSic.Eav.AppEngine/Interfaces/IEnvironmentFactory.cs
+++ ToSic.Eav.AppEngine/Interfaces/IEnvironmentFactory.cs
@@ -9,12 +9,12 @@ namespace ToSic.Eav.Apps.Interfaces
         /// <summary>
         /// Initialize this object so it can then give information regarding the permissions of an entity.
         /// </summary>
-        PermissionCheckBase ItemPermissions(IEntity targetItem, Log parentLog, IInstanceInfo module = null);
+        PermissionCheckBase ItemPermissions(IZoneIdentity zone, IEntity targetItem, Log parentLog, IInstanceInfo module = null);
 
         /// <summary>
         /// Initialize this object so it can then give information regarding the permissions of a type.
         /// </summary>
-        PermissionCheckBase TypePermissions(IContentType targetType, IEntity targetItem, Log parentLog, IInstanceInfo module = null);
+        PermissionCheckBase TypePermissions(IZoneIdentity zone, IContentType targetType, IEntity targetItem, Log parentLog, IInstanceInfo module = null);
 
         /// <summary>
         /// Initialize to get permissions for an instance
