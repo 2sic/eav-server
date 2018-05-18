ccdbfb788b88a9fd67a896385f8e7e89a6b7b1f6
iJungleboy <daniel.mettler@2sic.com>
2018-05-18 18:12:36 +0200

    version infos

--- ToSic.Eav.Apps.Tests/Properties/AssemblyInfo.cs
+++ ToSic.Eav.Apps.Tests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ToSic.Eav.Apps.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
