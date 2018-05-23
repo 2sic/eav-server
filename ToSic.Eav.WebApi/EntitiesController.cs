5da1539f9d15445c4d4df25b1f4784ea565b810e
iJungleboy <daniel.mettler@2sic.com>
2018-05-23 13:38:31 +0200

    more logging and fix https://github.com/2sic/2sxc/issues/1570

--- ToSic.Eav.WebApi/EntitiesController.cs
+++ ToSic.Eav.WebApi/EntitiesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Newtonsoft.Json;
+using ToSic.Eav.Apps;
 using ToSic.Eav.Data;
 using ToSic.Eav.Data.Builder;
 using ToSic.Eav.Enums;
@@ -20,40 +21,47 @@ namespace ToSic.Eav.WebApi
     {
         public EntitiesController(int appId) : base(appId) { }
         public EntitiesController(Log parentLog) : base(parentLog) { }
-        //public EntitiesController() { }
 
         public Dictionary<Guid, int> SaveMany([FromUri] int appId, [FromBody] List<EntityWithHeaderOldFormat> items, [FromUri] bool partOfPage = false, bool draftOnly = false)
         {
-            var myLog = new Log("Eav.SavMny", Log, "start");
+            var myLog = new Log("Eav.SavMny", Log, $"SaveMany(appId:{appId}, items:{items.Count}, partOfPage:{partOfPage}, draftOnly:{draftOnly})");
+
+            //todo: remove this once we're sure we're not using the global appid for anything
             SetAppId(appId);
 
             // must move guid from header to entity, because we only transfer it on the header (so no duplicates)
             foreach (var i in items)
                 i.Entity.Guid = i.Header.Guid;
 
-            if(draftOnly)
+            if (draftOnly)
                 foreach (var i in items)
                 {
+                    Log.Add($"draft only, will set published/isbranch on {i.Header.Guid}");
                     i.Entity.IsPublished = false;
                     i.Entity.IsBranch = true;
                 }
 
-            IDeferredEntitiesList appPack = AppManager.Package;
+            var appMan = new AppManager(appId, Log);
+
+            IDeferredEntitiesList appPack = appMan.Package;
 
             var entitiesToImport = items
                 .Where(entity => entity.Header.Group == null || !entity.Header.Group.SlotIsEmpty)
-                .Select(e => CreateEntityFromTransferObject(e, appPack))
+                .Select(e => CreateEntityFromTransferObject(appMan, e, appPack))
                 .Cast<IEntity>()
                 .ToList();
 
             myLog.Add("will save " + entitiesToImport.Count + " items");
-            AppManager.Entities.Save(entitiesToImport);
+            appMan.Entities.Save(entitiesToImport);
 
             // find / update IDs of items updated to return to client
             var idList = items.Select(e =>
             {
-                var foundEntity = AppManager.Read.Entities.Get(e.Header.Guid);
-                return foundEntity?.GetDraft() ?? foundEntity;  // return the draft (that would be the latest), or the found, or null if not found
+                var foundEntity = appMan.Read.Entities.Get(e.Header.Guid);
+                var state = foundEntity == null ? "not found" : foundEntity.IsPublished ? "published": "draft";
+                var draft = foundEntity?.GetDraft();
+                Log.Add($"draft check: entity {e.Header.Guid} ({state}) - additional draft: {draft != null} - will return the draft");
+                return draft ?? foundEntity;  // return the draft (that would be the latest), or the found, or null if not found
             })
             .Where(e => e != null)
             .ToDictionary(f => f.EntityGuid, f => f.EntityId);
@@ -62,16 +70,16 @@ namespace ToSic.Eav.WebApi
         }
 
 
-        private Entity CreateEntityFromTransferObject(EntityWithHeaderOldFormat editInfo, IDeferredEntitiesList allEntitiesForRelationships)
+        private Entity CreateEntityFromTransferObject(AppManager appMan, EntityWithHeaderOldFormat editInfo, IDeferredEntitiesList allEntitiesForRelationships)
         {
+            Log.Add($"CreateEntityFromTransferObject(editInfo:{editInfo.Header.ContentTypeName}:{editInfo.Header.Guid}, allEntitiesForRelationships:{allEntitiesForRelationships?.List?.Count()})");
             var toEntity = editInfo.Entity;
-            var toMetadata = editInfo.Header.Metadata;
 
             // Attributes
             var attribs = new Dictionary<string, IAttribute>();
 
             // only transfer the fields / values which exist in the content-type definition
-            var type = AppManager.Read.ContentTypes.Get(toEntity.Type.StaticName);
+            var type = appMan.Read.ContentTypes.Get(toEntity.Type.StaticName);
             foreach (var attribute in toEntity.Attributes)
             {
                 var attDef = type[attribute.Key];
@@ -87,50 +95,58 @@ namespace ToSic.Eav.WebApi
 
                     // special situation: if it's an array of Guids, mixed with NULLs, then it's not correctly auto-de-serialized
                     if (attributeType == AttributeTypeEnum.Entity.ToString() && objValue is Newtonsoft.Json.Linq.JArray)
-                        // manually de-serialize
-                        objValue = JsonConvert.DeserializeObject<Guid?[]>(objValue.ToString());
+                        objValue = JsonConvert.DeserializeObject<Guid?[]>(objValue.ToString());// manually de-serialize
 
-
-                    var importValue = attribs.AddValue(attribute.Key, objValue, attributeType, allEntitiesForRelationships: allEntitiesForRelationships);
+                    var importValue = attribs.AddValue(attribute.Key, objValue, attributeType,
+                        allEntitiesForRelationships: allEntitiesForRelationships);
 
                     // append languages OR empty language as fallback
                     if (value.Dimensions != null)
                         foreach (var dimension in value.Dimensions)
                             importValue.Languages.Add(new Dimension { Key = dimension.Key, ReadOnly = dimension.Value });
-
                 }
             }
 
             if (toEntity.Guid == Guid.Empty)
                 throw new Exception("got empty guid - should never happen");
 
-            var importEntity = new Entity(AppId, toEntity.Id, type, attribs.ToDictionary(x => x.Key, y => (object)y.Value))
+            var importEntity = new Entity(appMan.AppId, toEntity.Id, type, attribs.ToDictionary(x => x.Key, y => (object)y.Value))
             {
-                #region Guids, Ids, Published, Content-Types
                 IsPublished = toEntity.IsPublished,
                 PlaceDraftInBranch = toEntity.IsBranch, // if it's not a branch, it should also force no branch...
-                #endregion
             };
-
             importEntity.SetGuid(toEntity.Guid);
 
+            Log.Add($"Import Entity app:{importEntity.AppId} id:{importEntity.EntityId}, "
+                    + $"guid:{importEntity.EntityGuid}, "
+                    + $"type:{importEntity.Type.Name}, published:{importEntity.IsPublished}, "
+                    + $"DraftInBranch:{importEntity.PlaceDraftInBranch}");
+
+
             #region Metadata if we have any
-            // todo: as the objects are of the same type, we can probably remove the type Format.Metadata soon...
-            if (toMetadata != null && toMetadata.HasMetadata)
+            var newMd = editInfo.Header.Metadata;
+            if (newMd != null && newMd.HasMetadata)
+            {
+                Log.Add($"will set metadata based on input type:{newMd.TargetType} guid:{newMd.KeyGuid} #:{newMd.KeyNumber} $:{newMd.KeyString}");
                 importEntity.SetMetadata(new MetadataFor
                 {
-                    TargetType = toMetadata.TargetType,
-                    KeyGuid = toMetadata.KeyGuid,
-                    KeyNumber = toMetadata.KeyNumber,
-                    KeyString = toMetadata.KeyString
-                });
+                    TargetType = newMd.TargetType,
+                    KeyGuid = newMd.KeyGuid,
+                    KeyNumber = newMd.KeyNumber,
+                    KeyString = newMd.KeyString
+                });}
             else
             {
-                // todo: must re-attach metadata-info from previous generation of item...
+                // must re-attach metadata-info from previous generation of item...
                 // this is important because the json must know about this for full serialization
-                var orig = AppManager.Read.Entities.Get(editInfo.Header.Guid);
-                if(orig != null)
-                    importEntity.SetMetadata(new MetadataFor(orig.MetadataFor));
+                var original = appMan.Read.Entities.Get(editInfo.Header.Guid); // get again, using guid!
+                if (original != null)
+                {
+                    Log.Add("found original - will use to re-attach metadata");
+                    importEntity.SetMetadata(new MetadataFor(original.MetadataFor));
+                }
+                else
+                    Log.Add("no original found, will not attach metadata");
             }
             #endregion
 
