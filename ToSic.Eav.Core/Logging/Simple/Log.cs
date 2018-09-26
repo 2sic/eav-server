7d777847b8140b869e40f4cee7b59496692d5194
iJungleboy <daniel.mettler@2sic.com>
2018-09-26 12:12:27 +0200

    refactor logging to be nicer

--- ToSic.Eav.Core/Logging/Simple/Log.cs
+++ ToSic.Eav.Core/Logging/Simple/Log.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ToSic.Eav.Logging.Simple
 {
@@ -9,8 +8,8 @@ namespace ToSic.Eav.Logging.Simple
         // unique ID of this logger, to not confuse it with other loggers
         private readonly string _id = Guid.NewGuid().ToString().Substring(0, 2);
 
-        private string _name = "unknwn";
-        private string _scope = "tdo";
+        protected string Name = "unknwn";
+        protected string Scope = "tdo";
         private const int MaxScopeLen = 3;
         private const int MaxNameLen = 6;
         public readonly DateTime Created = DateTime.Now;
@@ -18,7 +17,7 @@ namespace ToSic.Eav.Logging.Simple
         public  List<Entry> Entries { get; } = new List<Entry>();
         private Log _parent;
 
-        private string Identifier => $"{_scope}{_name}[{_id}]";
+        private string Identifier => $"{Scope}{Name}[{_id}]";
 
         public string FullIdentifier => _parent?.FullIdentifier + Identifier;
 
@@ -55,79 +54,13 @@ namespace ToSic.Eav.Logging.Simple
             try
             {
                 var dot = name.IndexOf(".", StringComparison.Ordinal);
-                _scope = dot > 0 ? name.Substring(0, Math.Min(dot, MaxScopeLen)) + "." : "";
+                Scope = dot > 0 ? name.Substring(0, Math.Min(dot, MaxScopeLen)) + "." : "";
                 var rest = dot > 0 ? name.Substring(dot + 1) : name;
-                _name = rest.Substring(0, Math.Min(rest.Length, MaxNameLen));
-                _name = _name.Substring(0, Math.Min(_name.Length, MaxNameLen));
+                Name = rest.Substring(0, Math.Min(rest.Length, MaxNameLen));
+                Name = Name.Substring(0, Math.Min(Name.Length, MaxNameLen));
             }
             catch { /* ignore */ }
         }
-
-        /// <summary>
-        /// Add a message
-        /// </summary>
-        /// <param name="message"></param>
-        public string Add(string message)
-        {
-            AddEntry(message);
-            return message;
-        }
-
-        /// <summary>
-        /// Add a message
-        /// </summary>
-        /// <param name="message"></param>
-        private Entry AddEntry(string message)
-        {
-            var e = new Entry(this, message, WrapDepth);
-            Add(e);
-            return e;
-        }
-
-
-        #region Log Helpers
-
-        /// <summary>
-        /// Try to call a function generating a message. 
-        /// This will be inside a try/catch, to prevent crashes because of looping on nulls etc.
-        /// </summary>
-        /// <param name="messageMaker"></param>
-        private static string Try(Func<string> messageMaker)
-        {
-            try
-            {
-                return messageMaker.Invoke();
-            }
-            catch (Exception ex)
-            {
-                return "LOG: failed to generate from code, error: " + ex.Message;
-            }
-        }
-        #endregion
-
-
-        public string Warn(string message)
-        {
-            Add(new Entry(this, "WARNING: " + message, WrapDepth));
-            return message;
-        }
-
-
-        /// <summary>
-        /// add an existing entry of another logger
-        /// </summary>
-        /// <param name="entry"></param>
-        private void Add(Entry entry)
-        {
-            Entries.Add(entry);
-            _parent?.Add(entry);
-        }
-
-        /// <summary>
-        /// Add a message by calling a function. This will be inside a try/catch, to prevent crashes because of looping on nulls etc.
-        /// </summary>
-        /// <param name="messageMaker"></param>
-        public void Add(Func<string> messageMaker) => Add(Try(messageMaker));
 
 
         /// <summary>
---
+++ ToSic.Eav.Core/Logging/Simple/Log_Adding.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ToSic.Eav.Logging.Simple
+{
+    public partial class Log
+    {
+
+        /// <summary>
+        /// Add a message
+        /// </summary>
+        /// <param name="message"></param>
+        public string Add(string message)
+        {
+            AddEntry(message);
+            return message;
+        }
+
+        /// <summary>
+        /// Add a message
+        /// </summary>
+        /// <param name="message"></param>
+        private Entry AddEntry(string message)
+        {
+            var e = new Entry(this, message, WrapDepth);
+            AddToEntriesAndParent(e);
+            return e;
+        }
+
+
+        /// <summary>
+        /// Try to call a function generating a message. 
+        /// This will be inside a try/catch, to prevent crashes because of looping on nulls etc.
+        /// </summary>
+        /// <param name="messageMaker"></param>
+        private static string Try(Func<string> messageMaker)
+        {
+            try
+            {
+                return messageMaker.Invoke();
+            }
+            catch (Exception ex)
+            {
+                return "LOG: failed to generate from code, error: " + ex.Message;
+            }
+        }
+
+
+        public string Warn(string message) => Add("WARNING: " + message);
+
+
+        /// <summary>
+        /// add an existing entry of another logger
+        /// </summary>
+        /// <param name="entry"></param>
+        private void AddToEntriesAndParent(Entry entry)
+        {
+            Entries.Add(entry);
+            _parent?.AddToEntriesAndParent(entry);
+        }
+
+        /// <summary>
+        /// Add a message by calling a function. This will be inside a try/catch, to prevent crashes because of looping on nulls etc.
+        /// </summary>
+        /// <param name="messageMaker"></param>
+        public void Add(Func<string> messageMaker) => Add(Try(messageMaker));
+
+    }
+}
