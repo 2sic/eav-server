eb6ca5316ab9d80c9e564df216ddfe524f5dd33f
iJungleboy <daniel.mettler@2sic.com>
2018-09-29 09:29:02 +0200

    minor

--- ToSic.Eav.AppEngine/SystemRuntime.cs
+++ ToSic.Eav.AppEngine/SystemRuntime.cs
@@ -1,9 +1,18 @@
-﻿using ToSic.Eav.Interfaces;
+﻿using ToSic.Eav.DataSources.Caches;
+using ToSic.Eav.Interfaces;
 
 namespace ToSic.Eav.Apps
 {
     public class SystemRuntime
     {
+        /// <summary>
+        /// Quickly lookup the zone id of an app
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <returns></returns>
+        public static int ZoneIdOfApp(int appId)
+            => ((BaseCache)DataSource.GetCache(null)).GetZoneAppId(appId: appId).Item1;
+
         /// <summary>
         /// Retrieve the Assignment-Type-ID which is used to determine which type of object
         /// an entity is assigned to (because just the object ID would be ambiguous)
