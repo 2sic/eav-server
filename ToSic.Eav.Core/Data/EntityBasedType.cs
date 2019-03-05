e52a6c32d144cb6da066d29cff620a82b0545554
iJungleboy <daniel.mettler@2sic.com>
2019-03-05 08:13:02 +0100

    minor refactoring permissions-object

---
+++ ToSic.Eav.Core/Data/EntityBasedType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ToSic.Eav.Interfaces;
+
+namespace ToSic.Eav.Data
+{
+    /// <summary>
+    /// Temporary, name / etc. still WIP!
+    /// </summary>
+    public abstract class EntityBasedType
+    {
+        public readonly IEntity Entity;
+
+        protected EntityBasedType(IEntity entity)
+        {
+            Entity = entity;
+        }
+
+        public virtual string EntityTitle
+        {
+            get => _name ?? (_name = Entity?.GetBestTitle());
+            set => _name = value;
+        }
+        private string _name;
+
+        public int EntityId => Entity?.EntityId ?? 0;
+        public Guid EntityGuid => Entity?.EntityGuid ?? Guid.Empty;
+    }
+}
