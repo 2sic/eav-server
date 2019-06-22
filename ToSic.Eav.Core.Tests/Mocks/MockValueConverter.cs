fe15fb3d1e1342cfb128bffd5c717eb9c137e1ce
ijungleboy <daniel.mettler@2sic.com>
2019-06-22 14:56:43 +0200

    minor

--- ToSic.Eav.Core.Tests/Mocks/MockValueConverter.cs
+++ ToSic.Eav.Core.Tests/Mocks/MockValueConverter.cs
@@ -8,19 +8,13 @@ namespace ToSic.Eav.Core.Tests.Mocks
     /// </summary>
     public class MockValueConverter:IEavValueConverter
     {
-        //public string Convert(ConversionScenario scenario, string type, string originalValue, int appId)
-        //{
-        //    return originalValue.ToLowerInvariant().StartsWith("page:") || originalValue.ToLowerInvariant().StartsWith("file:")
-        //        ? "http://mock.converted/" + originalValue
-        //        : originalValue;
-        //}
 
         public string ToReference(string value)
         {
             throw new NotImplementedException();
         }
 
-        public string ToValue(int appId, Guid itemGuid, string reference)
+        public string ToValue(Guid itemGuid, string reference)
         {
             return reference.ToLowerInvariant().StartsWith("page:") || reference.ToLowerInvariant().StartsWith("file:")
                 ? "http://mock.converted/" + reference
