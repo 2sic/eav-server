c880efc8d561be969f3168aa4023985a4c4db37b
ijungleboy <daniel.mettler@2sic.com>
2019-10-25 14:45:22 +0200

    Added docs project and attributes to mark public/private APIs

---
+++ ToSic.Eav.Core/PublicApi/PrivateApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ToSic.Eav.PublicApi
+{
+    /// <summary>
+    /// This attribute serves as metadata for other things to mark them as private APIs
+    /// So they should not be publicly documented
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    public class PrivateApi: Attribute
+    {
+    }
+}
---
+++ ToSic.Eav.Core/PublicApi/PublicApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToSic.Eav.PublicApi
+{
+    /// <summary>
+    /// This attribute serves as metadata for other things to mark them as public APIs
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    public class PublicApi: Attribute
+    {
+    }
+}
