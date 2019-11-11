f614674cea055fc28142afc93995c208ff8a6b80
ijungleboy <daniel.mettler@2sic.com>
2019-11-11 18:06:22 +0100

    minor

--- ToSic.Eav.Core/Data/EntityBasedWithLog.cs
+++ ToSic.Eav.Core/Data/EntityBasedWithLog.cs
@@ -4,14 +4,24 @@ using ToSic.Eav.Logging.Simple;
 
 namespace ToSic.Eav.Data
 {
-    [PrivateApi]
+    /// <summary>
+    /// A strongly typed object which gets its data from an entity, and also logs what it does.
+    /// </summary>
+    [PublicApi]
     public abstract class EntityBasedWithLog: EntityBasedType, IHasLog
     {
+        /// <summary>
+        /// An entity based type which also logs what it does
+        /// </summary>
+        /// <param name="entity">entity which will be the foundation of this type</param>
+        /// <param name="parentLog">parent log to chain</param>
+        /// <param name="logName">Name for the logger</param>
         protected EntityBasedWithLog(IEntity entity, ILog parentLog, string logName) : base(entity)
         {
             Log = new Log(logName, parentLog);
         }
 
+        /// <inheritdoc/>
         public ILog Log { get; }
 
     }
