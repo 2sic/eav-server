350fdfda6d3a4d883f5ea067b1b062a958826946
ijungleboy <daniel.mettler@2sic.com>
2019-12-09 22:34:22 +0100

    create iquery for query Params

--- ToSic.Eav.DataSources/DataSources/Query.cs
+++ ToSic.Eav.DataSources/DataSources/Query.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ToSic.Eav.DataSources.Queries;
 using ToSic.Eav.Documentation;
 using ToSic.Eav.LookUp;
@@ -10,14 +11,14 @@ namespace ToSic.Eav.DataSources
 	/// Provides a data-source to a query, but won't assemble the query unless accessed. 
 	/// </summary>
 	[PublicApi]
-	public sealed class Query : DataSourceBase
+	public sealed class Query : DataSourceBase, IQuery
 	{
         #region Configuration-properties
         [PrivateApi]
 	    public override string LogId => "DS.DefQry";
 
-        [PrivateApi("todo: improve typing")]
-        public QueryDefinition QueryDefinition;
+        [PrivateApi]
+        public QueryDefinition Definition { get; }
 
 		private IDictionary<string, IDataStream> _out = new Dictionary<string, IDataStream>();
 		private bool _requiresRebuildOfOut = true;
@@ -47,23 +48,42 @@ namespace ToSic.Eav.DataSources
 		{
 		    ZoneId = zoneId;
 		    AppId = appId;
-            QueryDefinition = new QueryDefinition(queryDef, appId);
+            Definition = new QueryDefinition(queryDef, appId, Log);
 		    ConfigurationProvider = config;
             _showDrafts = showDrafts;
+            //Params = Definition.Params;
 
-		    // this one is unusual, so don't pre-attach a default data stream
-		    //Out.Add(Constants.DefaultStreamName, new DataStream(this, Constants.DefaultStreamName, GetEntities));
-		}
+            // this one is unusual, so don't pre-attach a default data stream
+            //Out.Add(Constants.DefaultStreamName, new DataStream(this, Constants.DefaultStreamName, GetEntities));
+        }
 
 		/// <summary>
 		/// Create a stream for each data-type
 		/// </summary>
 		private void CreateOutWithAllStreams()
 		{
-		    //var pipeline = new QueryBuilder(Log).GetAsDataSource(AppId, QueryDefinition.Entity, ConfigurationProvider, null, _showDrafts);
-		    var pipeline = new QueryBuilder(Log).GetAsDataSource(QueryDefinition, ConfigurationProvider, null, null, _showDrafts);
+            // clone config provider so we can add the params
+            var queryConfig = new LookUpEngine(ConfigurationProvider);
+            //var paramsSource = new LookUpInDictionary(QueryConstants.ParamsLookup, Params);
+            //queryConfig.Add(paramsSource);
+
+		    var pipeline = new QueryBuilder(Log).GetAsDataSource(Definition, queryConfig, null, null, _showDrafts);
 		    _out = pipeline.Out;
-		}
-	}
+        }
+
+
+        /// <inheritdoc />
+        [PrivateApi] 
+        //public IDictionary<string, string> Params { get; } // = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        /// <inheritdoc />
+        [PrivateApi]
+        public void Param(string key, string value)
+        {
+            if(!_requiresRebuildOfOut)
+                throw new Exception("Can't set param any more, the query has already been compiled. Always set params before accessing the data.");
+            Definition.Params[key] = value;
+        }
+    }
 
 }
---
+++ ToSic.Eav.DataSources/Queries/IQuery.cs
@@ -0,0 +1,20 @@
+﻿using ToSic.Eav.Documentation;
+
+namespace ToSic.Eav.DataSources.Queries
+{
+    [PrivateApi]
+    public interface IQuery: IDataSource
+    {
+        /// <summary>
+        /// The underlying definition for the current query so you can check what's inside.
+        /// </summary>
+        QueryDefinition Definition { get; }
+
+        /// <summary>
+        /// Add/Set a parameter for the query, which will be used by the [Params:Xxx] tokens.
+        /// </summary>
+        /// <param name="key">Key - the part used in [Params:key]</param>
+        /// <param name="value">The value it will resolve to. Can also be another token.</param>
+        void Param(string key, string value);
+    }
+}
