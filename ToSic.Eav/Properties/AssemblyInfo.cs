70c6c594b64fbd7fa5f7ac87d1655ceb27066f9b
ijungleboy <daniel.mettler@2sic.com>
2019-12-12 23:26:32 +0100

    version bump 10.22

--- ToSic.Eav/Properties/AssemblyInfo.cs
+++ ToSic.Eav/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-[assembly: AssemblyVersion("10.21.01.*")]
+[assembly: AssemblyVersion("10.22.00.*")]
 
 // This is needed for unit testing
 [assembly: InternalsVisibleTo("ToSic.Eav.UnitTests")]
