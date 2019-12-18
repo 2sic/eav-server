cfb30cc93ea16a551319a51f2b0ad76786dbdc64
ijungleboy <daniel.mettler@2sic.com>
2019-12-18 08:47:42 +0100

    create interface for compatibility level

---
+++ ToSic.Eav.Core/Run/ICompatibilityLevel.cs
@@ -0,0 +1,16 @@
+﻿using ToSic.Eav.Documentation;
+
+namespace ToSic.Eav.Run
+{
+    /// <summary>
+    /// Carries information about what compatibility level to use. Important for components that have an older and newer API.
+    /// </summary>
+    [PublicApi]
+    public interface ICompatibilityLevel
+    {
+        /// <summary>
+        /// The compatibility level to use. 
+        /// </summary>
+        int CompatibilityLevel { get; }
+    }
+}
