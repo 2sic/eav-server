547ee23138fb22130bff16970026dd81848d9abb
ijungleboy <daniel.mettler@2sic.com>
2020-04-27 16:13:39 +0200

    version bump 10.29.01

--- ToSic.Eav.ImportExport/Properties/AssemblyInfo.cs
+++ ToSic.Eav.ImportExport/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("10.29.00.*")]
+[assembly: AssemblyVersion("10.29.01.*")]
 [assembly: InternalsVisibleTo("ToSic.Eav.ImportExport.Tests")]
