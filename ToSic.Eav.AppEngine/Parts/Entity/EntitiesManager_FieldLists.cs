059371f5d1cdafeaf477b0c68715d8c466062cff
ijungleboy <daniel.mettler@2sic.com>
2020-05-07 14:28:33 +0200

    moved field-list editing to EntitiesManager part of https://github.com/2sic/2sxc/issues/2009

---
+++ ToSic.Eav.AppEngine/Parts/Entity/EntitiesManager_FieldLists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToSic.Eav.Apps.Parts.Tools;
+using ToSic.Eav.Data;
+using UpdateList = System.Collections.Generic.Dictionary<string, object>;
+
+namespace ToSic.Eav.Apps.Parts
+{
+    public partial class EntitiesManager
+    {
+        public void FieldListUpdate(IEntity target, string[] fields, bool enableVersioning,
+            Func<CoupledIdLists, UpdateList> callback)
+        {
+            var lists = new CoupledIdLists(fields.ToDictionary(f => f, f => FieldListIdsWithNulls(target.Children(f))), Log);
+            var values = callback.Invoke(lists);
+            AppManager.Entities.UpdateParts(target, values, enableVersioning);
+        }
+
+        public void FieldListRemove(IEntity target, string[] fields, int index, bool enableVersioning)
+            => FieldListUpdate(target, fields, enableVersioning, lists => lists.Remove(index));
+
+        private static List<int?> FieldListIdsWithNulls(IEnumerable<IEntity> list)
+            => list.Select(p => p?.EntityId).ToList();
+
+    }
+}
