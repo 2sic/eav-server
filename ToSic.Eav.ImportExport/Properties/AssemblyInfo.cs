185b2b67f6f2f1fc134593508ea07e796188a415
ijungleboy <daniel.mettler@2sic.com>
2020-07-01 20:15:50 +0200

    version bump 11.03.01

--- ToSic.Eav.ImportExport/Properties/AssemblyInfo.cs
+++ ToSic.Eav.ImportExport/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("11.03.00.*")]
+[assembly: AssemblyVersion("11.03.01.*")]
 [assembly: InternalsVisibleTo("ToSic.Eav.ImportExport.Tests")]
