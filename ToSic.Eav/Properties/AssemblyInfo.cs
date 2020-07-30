34f9525e2944e1b2f0ced0a3336d92deeb6882e6
ijungleboy <daniel.mettler@2sic.com>
2020-07-30 12:38:38 +0200

    Releasing 11.4

--- ToSic.Eav/Properties/AssemblyInfo.cs
+++ ToSic.Eav/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-[assembly: AssemblyVersion("11.03.01.*")]
+[assembly: AssemblyVersion("11.04.00.*")]
 
 // This is needed for unit testing
 [assembly: InternalsVisibleTo("ToSic.Eav.UnitTests")]
