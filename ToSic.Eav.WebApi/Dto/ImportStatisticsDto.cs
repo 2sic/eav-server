efd4a634eea06e8a45235f8970fa4e50ca37970c
ijungleboy <daniel.mettler@2sic.com>
2020-08-05 12:33:22 +0200

    Update ImportStatisticsDto.cs

--- ToSic.Eav.WebApi/Dto/ImportStatisticsDto.cs
+++ ToSic.Eav.WebApi/Dto/ImportStatisticsDto.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ToSic.Eav.WebApi.Dto
 {
-    class ImportStatisticsDto
+    public class ImportStatisticsDto
     {
+        public int AmountOfEntitiesCreated;
+        public int AmountOfEntitiesDeleted;
+        public int AmountOfEntitiesUpdated;
+        public IEnumerable<string> AttributeNamesInDocument;
+        public IEnumerable<string> AttributeNamesInContentType;
+        public IEnumerable<string> AttributeNamesNotImported;
+        public int DocumentElementsCount;
+        public int LanguagesInDocumentCount;
     }
 }
