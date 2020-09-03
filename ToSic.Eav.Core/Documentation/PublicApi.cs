e2baf38ad152b2ea3903d2639ddcb9e8411ecf86
ijungleboy <daniel.mettler@2sic.com>
2020-09-03 20:18:42 +0200

    minor improvements

--- ToSic.Eav.Core/Documentation/PublicApi.cs
+++ ToSic.Eav.Core/Documentation/PublicApi.cs
@@ -3,10 +3,24 @@
 namespace ToSic.Eav.Documentation
 {
     /// <summary>
-    /// This attribute serves as metadata for other things to mark them as public APIs
+    /// This attribute marks classes, properties etc. as public APIs. 
+    /// Any API / code with this attribute will be published in the docs.
+    /// You can apply it to anything, but usually you will only need it on classes. 
     /// </summary>
     [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
-    public class PublicApi: Attribute
+    public class PublicApi : Attribute
     {
+        /// <summary>
+        /// The `[PublicApi]` attribute can usually be used without additional comment. 
+        /// </summary>
+        // Important note - this constructor looks unnecessary, because comment could be optional in the other constructor
+        // but we need it because of a minor issue in docfx
+        public PublicApi() { }
+
+        /// <summary>
+        /// Constructor with optional comment `[PublicApi(some-comment)]`
+        /// </summary>
+        /// <param name="comment">Reason why it's public, optional</param>
+        public PublicApi(string comment) { }
     }
 }
--- ToSic.Eav.Core/Documentation/WorkInProgressApi.cs
+++ ToSic.Eav.Core/Documentation/WorkInProgressApi.cs
@@ -3,11 +3,15 @@
 namespace ToSic.Eav.Documentation
 {
     /// <summary>
-    /// This attribute serves as metadata for other things to mark them as public APIs
+    /// This attribute marks APIs to be publicly documented with a clear warning that it's work in progress.
     /// </summary>
     [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
-    public class WorkInProgressApi: Attribute
+    public class WorkInProgressApi : Attribute
     {
-        public WorkInProgressApi(string message) { }
+        /// <summary>
+        /// Constructor. WIP attributes must always have a comment why they are WIP.
+        /// </summary>
+        /// <param name="comment">Reason why it's WIP, required</param>
+        public WorkInProgressApi(string comment) { }
     }
 }
