795fabd8f9ce7ba446b55e60da2ffc0127303a86
ijungleboy <daniel.mettler@2sic.com>
2020-09-16 08:13:29 +0200

    Move Multi-Permission checks to core EAV
    https://github.com/2sic/2sxc/issues/2150

---
+++ ToSic.Eav.WebApi/Security/MultiPermissionsItems.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ToSic.Eav.Apps;
+using ToSic.Eav.Apps.Run;
+using ToSic.Eav.Logging;
+using ToSic.Eav.Security;
+using IEntity = ToSic.Eav.Data.IEntity;
+
+namespace ToSic.Eav.WebApi.Security
+{
+    internal class MultiPermissionsItems: MultiPermissionsApp
+    {
+        protected List<IEntity> Items;
+
+        #region Constructors and DI / Init
+
+        public MultiPermissionsItems Init(IInstanceContext context, IApp app, IEntity item, ILog parentLog) 
+        {
+            Init(context, app, parentLog, "Sec.MpItms");
+            Items = new List<IEntity> {item};
+            return this;
+        }
+
+        #endregion
+
+        protected override Dictionary<string, IPermissionCheck> InitializePermissionChecks()
+            => Items.ToDictionary(i => i.EntityId.ToString(), BuildItemPermissionChecker);
+
+        /// <summary>
+        /// Creates a permission checker for an type in this app
+        /// </summary>
+        /// <returns></returns>
+        protected IPermissionCheck BuildItemPermissionChecker(IEntity item)
+        {
+            Log.Call($"{item.EntityId}");
+            // now do relevant security checks
+            return BuildPermissionChecker(item.Type, item);
+        }
+    }
+}
