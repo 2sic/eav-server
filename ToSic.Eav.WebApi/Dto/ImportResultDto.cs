61ff99dfe7f93b40d2c36583c10a78b14eb22f89
ijungleboy <daniel.mettler@2sic.com>
2020-09-24 13:14:24 +0200

    field rename

--- ToSic.Eav.WebApi/Dto/ImportResultDto.cs
+++ ToSic.Eav.WebApi/Dto/ImportResultDto.cs
@@ -5,13 +5,13 @@ namespace ToSic.Eav.WebApi.Dto
 {
     public class ImportResultDto
     {
-        public bool Succeeded;
+        public bool Success;
 
         public List<Message> Messages = new List<Message>();
 
         public ImportResultDto(bool success = false, string msg = null, Message.MessageTypes type = Message.MessageTypes.Information)
         {
-            Succeeded = success;
+            Success = success;
             if(msg != null) Messages.Add(new Message(msg, type));
         }
     }
