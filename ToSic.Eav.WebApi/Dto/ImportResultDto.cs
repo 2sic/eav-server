e21487484f2b43a8ed4b84b52241ce40f1e43485
ijungleboy <daniel.mettler@2sic.com>
2020-09-24 11:43:00 +0200

    minor enhancements

--- ToSic.Eav.WebApi/Dto/ImportResultDto.cs
+++ ToSic.Eav.WebApi/Dto/ImportResultDto.cs
@@ -7,7 +7,13 @@ namespace ToSic.Eav.WebApi.Dto
     {
         public bool Succeeded;
 
-        public List<Message> Messages;
+        public List<Message> Messages = new List<Message>();
+
+        public ImportResultDto(bool success = false, string msg = null, Message.MessageTypes type = Message.MessageTypes.Information)
+        {
+            Succeeded = success;
+            if(msg != null) Messages.Add(new Message(msg, type));
+        }
     }
 
 }
