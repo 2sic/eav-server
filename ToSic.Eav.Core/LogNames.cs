8f78efc31c729cb1b75f502ddd7681d69f94d6e7
ijungleboy <daniel.mettler@2sic.com>
2020-11-30 07:24:23 +0100

    Update LogNames.cs

--- ToSic.Eav.Core/LogNames.cs
+++ ToSic.Eav.Core/LogNames.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToSic.Eav
+﻿namespace ToSic.Eav
 {
     public class LogNames
     {
+        /// <summary>
+        /// Anything using this prefix allows the program to run, but doesn't do anything useful.
+        /// Like a security check which always says not-allowed, or an exporter which doesn't export
+        /// </summary>
         public const string NotImplemented = "NOT";
+
+        /// <summary>
+        /// Anything using this prefix is a basic implementation which works, but doesn't
+        /// support anything more advanced / specific to a platform. It's usually fallback implementation.
+        /// </summary>
+        public const string Basic = "BAS";
     }
 }
