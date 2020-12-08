671f71591d83e2e3585cb8c495001c79130d341f
ijungleboy <daniel.mettler@2sic.com>
2020-12-08 21:00:22 +0100

    enable editing roundtrips of unknown metadata type
    https://github.com/2sic/2sxc/issues/2281

--- ToSic.Eav.Persistence.Efc/EfcMetadataTargetTypes.cs
+++ ToSic.Eav.Persistence.Efc/EfcMetadataTargetTypes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using ToSic.Eav.Metadata;
@@ -8,13 +9,36 @@ namespace ToSic.Eav.Persistence.Efc
 {
     public class EfcMetadataTargetTypes : ITargetTypes
     {
-        private readonly Lazy<EavDbContext> _dbLazy;
+        #region DI Constructor
 
         public EfcMetadataTargetTypes(Lazy<EavDbContext> dbLazy) => _dbLazy = dbLazy;
+        private readonly Lazy<EavDbContext> _dbLazy;
 
-        public int GetId(string typeName) => TargetTypes.First(mt => mt.Value == typeName).Key;
+        #endregion
 
-        public string GetName(int typeId) => TargetTypes[typeId];
+        private const string ReservedType = "Reserved";
+
+        public int GetId(string targetTypeName)
+        {
+            var found= TargetTypes.FirstOrDefault(mt => mt.Value == targetTypeName);
+            if (!found.Equals(default(KeyValuePair<int, string>))) return found.Key;
+
+            // not found yet, check if we got an int and should just return that
+            if (int.TryParse(targetTypeName, out var id)) return id;
+            throw new ArgumentException($"Tried to find metadata-target name but failed for '{targetTypeName}'", nameof(targetTypeName));
+        }
+
+        public string GetName(int typeId)
+        {
+            if (!TargetTypes.TryGetValue(typeId, out var result))
+                throw new ArgumentException(
+                    $"Tried to get TargetType name of '{typeId}' but couldn't find it in the list.", nameof(typeId));
+
+            // if it's a reserved key, then future lookups would fail completely (as it exists many times), so in that case 
+            // better safely just return the ID
+            // note that this isn't perfect for data export/import, but for editing round-trips it's fine
+            return result != ReservedType ? result : typeId.ToString();
+        }
 
         public ImmutableDictionary<int, string> TargetTypes => _targetTypes ?? (_targetTypes = GetTargetTypes());
         private static ImmutableDictionary<int, string> _targetTypes;
