841c348888859562b173ca3a5199e952a81e2721
ijungleboy <daniel.mettler@2sic.com>
2020-12-14 18:20:42 +0100

    Extend prefetch to carry entity DTOs
    https://github.com/2sic/2sxc/issues/2295

--- ToSic.Eav.WebApi/Dto/EditPrefetchDto.cs
+++ ToSic.Eav.WebApi/Dto/EditPrefetchDto.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ToSic.Eav.WebApi.Dto
 {
     public class EditPrefetchDto
     {
         public Dictionary<string, string> Links { get; set; }
+
+        public List<EntityForPickerDto> Entities { get; set; }
     }
 }
