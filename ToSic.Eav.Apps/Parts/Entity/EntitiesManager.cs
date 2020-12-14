a98705cea1f5bbd780b3345ed4af4296fc6df898
ijungleboy <daniel.mettler@2sic.com>
2020-12-15 00:18:59 +0100

    implement app-state transaction lock
    to block the app during changes and better handle parallel updates / save requests https://github.com/2sic/2sxc/issues/2298

--- ToSic.Eav.Apps/Parts/Entity/EntitiesManager.cs
+++ ToSic.Eav.Apps/Parts/Entity/EntitiesManager.cs
@@ -48,30 +48,42 @@ namespace ToSic.Eav.Apps.Parts
         public List<int> Save(List<IEntity> entities, SaveOptions saveOptions = null)
         {
             var wrapLog = Log.Call("", message: "save count:" + entities.Count + ", with Options:" + (saveOptions != null));
-            saveOptions = saveOptions ?? SaveOptions.Build(Parent.ZoneId);
 
-            // ensure the type-definitions are real, not just placeholders
-            foreach (var entity in entities)
-                if (entity is Entity e2
-                    && !e2.Type.IsDynamic // it's not dynamic
-                    && e2.Type.Attributes == null) // it doesn't have attributes, so it must have been in-memory
-                {
-                    var newType = Parent.Read.ContentTypes.Get(entity.Type.Name);
-                    if(newType != null) e2.UpdateType(newType); // try to update, but leave if not found
-                }
+            // Run the change in a lock/transaction
+            // This is to avoid parallel creation of new entities
+            // because sometimes the save may be executed twice before the state knows that the entity exists
+            // in which case it would add it twice
+            var appState = Parent.AppState;
 
-            // attach relationship resolver - important when saving data which doesn't yet have the guid
-            entities.ForEach(Parent.AppState.Relationships.AttachRelationshipResolver);
+            // Inner call which will be executed with the Lock of the AppState
+            List<int> InnerSaveInLock()
+            {
+                saveOptions = saveOptions ?? SaveOptions.Build(Parent.ZoneId);
+                // ensure the type-definitions are real, not just placeholders
+                foreach (var entity in entities)
+                    if (entity is Entity e2
+                        && !e2.Type.IsDynamic // it's not dynamic
+                        && e2.Type.Attributes == null) // it doesn't have attributes, so it must have been in-memory
+                    {
+                        var newType = Parent.Read.ContentTypes.Get(entity.Type.Name);
+                        if (newType != null) e2.UpdateType(newType); // try to update, but leave if not found
+                    }
+
+                // attach relationship resolver - important when saving data which doesn't yet have the guid
+                entities.ForEach(appState.Relationships.AttachRelationshipResolver);
+
+                List<int> intIds = null;
+                var dc = Parent.DataController;
+                dc.DoButSkipAppCachePurge(() => intIds = dc.Save(entities, saveOptions));
+
+                // Tell the cache to do a partial update
+                State.Cache.Update(Parent, intIds, Log);
+                return intIds;
+            }
+
 
             List<int> ids = null;
-            var dc = Parent.DataController;
-            dc.DoButSkipAppCachePurge(
-                // () => dc.Relationships.DoWhileQueueingRelationships(
-                    () => ids = dc.Save(entities, saveOptions));
-            //);
-
-            // Tell the cache to do a partial update
-            State.Cache.Update(Parent, ids, Log);
+            appState.DoInLock(Log, () => ids = InnerSaveInLock());
 
             wrapLog($"ids:{ids.Count}");
             return ids;
---
+++ ToSic.Eav.Core/Apps/State/AppState_Transaction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ToSic.Eav.Logging;
+
+namespace ToSic.Eav.Apps
+{
+    public partial class AppState
+    {
+        private readonly object _transactionLock = new object();
+
+        /// <summary>
+        /// Experiment to try to ensure the same entity cannot be created twice
+        /// </summary>
+        /// <param name="parentLog"></param>
+        /// <param name="transaction"></param>
+        public void DoInLock(ILog parentLog, Action transaction)
+        {
+            var wrapLog = parentLog.Call();
+
+            try
+            {
+                lock (_transactionLock)
+                {
+                    var inLock = parentLog.Call(message: "in lock");
+                    transaction();
+                    inLock.Invoke(null);
+                }
+            }
+            finally{
+
+            }
+
+            wrapLog.Invoke("ok");
+        }
+    }
+}
