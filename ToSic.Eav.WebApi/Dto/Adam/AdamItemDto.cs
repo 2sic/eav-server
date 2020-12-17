ecc1e1decbb09fbbffb12f394410be20850ce8ae
ijungleboy <daniel.mettler@2sic.com>
2020-12-17 10:27:27 +0100

    move AdamDto to eav
    for https://github.com/2sic/2sxc/issues/2309

---
+++ ToSic.Eav.WebApi/Dto/Adam/AdamItemDto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ToSic.Eav.WebApi.Dto
+{
+    public class AdamItemDto
+    {
+        public bool IsFolder { get; }
+        public bool AllowEdit { get; set; }
+        public int Size { get; set; }
+        public int MetadataId { get; set; }
+
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public DateTime Created { get; }
+        public DateTime Modified { get; }
+
+        /// <summary>
+        /// Small preview thumbnail
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string ThumbnailUrl { get; set; }
+
+        /// <summary>
+        /// Large preview
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string PreviewUrl { get; set; }
+
+        /// <summary>
+        /// Normal url to access the resource
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Url { get; set; }
+
+        public AdamItemDto(bool isFolder, string name, int size, DateTime created, DateTime modified)
+        {
+            IsFolder = isFolder;
+            // note that the type will be set by other code later on if it's a file
+            Type = isFolder ? "folder" : "unknown";
+            Name = name;
+            Size = size;
+            Created = created;
+            Modified = modified;
+        }
+
+    }
+}
--- ToSic.Eav.WebApi/Dto/EditPrefetchDto.cs
+++ ToSic.Eav.WebApi/Dto/EditPrefetchDto.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ToSic.Eav.WebApi.Dto
 {
     public class EditPrefetchDto
     {
+        /// <summary>
+        /// Prefetched links for hyperlink fields
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public Dictionary<string, string> Links { get; set; }
 
+        /// <summary>
+        /// Prefetched entities for entity picker
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public List<EntityForPickerDto> Entities { get; set; }
+
+        /// <summary>
+        /// Dictionary where each field contains a list of ADAM items
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, List<AdamItemDto>> Adam { get; set; }
     }
 }
