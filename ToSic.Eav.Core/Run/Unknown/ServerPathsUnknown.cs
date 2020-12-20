430d734b47552a0880a98ff534dc2fac409e3834
ijungleboy <daniel.mettler@2sic.com>
2020-12-20 17:29:22 +0100

    minor - clean-up unused FullSystemPath

--- ToSic.Eav.Core/Run/Unknown/ServerPathsUnknown.cs
+++ ToSic.Eav.Core/Run/Unknown/ServerPathsUnknown.cs
@@ -4,7 +4,7 @@
     {
         public string FullAppPath(string virtualPath) => "unknown-path, please implement IServerPaths";
 
-        public string FullSystemPath(string virtualPath) => "unknown-path, please implement IServerPaths";
+        //public string FullSystemPath(string virtualPath) => "unknown-path, please implement IServerPaths";
 
         public string FullContentPath(string virtualPath) => "unknown-path, please implement IServerPaths";
     }
