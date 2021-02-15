60b23d0ed5578d7a40ecd241771a77b0f1aa25d9
ijungleboy <daniel.mettler@2sic.com>
2021-02-15 21:00:09 +0100

    constants

--- ToSic.Eav.Core/Data/ValueConverter/ValueConverterBase.cs
+++ ToSic.Eav.Core/Data/ValueConverter/ValueConverterBase.cs
@@ -11,6 +11,11 @@ namespace ToSic.Eav.Data
         public const string PrefixFile = "file";
         public const string Separator = ":";
 
+        public const string RegExToDetectConvertable = @"^(?<type>(file|page)):(?<id>[0-9]+)(?<params>(\?|\#).*)?$";
+        public const string RegExType = "type";
+        public const string RegExId = "id";
+        public const string RegExParams = "params";
+
         public string ToReference(string value) => value;
 
         public string ToValue(string reference, Guid itemGuid) => reference;
