64d23371577dd1e8276e9d201637a8a09cdf55a0
Tonći Vatavuk <tonci@sistemi.hr>
2021-04-25 19:35:02 +0200

    update export app to return IActionResult for zip file download

--- ToSic.Eav.WebApi/PublicApi/IAppController.cs
+++ ToSic.Eav.WebApi/PublicApi/IAppController.cs
@@ -1,16 +1,19 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+#if NETSTANDARD
+using Microsoft.AspNetCore.Mvc;
+#endif
 using ToSic.Eav.WebApi.Dto;
 
 namespace ToSic.Eav.WebApi.PublicApi
 {
     public interface IAppController
     {
-#if NET451
-        HttpResponseMessage
+#if NETSTANDARD
+        IActionResult Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
 #else
-        Microsoft.AspNetCore.Mvc.IActionResult
+        HttpResponseMessage Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
 #endif
-            Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
         bool SaveData(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
         AppExportInfoDto Statistics(int appId, int zoneId);
         ImportResultDto Import(int zoneId);
