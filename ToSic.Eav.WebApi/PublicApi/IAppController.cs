cb88e7843c971b28717c7b091355467056c9465e
ijungleboy <daniel.mettler@2sic.com>
2021-04-26 21:03:06 +0200

    add reset to AppController

--- ToSic.Eav.WebApi/PublicApi/IAppController.cs
+++ ToSic.Eav.WebApi/PublicApi/IAppController.cs
@@ -1,18 +1,26 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
 using ToSic.Eav.WebApi.Dto;
+#if NET451
+using ExportResponse = System.Net.Http.HttpResponseMessage;
+#else
+using ExportResponse = Microsoft.AspNetCore.Mvc.IActionResult;
+#endif
 
 namespace ToSic.Eav.WebApi.PublicApi
 {
     public interface IAppController
     {
-#if NET451
-        HttpResponseMessage
-#else
-        Microsoft.AspNetCore.Mvc.IActionResult
-#endif
-            Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
+        List<AppDto> List(int zoneId);
+
+        ExportResponse Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
         bool SaveData(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
         AppExportInfoDto Statistics(int appId, int zoneId);
         ImportResultDto Import(int zoneId);
+
+        /// <summary>
+        /// Reset an App to the last xml state
+        /// </summary>
+        /// <returns></returns>
+        ImportResultDto Reset(int zoneId, int appId);
     }
 }
