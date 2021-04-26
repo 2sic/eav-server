72bec62235ef4ea1ce70dc7b95f16e3936ab5f72
ijungleboy <daniel.mettler@2sic.com>
2021-04-26 11:38:54 +0200

    minor

--- ToSic.Eav.WebApi/PublicApi/IAppController.cs
+++ ToSic.Eav.WebApi/PublicApi/IAppController.cs
@@ -5,7 +5,12 @@ namespace ToSic.Eav.WebApi.PublicApi
 {
     public interface IAppController
     {
-        HttpResponseMessage Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
+#if NET451
+        HttpResponseMessage
+#else
+        Microsoft.AspNetCore.Mvc.IActionResult
+#endif
+            Export(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
         bool SaveData(int appId, int zoneId, bool includeContentGroups, bool resetAppGuid);
         AppExportInfoDto Statistics(int appId, int zoneId);
         ImportResultDto Import(int zoneId);
