7785dcc5de39fb62563ab2cb9d8cc0ef99ca6c42
ijungleboy <daniel.mettler@2sic.com>
2021-06-04 11:00:55 +0200

    docs

--- ToSic.Eav.Core/Factory.cs
+++ ToSic.Eav.Core/Factory.cs
@@ -5,10 +5,12 @@ using ToSic.Eav.Plumbing;
 
 namespace ToSic.Eav
 {
-	/// <summary>
-	/// The Eav DI Factory, used to construct various objects through Dependency Injection.
-	/// </summary>
-	[PublicApi_Stable_ForUseInYourCode]
+    /// <summary>
+    /// The Eav DI Factory, used to construct various objects through [Dependency Injection](xref:NetCode.DependencyInjection.Index).
+    ///
+    /// If possible avoid using this, as it's a workaround for code which is outside of the normal Dependency Injection and therefor a bad pattern.
+    /// </summary>
+    [PublicApi_Stable_ForUseInYourCode]
 	public partial class Factory
 	{
         private static IServiceCollection _serviceCollection = new ServiceCollection();
