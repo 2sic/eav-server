e5577bfa17bb4f9b18b2bbf1a3d7b12ed3b6db50
ijungleboy <daniel.mettler@2sic.com>
2021-06-07 19:05:28 +0200

    Clean/add plumbing helpers

---
+++ ToSic.Eav.Core/Plumbing/ObjectExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ToSic.Eav.Plumbing
+{
+    public static class ObjectExtensions
+    {
+        // https://stackoverflow.com/questions/6553183/check-to-see-if-a-given-object-reference-or-value-type-is-equal-to-its-default
+        // //Adapted from https://stackoverflow.com/a/6553276/1889720
+        public static bool IsNullOrDefault<TObject>(this TObject argument)
+        {
+            // deal with normal scenarios
+            if (argument == null) return true;
+            if (object.Equals(argument, default(TObject))) return true;
+
+            // deal with non-null nullables
+            var methodType = typeof(TObject);
+            if (Nullable.GetUnderlyingType(methodType) != null) return false;
+
+            // deal with boxed value types
+            var argumentType = argument.GetType();
+            if (argumentType.IsValueType && argumentType != methodType)
+            {
+                var obj = Activator.CreateInstance(argument.GetType());
+                return obj.Equals(argument);
+            }
+
+            return false;
+        }
+    }
+}
