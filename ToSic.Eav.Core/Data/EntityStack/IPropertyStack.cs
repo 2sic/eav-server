0a1a2cb3e266e5534bb90e716df177faa433291a
ijungleboy <daniel.mettler@2sic.com>
2021-06-08 19:26:57 +0200

    docs

--- ToSic.Eav.Core/Data/EntityStack/IPropertyStack.cs
+++ ToSic.Eav.Core/Data/EntityStack/IPropertyStack.cs
@@ -5,7 +5,7 @@ namespace ToSic.Eav.Data
     /// <summary>
     /// Should be an entity-reader which has a stack of entities it tries to access and prioritize which ones are to be asked first.
     /// </summary>
-    [PrivateApi("still WIP")]
+    [PrivateApi("internal only - don't publish in docs, can change at any time")]
     public interface IPropertyStack: IPropertyLookup
     {
         IPropertyLookup GetSource(string name);
