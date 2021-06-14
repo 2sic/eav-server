ad1fb2440bad478767a6db68767d2b59be4f6318
ijungleboy <daniel.mettler@2sic.com>
2021-06-15 00:22:31 +0200

    wip https://github.com/2sic/2sxc/issues/2459

--- ToSic.Eav.Core/Apps/State/AppState_System.cs
+++ ToSic.Eav.Core/Apps/State/AppState_System.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Immutable;
 using System.Linq;
 using ToSic.Eav.Caching;
-using ToSic.Eav.Configuration;
 using ToSic.Eav.Data;
 using ToSic.Eav.Documentation;
+using static System.StringComparison;
+using static ToSic.Eav.Configuration.ConfigurationConstants;
 
 namespace ToSic.Eav.Apps
 {
@@ -14,13 +15,57 @@ namespace ToSic.Eav.Apps
         /// The simple list of <em>all</em> entities, used everywhere
         /// </summary>
         [PrivateApi("WIP 12.03")]
-        public IEntity SystemSettings
-            => (_systemSettings
-                ?? (_systemSettings = new SynchronizedEntityList(this,
-                    () => Index.Values.Where(e => e.Type.Is(ConfigurationConstants.ContentTypeSettings)).ToImmutableArray())))
+        public IEntity SystemSettingsApp
+        {
+            get
+            {
+                var list = SystemSettingsList.List;
+                return list.Count < 2 
+                    ? list.FirstOrDefault() 
+                    : list.FirstOrDefault(e => string.IsNullOrEmpty(e.GetBestValue<string>(ContentTypeSettingsScope, null)));
+            }
+        }
+        
+        /// <summary>
+        /// The simple list of <em>all</em> entities, used everywhere
+        /// </summary>
+        [PrivateApi("WIP 12.03")]
+        public IEntity SystemSettingsSite
+        {
+            get
+            {
+                var list = SystemSettingsList.List;
+                return list.Count < 2 
+                    ? list.FirstOrDefault() 
+                    : list.FirstOrDefault(e => ContentTypeSettingsScopeSite.Equals(e.GetBestValue<string>(ContentTypeSettingsScope, null), InvariantCultureIgnoreCase));
+            }
+        }
+
+        private SynchronizedEntityList SystemSettingsList
+            => _systemSettingsList
+               ?? (_systemSettingsList = new SynchronizedEntityList(this, () => Index.Values
+                   .Where(e => e.Type.Is(ContentTypeSettings))
+                   .ToImmutableArray()));
+        private SynchronizedEntityList _systemSettingsList;
+
+
+        [PrivateApi("WIP 12.03")]
+        public IEntity SiteSettingsCustom
+            => (_siteSettingsCustom
+                ?? (_siteSettingsCustom = new SynchronizedEntityList(this,
+                    () => Index.Values.Where(e => e.Type.Is(ContentTypeSiteSettings)).ToImmutableArray())))
                 .List
                 .FirstOrDefault();
-        private SynchronizedEntityList _systemSettings;
+        private SynchronizedEntityList _siteSettingsCustom;
 
+        [PrivateApi("WIP 12.03")]
+        public IEntity GlobalSettingsCustom
+            => (_globalSettingsCustom
+                ?? (_globalSettingsCustom = new SynchronizedEntityList(this,
+                    () => Index.Values.Where(e => e.Type.Is(ContentTypeGlobalSettings)).ToImmutableArray())))
+                .List
+                .FirstOrDefault();
+        private SynchronizedEntityList _globalSettingsCustom;
+        
     }
 }
--- ToSic.Eav.Core/Configuration/ConfigurationConstants.cs
+++ ToSic.Eav.Core/Configuration/ConfigurationConstants.cs
@@ -3,6 +3,13 @@
     public class ConfigurationConstants
     {
         public const string ContentTypeSettings = "SystemSettings";
+        public const string ContentTypeSettingsScope = "SettingsEntityScope";
+        public const string ContentTypeSettingsScopeSite = "site";
+        public const string ContentTypeSiteSettings = "SiteSettings";
+
+
+        public const string ContentTypeGlobalSettings = "GlobalSettings";
+        
         public const string ContentTypeResources = "SystemResources";
     }
 }
