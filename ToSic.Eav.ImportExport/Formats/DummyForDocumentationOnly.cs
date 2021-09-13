b2e5a96855cc1a2e373eebe5f30c01476b095585
ijungleboy <daniel.mettler@2sic.com>
2021-09-13 15:19:30 +0200

    Re-org folders in ImportExport project

---
+++ ToSic.Eav.ImportExport/Formats/DummyForDocumentationOnly.cs
@@ -0,0 +1,13 @@
+﻿using ToSic.Eav.Documentation;
+
+namespace ToSic.Eav.Formats
+{
+    /// <summary>
+    /// This is just a dummy class to ensure that the namespace will be listed in the documentation.
+    /// The documentation generator will filter out this class afterwards.
+    /// </summary>
+    [InternalApi_DoNotUse_MayChangeWithoutNotice]
+    public class DummyForDocumentationOnly
+    {
+    }
+}
