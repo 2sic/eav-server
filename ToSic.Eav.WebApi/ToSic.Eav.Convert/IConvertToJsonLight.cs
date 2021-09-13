d1dc6b78e376e586eea049b11340a77e274d633c
ijungleboy <daniel.mettler@2sic.com>
2021-09-13 15:00:27 +0200

    minor before rename

--- ToSic.Eav.WebApi/ToSic.Eav.Convert/IConvertToJsonLight.cs
+++ ToSic.Eav.WebApi/ToSic.Eav.Convert/IConvertToJsonLight.cs
@@ -6,9 +6,10 @@ using ToSic.Eav.Documentation;
 namespace ToSic.Eav.ImportExport.JsonLight
 {
     /// <summary>
-    /// Service to Convert Entities, Streams and DataSources to <see cref="JsonLight"/> for automatic serialization in WebApis. 
+    /// Service to prepare Entities, Streams and DataSources to <see cref="JsonLight"/> for automatic serialization in WebApis. 
     ///
-    /// It can convert single items like <see cref="IEntity"/> and <see cref="IEntityWrapper"/> like DynamicEntities.
+    /// It can prepare single items like <see cref="IEntity"/> and <see cref="IEntityWrapper"/> like DynamicEntities.
+    /// It can also prepare IEnumerable/List of these types, as well as **DataStream** and **DataSource** objects. 
     ///
     /// In Custom Code / Razor / WebApi you can get this service with `var converter = GetService&lt;IConvertToJsonLight&gt;()`
     /// </summary>
