0ffb241f93e27a58701f0adbb8e9beee045b1612
Tonći Vatavuk <tonci@sistemi.hr>
2021-10-11 08:11:30 +0200

    PhysicalPath property on File

--- ToSic.Eav.Apps/Assets/File.cs
+++ ToSic.Eav.Apps/Assets/File.cs
@@ -34,6 +34,7 @@ namespace ToSic.Eav.Apps.Assets
 
         public int Size { get; set; }
 
+        public string PhysicalPath { get; set; }
 
         #region not available properties which existed on the previous DNN interface
         // These commented out fields are what DNN would have and they have problems
