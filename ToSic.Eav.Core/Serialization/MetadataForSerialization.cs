3ba6b0e9f426e30523cb7ef0316c07c8df15b8fe
ijungleboy <daniel.mettler@2sic.com>
2021-11-11 09:50:55 +0100

    Fix bug where metadata information was missing in admin list https://github.com/2sic/2sxc/issues/2585

--- ToSic.Eav.Core/Serialization/MetadataForSerialization.cs
+++ ToSic.Eav.Core/Serialization/MetadataForSerialization.cs
@@ -2,22 +2,30 @@
 {
     public class MetadataForSerialization
     {
+        /// <summary>
+        /// Should be serialized
+        /// </summary>
         public bool? Serialize { get; set; }
 
+        /// <summary>
+        /// The key should be serialized
+        /// </summary>
         public bool? SerializeKey { get; set; }
+
+        /// <summary>
+        /// The type should be serialized
+        /// </summary>
         public bool? SerializeType { get; set; }
-
-
 
         public static MetadataForSerialization Stabilize(
             MetadataForSerialization original,
-            MetadataForSerialization addition = null,
+            /*MetadataForSerialization*/ ISubEntitySerialization  addition = null,
             bool serialize = false, bool key = false, bool type = false) =>
             new MetadataForSerialization
             {
                 Serialize = original?.Serialize ?? addition?.Serialize ?? serialize,
-                SerializeKey = original?.SerializeKey ?? addition?.SerializeKey ?? key,
-                SerializeType = original?.SerializeType ?? addition?.SerializeType ?? type,
+                SerializeKey = original?.SerializeKey /*?? addition?.SerializeKey*/ ?? key,
+                SerializeType = original?.SerializeType /*?? addition?.SerializeType*/ ?? type,
             };
 
     }
