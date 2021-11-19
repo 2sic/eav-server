483116909345f864221da39c9ba23aa7be9d1a08
ijungleboy <daniel.mettler@2sic.com>
2021-11-19 18:29:00 +0100

    Update Decorators.cs
    https://github.com/2sic/2sxc/issues/2597

--- ToSic.Eav.Core/Types/Decorators.cs
+++ ToSic.Eav.Core/Types/Decorators.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ToSic.Eav.Types
+﻿namespace ToSic.Eav.Types
 {
     public class Decorators
     {
@@ -8,18 +6,21 @@ namespace ToSic.Eav.Types
 
         // Is-Obsolete Decorator
         public static string IsObsoleteDecoratorId = "852fe15e-bf23-44e6-a856-0a130203496c";
-        //public static Guid IsObsoleteDecoratorGuid = new Guid(IsObsoleteDecoratorId);
 
         // Is-Default Decorator
         public static string IsDefaultDecorator = "529ba3a2-d7d4-4f40-a81b-ff819de03a9e";
 
-        // Priority Decorator
+        // Priority Decorator - not included ATM
         //public static string PriorityDecoratorId = "1291dbc0-9c52-4c79-8c95-698e5c3aa299";
-        //public static Guid PriorityDecoratorGuid = new Guid(PriorityDecoratorId);
         //public static string PriorityDefaultField = "IsDefault";
 
         // Is-Recommended Decorator
         public static string RecommendedDecoratorId = "c740085a-d548-41f3-8d06-0a48b8692345";
-        //public static Guid RecommendedDecoratorGuid = new Guid(RecommendedDecoratorId);
+
+        // Informs what Metadata is expected / used on a specific item
+        public static string UseMetadataDecoratorId = "c490b369-9cd2-4298-af74-19c1e438cdfc";
+
+        // This marks entities which should allow saving empty
+        public static string SaveEmptyDecoratorId = "8c78dabf-e0ad-4c26-b750-48138ecb8a39";
     }
 }
