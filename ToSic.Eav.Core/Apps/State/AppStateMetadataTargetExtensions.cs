dc1df06ddd3f0586e88c2ed4f35dbf0fd8db819e
ijungleboy <daniel.mettler@2sic.com>
2021-11-22 23:51:42 +0100

    Prepare for UI of recommended Decorators
    https://github.com/2sic/2sxc/issues/2600

---
+++ ToSic.Eav.Core/Apps/State/AppStateMetadataTargetExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using ToSic.Eav.Data;
+using ToSic.Eav.Documentation;
+using ToSic.Eav.Metadata;
+
+namespace ToSic.Eav.Apps
+{
+    [PrivateApi("WIP v13")]
+    public static class AppStateMetadataTargetExtensions
+    {
+        // 2021-11-22 2dm WIP - not used yet
+        // Idea was to be able to get Metadata Targets, but I'm not sure if it's useful at all
+        public static IMetadataOf FindTarget(this AppState appState, int targetType, string key)
+        {
+
+            if (!Enum.IsDefined(typeof(TargetTypes), targetType)) return null;
+            var tType = (TargetTypes)targetType;
+            
+            switch (tType)
+            {
+                case TargetTypes.None: 
+                    return null;
+                case TargetTypes.Attribute:
+                    if (!int.TryParse(key, out var keyInt)) return null;
+                    var attr = appState.FindAttribute(keyInt);
+                    return attr?.Item2?.Metadata;
+                case TargetTypes.App:
+                    return appState.Metadata;
+                case TargetTypes.Entity:
+                    if (!Guid.TryParse(key, out var guidKey)) return null;
+                    return appState.List.One(guidKey)?.Metadata;
+                case TargetTypes.ContentType:
+                    return appState.GetContentType(key)?.Metadata;
+                case TargetTypes.Zone:
+                    return null;
+                case TargetTypes.CmsItem:
+                    return null;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public static Tuple<IContentType, IContentTypeAttribute> FindAttribute(this AppState appState, string idString) 
+            => !int.TryParse(idString, out var keyInt) ? null : appState.FindAttribute(keyInt);
+
+        public static Tuple<IContentType, IContentTypeAttribute> FindAttribute(this AppState appState, int id)
+        {
+            var allLocalCts = appState.ContentTypes
+                .Where(ct => !ct.HasAncestor());
+            var attr = allLocalCts
+                .SelectMany(ct => ct.Attributes.Select(at => new Tuple<IContentType, IContentTypeAttribute>(ct, at)))
+                .FirstOrDefault(set => set.Item2.AttributeId == id);
+            return attr;
+        }
+    }
+}
--- ToSic.Eav.WebApi/MetadataBackend.cs
+++ ToSic.Eav.WebApi/MetadataBackend.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using ToSic.Eav.Apps;
+using ToSic.Eav.Data;
 using ToSic.Eav.DataFormats.EavLight;
-using ToSic.Eav.WebApi.Helpers;
+using ToSic.Eav.Helpers;
+using ToSic.Eav.Logging;
+using ToSic.Eav.Metadata;
+using ToSic.Eav.Types;
+using ToSic.Eav.WebApi.Dto;
+using static System.StringComparison;
 using IEntity = ToSic.Eav.Data.IEntity;
 
 namespace ToSic.Eav.WebApi
@@ -12,10 +18,10 @@ namespace ToSic.Eav.WebApi
 	/// Web API Controller for MetaData
 	/// Metadata-entities (content-items) are additional information about some other object
 	/// </summary>
-	public class MetadataBackend
+	public class MetadataBackend: HasLog<MetadataBackend>
     {
 
-        public MetadataBackend(IConvertToEavLight converter, IAppStates appStates)
+        public MetadataBackend(IConvertToEavLight converter, IAppStates appStates): base($"{LogNames.WebApi}.MetaDT")
         {
             _converter = converter;
             _appStates = appStates;
@@ -29,8 +35,9 @@ namespace ToSic.Eav.WebApi
         /// <summary>
         /// Get Entities with specified AssignmentObjectTypeId and Key
         /// </summary>
-        public IEnumerable<IDictionary<string, object>> Get(int appId, int targetType, string keyType, string key, string contentType = null)
+        public MetadataListDto Get(int appId, int targetType, string keyType, string key, string contentType = null)
         {
+            var wrapLog = Log.Call<MetadataListDto>();
             IEnumerable<IEntity> entityList = null;
 
             var appState = _appStates.Get(appId);
@@ -59,9 +66,179 @@ namespace ToSic.Eav.WebApi
             if (string.IsNullOrEmpty(contentType))
                 entityList = entityList.Where(e => !Eav.Security.Permission.IsPermission(e));
 
+            IEnumerable<MetadataRecommendationDto> recommendations = null;
+            try
+            {
+                recommendations = GetRecommendations(appState, targetType, keyType, key, contentType);
+            }
+            catch (Exception e)
+            {
+                Log.Add("Error getting recommendations");
+                Log.Exception(e);
+            }
+
             _converter.WithGuid = true;
-            return _converter.Convert(entityList);
+            var result = new MetadataListDto()
+            {
+                Items = _converter.Convert(entityList),
+                Recommendations = recommendations,
+            };
+            return wrapLog("ok", result);
         }
 
+        private IEnumerable<MetadataRecommendationDto> GetRecommendations(AppState appState, int targetType, string keyType, string key, string contentType = null)
+        {
+            var wrapLog = Log.Call<IEnumerable<MetadataRecommendationDto>>($"targetType: {targetType}");
+
+            if (!string.IsNullOrWhiteSpace(contentType))
+            {
+                var type = appState.GetContentType(contentType);
+                if (type == null) return wrapLog("existing, not found", null);
+
+                return wrapLog("use existing name", new[]
+                {
+                    new MetadataRecommendationDto
+                    {
+                        Id = type.StaticName,
+                        Name = type.Name,
+                        Count = -1,
+                    }
+                });
+            }
+
+            if (!Enum.IsDefined(typeof(TargetTypes), targetType))
+                return wrapLog("invalid target type", null);
+
+            // Find Content-Types carrying pre-configured MetadataFor entities
+            var initialTypes = 
+                (FindSelfDeclaringTypes(appState, targetType, key) ?? new List<IContentType>())
+                .Select(ct => new MetadataRecommendationDto
+                {
+                    Id = ct.StaticName,
+                    Name = ct.Name,
+                    Count = 1,
+                });
+
+            var attachedRecommendations = GetAttachedRecommendations(appState, targetType, key)
+                ?? new List<MetadataRecommendationDto>();
+
+            attachedRecommendations.AddRange(initialTypes);
+
+            // Todo: remove duplicates
+
+            return wrapLog("unknown case", attachedRecommendations);
+        }
+
+        private List<MetadataRecommendationDto> GetAttachedRecommendations(AppState appState, int targetType, string key)
+        {
+            var wrapLog = Log.Call<List<MetadataRecommendationDto>>($"targetType: {targetType}");
+
+            switch (targetType)
+            {
+                case 0:
+                case (int)TargetTypes.None:
+                    return wrapLog("no target", null);
+                case (int)TargetTypes.Attribute:
+                    // TODO - PROBABLY TRY TO FIND THE ATTRIBUTE
+                    return wrapLog("attributes not supported ATM", null);
+                case (int)TargetTypes.App:
+                    // TODO: this won't work - needs another way of finding assignments
+                    return wrapLog("app", GetRecommendationsOfMetadata(appState, appState.Metadata));
+                case (int)TargetTypes.Entity:
+                    if (!Guid.TryParse(key, out var guidKey)) return wrapLog("entity not guid", null);
+                    var entity = appState.List.One(guidKey);
+                    if (entity == null) return wrapLog("entity not found", null);
+                    return wrapLog("entity", GetRecommendationsOfMetadata(appState, entity.Metadata));
+                case (int)TargetTypes.ContentType:
+                    var ct = appState.GetContentType(key);
+                    if (ct == null) return wrapLog("type not found", null);
+                    return wrapLog("content type", GetRecommendationsOfMetadata(appState, ct.Metadata));
+                case (int)TargetTypes.Zone:
+                case (int)TargetTypes.CmsItem:
+                    // todo: maybe improve?
+                    return wrapLog("zone or CmsObject not supported", null);
+            };
+            return new List<MetadataRecommendationDto>();
+
+        }
+
+
+        private List<IContentType> FindSelfDeclaringTypes(AppState appState, int targetType, string key)
+        {
+            var wrapLog = Log.Call<List<IContentType>>();
+            // for path comparisons, make sure we have the slashes cleaned
+            var keyForward = (key ??"").ForwardSlash().Trim();
+
+            var recommendedTypes = appState.ContentTypes
+                .Where(ct =>
+                {
+                    var decor = ct.Metadata.OfType(Decorators.MetadataForDecoratorId).FirstOrDefault();
+                    if (decor == null) return false;
+                    if (decor.GetBestValue<int>(Decorators.MetadataForTargetTypeField, new string[0]) != targetType)
+                        return false;
+
+                    var targetName = decor.GetBestValue<string>(Decorators.MetadataForTargetNameField, new string[0]) ?? "";
+
+                    switch (targetType)
+                    {
+                        case 0:
+                        case (int)TargetTypes.None:
+                            return false;
+                        case (int)TargetTypes.Attribute:
+                            if (targetName.Equals(key, InvariantCultureIgnoreCase)) return true;
+                            var attr = appState.FindAttribute(key);
+                            return string.Equals(keyForward, attr.Item1.StaticName + "/" + attr.Item2.Name, InvariantCultureIgnoreCase)
+                                   || string.Equals(keyForward, attr.Item1.Name + "/" + attr.Item2.Name, InvariantCultureIgnoreCase);
+                        // App and ContentType don't need extra specifiers
+                        case (int)TargetTypes.App: return true;
+                        case (int)TargetTypes.Entity:
+                            if (!Guid.TryParse(key, out var guidKey)) return false;
+                            var entity = appState.List.One(guidKey);
+                            return entity?.Type?.Is(targetName) ?? false;
+                        // App and ContentType don't need extra specifiers
+                        case (int)TargetTypes.ContentType: return true;
+                        case (int)TargetTypes.Zone: return true;
+                        // todo: not handled yet
+                        case (int)TargetTypes.CmsItem: return false;
+                        default: return false;
+                    }
+                }).ToList();
+            return wrapLog($"{recommendedTypes.Count}", recommendedTypes);
+        }
+
+        private List<MetadataRecommendationDto> GetRecommendationsOfMetadata(AppState appState, IMetadataOf md)
+        {
+            var wrapLog = Log.Call<List<MetadataRecommendationDto>>();
+
+            var recommendations = md.OfType(Decorators.MetadataExpectedDecoratorId).FirstOrDefault();
+            if (recommendations == null) return wrapLog("no recommendations", null);
+            var config = recommendations.GetBestValue<string>(Decorators.MetadataExpectedTypesField, new string[0]);
+            if (string.IsNullOrWhiteSpace(config)) return wrapLog("no values in config", null);
+            var result = config
+                .Split(',')
+                .Select(s => s.Trim())
+                .Select(s => GetRecommendation(appState, s, 1))
+                .ToList();
+
+            return wrapLog("found", result);
+        }
+
+        private MetadataRecommendationDto GetRecommendation(AppState appState, string name, int count)
+        {
+            var wrapLog = Log.Call<MetadataRecommendationDto>(name);
+
+            if (string.IsNullOrWhiteSpace(name)) return wrapLog("empty name", null);
+            
+            var type = appState.GetContentType(name);
+            if (type == null) return wrapLog("name not found", null);
+
+            return wrapLog("use existing name",
+                new MetadataRecommendationDto
+                {
+                    Id = type.StaticName,
+                    Name = type.Name,
+                    Count = count,
+                });
+        }
     }
 }
