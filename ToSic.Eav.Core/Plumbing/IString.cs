81e3749e4018276f6163280301f66d3cbec447c4
ijungleboy <daniel.mettler@2sic.com>
2021-11-29 15:07:34 +0100

    Create IString.cs

---
+++ ToSic.Eav.Core/Plumbing/IString.cs
@@ -0,0 +1,12 @@
+﻿using ToSic.Eav.Documentation;
+
+namespace ToSic.Eav.Plumbing
+{
+    /// <summary>
+    /// Mark an object as a string, so the code can determine that it should be treated as a string, even if it's an object
+    /// </summary>
+    [PrivateApi]
+    public interface IString
+    {
+    }
+}
