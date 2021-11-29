79a7ca3000d738b90446230139fd369dbc600108
ijungleboy <daniel.mettler@2sic.com>
2021-11-29 21:16:59 +0100

    minor deduplicate metadata recommendations
    https://github.com/2sic/2sxc/issues/2600

--- ToSic.Eav.WebApi/Dto/MetadataRecommendationDto.cs
+++ ToSic.Eav.WebApi/Dto/MetadataRecommendationDto.cs
@@ -1,15 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Newtonsoft.Json;
 using ToSic.Eav.Data;
 using ToSic.Eav.Types;
 
 namespace ToSic.Eav.WebApi.Dto
 {
-    public class MetadataRecommendationDto
+    public class MetadataRecommendationDto: IEquatable<MetadataRecommendationDto>
     {
-        public string Id { get; set; }
+        public string Id { get; }
 
-        public string Name { get; set; }
+        public string Name { get; }
 
         public int Count { get; set; }
 
@@ -19,15 +20,45 @@ namespace ToSic.Eav.WebApi.Dto
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public bool? CreateEmpty { get; set; }
 
-        public MetadataRecommendationDto(IContentType type, int count)
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Debug { get; set; }
+
+        public MetadataRecommendationDto(IContentType type, int count, string debugMessage)
         {
             Id = type.StaticName;
             Name = type.Name;
             Count = count;
+            Debug = debugMessage;
 
             // Mark empty if possible
             if (!type.Attributes.Any() && type.Metadata.HasType(Decorators.SaveEmptyDecoratorId))
                 CreateEmpty = true;
         }
+
+        #region Equality Comparison for deduplication
+        public bool Equals(MetadataRecommendationDto other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id && Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MetadataRecommendationDto)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Id != null ? Id.GetHashCode() : 0) * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+            }
+        }
+        #endregion
     }
 }
