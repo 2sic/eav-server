d281f5f82840ee29362f618a319fa2e8065aa51e
ijungleboy <daniel.mettler@2sic.com>
2021-11-29 21:00:51 +0100

    minor / metadata
    https://github.com/2sic/2sxc/issues/2600

--- ToSic.Eav.ImportExport/Json/V1/JsonType.cs
+++ ToSic.Eav.ImportExport/Json/V1/JsonType.cs
@@ -1,4 +1,5 @@
-﻿using ToSic.Eav.Data;
+﻿using Newtonsoft.Json;
+using ToSic.Eav.Data;
 
 namespace ToSic.Eav.ImportExport.Json.V1
 {
@@ -8,14 +9,23 @@ namespace ToSic.Eav.ImportExport.Json.V1
         public string Id;
 
         /// <summary>
+        /// Additional description for the type, usually not included.
+        /// Sometimes added in admin-UI scenarios, where additional info is useful
+        /// ATM only used for Metadata
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        /// <summary>
         /// Empty constructor is important for de-serializing
         /// </summary>
         public JsonType() { }
 
-        public JsonType(IEntity entity)
+        public JsonType(IEntity entity, bool withDescription = false)
         {
             Name = entity.Type.Name;
             Id = entity.Type.StaticName;
+            if (withDescription) Description = entity.Type.Description;
         }
     }
 }
