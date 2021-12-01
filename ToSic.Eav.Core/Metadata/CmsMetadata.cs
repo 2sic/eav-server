41c545e339bd9d5442f0e5cf22422f96d408ec03
ijungleboy <daniel.mettler@2sic.com>
2021-12-01 09:34:12 +0100

    Update CmsMetadata.cs

--- ToSic.Eav.Core/Metadata/CmsMetadata.cs
+++ ToSic.Eav.Core/Metadata/CmsMetadata.cs
@@ -3,6 +3,7 @@
     public class CmsMetadata
     {
         public static string SitePrefix = "site:";
+        public static string UserPrefix = "user:";
         public static string PagePrefix = "page:";
         public static string ModulePrefix = "module:";
         public static string FilePrefix = "file:";
