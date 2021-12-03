cf05082fe6cb05bbb324756fe0fb998386869857
ijungleboy <daniel.mettler@2sic.com>
2021-12-03 09:25:32 +0100

    Create StringExtensions.cs

---
+++ ToSic.Eav.Core/Plumbing/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ToSic.Eav.Plumbing
+{
+    public static class StringExtensions
+    {
+        //public static bool IsEmpty(this string value) => string.IsNullOrEmpty(value);
+        //public static bool IsEmptyOrWs(this string value) => string.IsNullOrWhiteSpace(value);
+        public static bool HasValue(this string value) => !string.IsNullOrWhiteSpace(value);
+    }
+}
