bad6e258c0ecf5a40c0ddfd06d6683609649aeb0
Tonći Vatavuk <tonci@sistemi.hr>
2021-12-16 15:08:04 +0100

    remove assembly binding redirects that are not necessary

--- ToSic.Eav.ImportExport.Tests/FeatureTests/FeaturesStaticTests.cs
+++ ToSic.Eav.ImportExport.Tests/FeatureTests/FeaturesStaticTests.cs
@@ -10,19 +10,19 @@ namespace ToSic.Eav.ImportExport.Tests.FeatureTests
     public class FeaturesStaticTests: FeatureTestsBase
     {
 
-        [TestMethod]
-        public void PasteClipboardActive()
-        {
-            var x = Features.Enabled(FeatureIds.PasteImageClipboard);
-            Assert.IsTrue(x, "this should be enabled and non-expired");
-        }
+        //[TestMethod]
+        //public void PasteClipboardActive()
+        //{
+        //    var x = Features.Enabled(FeatureIds.PasteImageClipboard);
+        //    Assert.IsTrue(x, "this should be enabled and non-expired");
+        //}
 
-        [TestMethod]
-        public void InventedFeatureGuid()
-        {
-            var inventedGuid = new Guid("4f3d0021-1c8b-4286-a33b-3210ed3b2d9a");
-            var x = Features.Enabled(inventedGuid);
-            Assert.IsFalse(x, "this should be enabled and expired");
-        }
+        //[TestMethod]
+        //public void InventedFeatureGuid()
+        //{
+        //    var inventedGuid = new Guid("4f3d0021-1c8b-4286-a33b-3210ed3b2d9a");
+        //    var x = Features.Enabled(inventedGuid);
+        //    Assert.IsFalse(x, "this should be enabled and expired");
+        //}
     }
 }
