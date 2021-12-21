2878dffd2e33f8e846557db21a05c82608fc75ab
Tonći Vatavuk <tonci@sistemi.hr>
2021-12-21 13:48:48 +0100

    Revert "when retrieving all metadata items, make sure that permissions are _not_ included"
    This reverts commit 74aa4f087f0c0ad9164f185716b7599197aba4b9.

--- ToSic.Eav.WebApi/ToSic.Eav.DataFormats.EavLight/ConvertToJsonBasic_SubItems.cs
+++ ToSic.Eav.WebApi/ToSic.Eav.DataFormats.EavLight/ConvertToJsonBasic_SubItems.cs
@@ -8,14 +8,12 @@ namespace ToSic.Eav.DataFormats.EavLight
 {
     public partial class ConvertToEavLight
     {
-        internal IEnumerable<EavLightEntityReference> CreateListOfSubEntities(IEnumerable<IEntity> items,
-            ISubEntitySerialization rules) =>
-            items.Where(e => !Eav.Security.Permission.IsPermission(e)) // When retrieving all items, make sure that permissions are _not_ included
-                .Select(e => new EavLightEntityReference
-                {
-                    Id = rules.SerializeId == true ? e?.EntityId : null,
-                    Guid = rules.SerializeGuid == true ? e?.EntityGuid : null,
-                    Title = rules.SerializeTitle == true ? e?.GetBestTitle(Languages) : null
-                });
+        internal IEnumerable<EavLightEntityReference> CreateListOfSubEntities(IEnumerable<IEntity> items, ISubEntitySerialization rules) =>
+            items.Select(e => new EavLightEntityReference
+            {
+                Id = rules.SerializeId == true ? e?.EntityId : null,
+                Guid = rules.SerializeGuid == true ? e?.EntityGuid : null,
+                Title = rules.SerializeTitle == true ? e?.GetBestTitle(Languages) : null
+            });
     }
 }
