8c04b6e29519884eec433f123d6e0231f0ac6e0d
ijungleboy <daniel.mettler@2sic.com>
2022-01-10 16:32:46 +0100

    minor docs

--- ToSic.Eav.Persistence.Efc/Models/ToSicEavApps.cs
+++ ToSic.Eav.Persistence.Efc/Models/ToSicEavApps.cs
@@ -2,7 +2,7 @@
 
 namespace ToSic.Eav.Persistence.Efc.Models
 {
-    public partial class ToSicEavApps//: RepoApp
+    public partial class ToSicEavApps
     {
         public ToSicEavApps()
         {
@@ -22,6 +22,10 @@ namespace ToSic.Eav.Persistence.Efc.Models
         // 2017-10-10 2dm new with entity > app mapping
         public virtual ICollection<ToSicEavEntities> ToSicEavEntities { get; set; }
 
+        /// <summary>
+        /// Special column to store info about loading the app
+        /// </summary>
+        /// <remarks>Added v13.01</remarks>
         public string SysSettings { get; set; } = null;
     }
 }
