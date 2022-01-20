985267095e4dfeb9b52f2ec9f81d1d95c5716a98
ijungleboy <daniel.mettler@2sic.com>
2022-01-20 23:12:17 +0100

    Get permissions to also respect site-settings
    https://github.com/2sic/2sxc/issues/2642

--- ToSic.Eav.Apps/Languages/AppUserLanguageCheck.cs
+++ ToSic.Eav.Apps/Languages/AppUserLanguageCheck.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using ToSic.Eav.Apps.Security;
 using ToSic.Eav.Context;
+using ToSic.Eav.Data;
 using ToSic.Eav.Logging;
 using ToSic.Eav.Metadata;
 using ToSic.Eav.Plumbing;
@@ -14,16 +15,18 @@ namespace ToSic.Eav.Apps.Languages
 {
     public class AppUserLanguageCheck: HasLog<AppUserLanguageCheck>
     {
-        public AppUserLanguageCheck(Lazy<IZoneMapper> zoneMapperLazy, IContextOfSite ctx, IServiceProvider checkGenerator)
+        public AppUserLanguageCheck(Lazy<IZoneMapper> zoneMapperLazy, IContextOfSite ctx, IServiceProvider checkGenerator, Lazy<IAppStates> appStatesLazy)
             : base($"{LogNames.Eav}.LngChk")
         {
             _zoneMapperLazy = zoneMapperLazy;
             _ctx = ctx;
             _checkGenerator = checkGenerator;
+            _appStatesLazy = appStatesLazy;
         }
         private readonly Lazy<IZoneMapper> _zoneMapperLazy;
         private readonly IContextOfSite _ctx;
         private readonly IServiceProvider _checkGenerator;
+        private readonly Lazy<IAppStates> _appStatesLazy;
 
         public List<AppUserLanguageState> LanguagesWithPermissions(AppState appState)
         {
@@ -31,16 +34,20 @@ namespace ToSic.Eav.Apps.Languages
 
             var languages = _zoneMapperLazy.Value.CulturesWithState(_ctx.Site);
 
-            var set = languages.Select(l => new
-            {
-                Permissions = appState.GetMetadata(TargetTypes.Dimension, l.Code?.ToLowerInvariant(), Permission.TypeName),
-                Language = l,
-                Allowed = true,
-            }).ToList();
-
+            var set = GetLanguagePermissions(appState, languages);
             Log.Add($"Found {set.Count} sets");
-
             var hasPermissions = set.Any(s => s.Permissions.Any());
+
+            // Find primary app, or stop if we're already there
+            if (!hasPermissions && appState.AppGuidName != Constants.PrimaryAppGuid)
+            {
+                Log.Add("No permissions, and not primary app - will try that");
+                var primaryId = _appStatesLazy.Value.PrimaryAppId(appState.ZoneId);
+                var primaryApp = _appStatesLazy.Value.Get(primaryId);
+                set = GetLanguagePermissions(primaryApp, languages);
+                hasPermissions = set.Any(s => s.Permissions.Any());
+            }
+
             var defaultAllowed = _ctx.User.IsSuperUser || !hasPermissions;
             Log.Add($"HasPermissions: {hasPermissions}, Initial Allowed: {defaultAllowed}");
 
@@ -67,6 +74,23 @@ namespace ToSic.Eav.Apps.Languages
                 .ToList();
             return wrapLog("ok", result);
         }
-        
+
+        private static List<LanguagePermission> GetLanguagePermissions(AppState appState, List<ISiteLanguageState> languages)
+        {
+            var set = languages.Select(l => new LanguagePermission
+            {
+                Permissions = appState.GetMetadata(TargetTypes.Dimension, l.Code?.ToLowerInvariant(), Permission.TypeName),
+                Language = l,
+                Allowed = true,
+            }).ToList();
+            return set;
+        }
+
+        private class LanguagePermission
+        {
+            public IEnumerable<IEntity> Permissions;
+            public bool Allowed;
+            public ISiteLanguageState Language;
+        }
     }
 }
