d9e93e026468c33fcbbd6aa1183cea134abf4f7f
ijungleboy <daniel.mettler@2sic.com>
2022-01-24 23:39:07 +0100

    Update year to 2022

--- ToSic.Eav.DataSources/Properties/AssemblyInfo.cs
+++ ToSic.Eav.DataSources/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCopyright("Copyright © 2sic 2021")]
+[assembly: AssemblyCopyright("Copyright © 2sic 2022")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
