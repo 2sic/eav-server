3ff792c9355c59334cb09adb9f907da0778d15b0
ijungleboy <daniel.mettler@2sic.com>
2022-01-30 11:02:24 +0100

    Update AppPaths.cs

--- ToSic.Eav.Apps/Paths/AppPaths.cs
+++ ToSic.Eav.Apps/Paths/AppPaths.cs
@@ -11,8 +11,26 @@ using static System.IO.Path;
 
 namespace ToSic.Eav.Apps.Paths
 {
+    /// <summary>
+    /// Find the App Paths for a specific App in a site
+    /// </summary>
+    /// <remarks>
+    /// IMPORTANT
+    /// * There is a complex problem related to Dnn which can have a module in another site.
+    ///   This can lead to an issue where the ISite used here may be the wrong one.
+    /// * The reason is that the ISite is shared across modules
+    ///   Which is difficult because it's initialized on module_load,
+    ///   But rendering will happen later, and in between other module do the _Load which can change the site
+    /// * If that happens on the initial call, the cache will be wrong forever.
+    ///   This is why as a temporary workaround we use the SiteId as part of the cache key.
+    ///   Not sexy, but I guess better than alternatives for now.
+    /// * Future: We should find a way to scope some DI to a module, so it doesn't bleed to others
+    ///   But that is a bit difficult, because there are also some services like the IPage which should be shared acrosss modules
+    /// </remarks>
     public class AppPaths: HasLog, IAppPaths
     {
+        private const bool Debug = true;
+
         public AppPaths(Lazy<IServerPaths> serverPaths, Lazy<IGlobalConfiguration> config) : base($"{LogNames.Eav}.AppPth")
         {
             _serverPaths = serverPaths;
@@ -41,23 +59,59 @@ namespace ToSic.Eav.Apps.Paths
         private AppState _appState;
         public bool InitDone;
 
-        public string Path => _appState.GetPiggyBack(nameof(Path), 
-            () => _site.AppAssetsLinkTemplate.Replace(AppConstants.AppFolderPlaceholder, _appState.Folder).ToAbsolutePathForwardSlash());
+        /// <summary>
+        /// We are having some difficulties that the App is caching the wrong path, so temporarily we'll log
+        /// when we are actually picking up the value to put into the AppState
+        /// </summary>
+        private void LogAppPathDetails(string property, string result)
+        {
+            var wrapLog = Log.Call();
+            Log.Add($"App State: {_appState.LogState()}");
+            Log.Add($"Site: {_site.Id}; Zone: {_site.ZoneId};");
+            Log.Add($"{property}: {result}");
+            wrapLog(null);
+        }
 
-        public string PathShared => _appState.GetPiggyBack(nameof(PathShared), 
+        private string InterceptAndLog(string name, string result)
+        {
+            if (Debug) Log.Add($"Intercept AppPath {name}: {result}");
+            return result;
+        }
+
+        private string GetInternal(string name, Func<string> callIfNotFound)
+        {
+            var key = name + _site.Id;
+            var final = _appState.GetPiggyBack(key,
+                () =>
+                {
+                    var result = callIfNotFound();
+                    if (Debug) LogAppPathDetails(nameof(Path), result);
+                    return result;
+                });
+            if (Debug) InterceptAndLog(name, final);
+            return final;
+        }
+
+        public string Path => GetInternal(nameof(Path), 
+            () => _site.AppAssetsLinkTemplate.Replace(AppConstants.AppFolderPlaceholder, _appState.Folder)
+                .ToAbsolutePathForwardSlash());
+
+        public string PathShared => GetInternal(nameof(PathShared), 
             () => Combine(_config.Value.SharedAppsFolder, AppConstants.AppsRootFolder, _appState.Folder)
                 .ToAbsolutePathForwardSlash());
 
-        public string PhysicalPath => _appState.GetPiggyBack(nameof(PhysicalPath), 
+        public string PhysicalPath => GetInternal(nameof(PhysicalPath), 
             () => Combine(_site.AppsRootPhysicalFull, _appState.Folder));
 
-        public string PhysicalPathShared => _appState.GetPiggyBack(nameof(PhysicalPathShared), 
-            () => _serverPaths.Value.FullAppPath(Combine(_config.Value.SharedAppsFolder, AppConstants.AppsRootFolder, _appState.Folder)));
+        public string PhysicalPathShared => GetInternal(nameof(PhysicalPathShared), 
+            () => _serverPaths.Value.FullAppPath(Combine(_config.Value.SharedAppsFolder,
+                AppConstants.AppsRootFolder, _appState.Folder)));
 
-        public string RelativePath => _appState.GetPiggyBack(nameof(RelativePath), 
-            () => Combine(_site.AppsRootPhysical, _appState.Folder));
+        public string RelativePath => GetInternal(nameof(RelativePath), 
+            () => Combine(_site.AppsRootPhysical, _appState.Folder).Backslash());
         
-        public string RelativePathShared => _appState.GetPiggyBack(nameof(RelativePathShared), 
-            () => Combine(_config.Value.SharedAppsFolder, AppConstants.AppsRootFolder, _appState.Folder).ToAbsolutePathForwardSlash());
+        public string RelativePathShared => GetInternal(nameof(RelativePathShared), 
+            () => Combine(_config.Value.SharedAppsFolder, AppConstants.AppsRootFolder, _appState.Folder)
+                .ToAbsolutePathForwardSlash());
     }
 }
