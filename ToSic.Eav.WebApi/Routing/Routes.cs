1628b1a8163887a8bbe8517f168ae11fde60512a
ijungleboy <daniel.mettler@2sic.com>
2022-02-22 15:24:52 +0100

    Move route constants to eav

---
+++ ToSic.Eav.WebApi/Routing/Routes.cs
@@ -0,0 +1,33 @@
+﻿namespace ToSic.Eav.WebApi.Routing
+{
+    /// <summary>
+    /// These are all the root paths used in the API
+    /// Basically anything after the system api-root, like /api/2sxc/* (in DNN) or /#/api/2sxc/* (in Oqtane)
+    /// </summary>
+    public class Areas
+    {
+        public const string App = "app";
+        public const string Sys = "sys";
+        public const string Cms = "cms";
+        public const string Admin = "admin";
+    }
+
+    public class AppParts
+    {
+        public const string Auto = "auto";
+        public const string Content = "content";
+        public const string Data = "data"; // new, v13
+        public const string Query = "query";
+    }
+
+
+    public class AppRoots
+    {
+        public const string AppAuto = Areas.App + "/" + AppParts.Auto;
+        public const string AppNamed = Areas.App + "/" + TokensFramework.AppPath;
+        public const string AppContentAuto = AppAuto + "/" + AppParts.Content;
+        public const string AppContentNamed = AppNamed + "/" + AppParts.Content;
+        public const string AppDataAuto = AppAuto + "/" + AppParts.Data; // new, v13
+        public const string AppDataNamed = AppNamed + "/" + AppParts.Data; // new, v13
+    }
+}
---
+++ ToSic.Eav.WebApi/Routing/TokensFramework.cs
@@ -0,0 +1,28 @@
+﻿namespace ToSic.Eav.WebApi.Routing
+{
+    /// <summary>
+    /// These are tokens in the format {xyz} used in routes to identify a part
+    /// </summary>
+    public class TokensFramework
+    {
+        // Single Placeholders
+        public const string Name = "{" + VarNames.Name + "}";
+        public const string Stream = "{" + VarNames.Stream + "}";
+        public const string Edition = "{" + VarNames.Edition + "}";
+        public const string AppPath = "{" + VarNames.AppPath + "}";
+        public const string Controller = "{" + VarNames.Controller + "}";
+        public const string Action = "{" + VarNames.Action + "}";
+
+        public const string ContentType = "{" + VarNames.ContentType + "}";
+        public const string Id = "{" + VarNames.Id + "}";
+        public const string Guid = "{" + VarNames.Guid + "}";
+        public const string Field = "{" + VarNames.Field + "}";
+
+        // Sets / Pairs of placeholders
+        public const string SetControllerAction = Controller + "/" + Action;
+        public const string SetTypeAndId = ContentType + "/" + Id;
+        public const string SetTypeAndGuid = ContentType + "/" + Guid;
+        public const string SetTypeGuidField = SetTypeAndGuid + "/" + Field;
+        public const string SetTypeGuidFieldAction = SetTypeGuidField + "/" + Action;
+    }
+}
