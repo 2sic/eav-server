5a0192c5da59ce4c007f41996e8f0073fcce713d
ijungleboy <daniel.mettler@2sic.com>
2022-02-22 10:47:47 +0100

    Update WarnUseOfUnknown.cs

--- ToSic.Eav.Core/Run/Unknown/WarnUseOfUnknown.cs
+++ ToSic.Eav.Core/Run/Unknown/WarnUseOfUnknown.cs
@@ -19,9 +19,8 @@ namespace ToSic.Eav.Run.Unknown
                 "The system uses extensive Dependency Injection, and defaults to 'Unknown' implementations if something is missing. ");
             log.Add("You are seeing this warning, because apparently such an implementation was initialized, which shouldn't happen in a good productive environment.");
             log.Add("This warning will only appear once per used type");
-            logHistory.Add("warnings-mock", log);
+            logHistory.Add("warnings-not-implemented", log);
         }
-
 
         private static readonly List<Type> AlreadyWarnedTypes = new List<Type>();
     }
