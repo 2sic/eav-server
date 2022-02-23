ff692ba759b750916b6eb62c2f549263b7732825
ijungleboy <daniel.mettler@2sic.com>
2022-02-23 11:05:06 +0100

    Create DummyControllerReal.cs

---
+++ ToSic.Eav.WebApi/DummyControllerReal.cs
@@ -0,0 +1,14 @@
+﻿using ToSic.Eav.Logging;
+
+namespace ToSic.Eav.WebApi
+{
+    /// <summary>
+    /// Temporary class to build the Real infrastructure before all controllers support it.
+    /// Created 2022-02
+    /// Probably remove again once we're done migrating - see https://r.2sxc.org/proxy-controllers
+    /// </summary>
+    public class DummyControllerReal: HasLog<DummyControllerReal>
+    {
+        public DummyControllerReal() : base("") { }
+    }
+}
