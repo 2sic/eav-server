fb90608b46e2935fd82a9ec77d2beb647aec6f29
ijungleboy <daniel.mettler@2sic.com>
2022-02-23 16:36:30 +0100

    minor clean-up

--- ToSic.Eav.WebApi/Security/SecurityHelpers.cs
+++ ToSic.Eav.WebApi/Security/SecurityHelpers.cs
@@ -1,35 +1,10 @@
-﻿using System;
-using System.Security.Authentication;
-using ToSic.Eav.Apps;
-using ToSic.Eav.Apps.Run;
+﻿using System.Security.Authentication;
 using ToSic.Eav.Context;
-using ToSic.Eav.Logging;
-using ToSic.Eav.Plumbing;
-using ToSic.Eav.Run;
-using ToSic.Eav.Security.Permissions;
-using ToSic.Eav.WebApi.Errors;
-using ToSic.Sxc.WebApi.Security;
 
 namespace ToSic.Eav.WebApi.Security
 {
     public static class SecurityHelpers
     {
-        //internal static void ThrowIfNotEditorOrIsPublicForm(IContextOfBlock context, IApp app, string contentTypeStaticName, ILog log)
-        //{
-        //    var permCheck = context.ServiceProvider.Build<MultiPermissionsTypes>().Init(context, app, contentTypeStaticName, log);
-        //    if (!permCheck.EnsureAll(GrantSets.WriteSomething, out var error))
-        //        throw HttpException.PermissionDenied(error);
-
-        //    if (!permCheck.UserCanWriteAndPublicFormsEnabled(out _, out error))
-        //        throw HttpException.PermissionDenied(error);
-        //}
-
-        //public static T RunIfAdmin<T>(IUser user, Func<T> task)
-        //{
-        //    ThrowIfNotAdmin(user);
-        //    return task.Invoke();
-        //}
-
         public static void ThrowIfNotAdmin(IUser user)
         {
             if (!user.IsAdmin)
