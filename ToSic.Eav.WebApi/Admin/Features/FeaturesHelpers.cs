3b71813f6a924e68cbeddb10e1dff30fc30ceaae
ijungleboy <daniel.mettler@2sic.com>
2022-03-11 09:59:14 +0100

    Update FeaturesHelpers.cs

--- ToSic.Eav.WebApi/Admin/Features/FeaturesHelpers.cs
+++ ToSic.Eav.WebApi/Admin/Features/FeaturesHelpers.cs
@@ -1,21 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using ToSic.Eav.Apps.Security;
 using ToSic.Eav.Configuration;
-using ToSic.Eav.Security.Permissions;
-using ToSic.Eav.WebApi.Security;
 
 namespace ToSic.Eav.WebApi.Admin.Features
 {
     internal class FeaturesHelpers
     {
-        internal static IEnumerable<FeatureState> FeatureListWithPermissionCheck(IFeaturesInternal features, MultiPermissionsApp permCheck)
-        {
-            // if the user has full edit permissions, he may also get the un-public features
-            // otherwise just the public Ui features
-            var includeNonPublic = permCheck.UserMayOnAll(GrantSets.WritePublished);
-
-            return features.EnabledUi.Where(f => includeNonPublic || f.Public == true);
-        }
+        /// <summary>
+        /// if the user has full edit permissions, he may also get the un-public features
+        /// otherwise just the public Ui features
+        /// </summary>
+        internal static IEnumerable<FeatureState> FeaturesUiBasedOnPermissions(IFeaturesInternal features, bool userHasPublishRight) 
+            => features.EnabledUi.Where(f => userHasPublishRight || f.Public);
     }
 }
