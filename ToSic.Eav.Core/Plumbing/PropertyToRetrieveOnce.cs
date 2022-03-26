6f4034d12763b295d3ab128caf1f0f50a1fb6948
ijungleboy <daniel.mettler@2sic.com>
2022-03-26 11:26:14 +0100

    Create PropertyToRetrieveOnce.cs

---
+++ ToSic.Eav.Core/Plumbing/PropertyToRetrieveOnce.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ToSic.Eav.Plumbing
+{
+    /// <summary>
+    /// Simple helper class to use on object properties which should be generated once.
+    /// 
+    /// Important for properties which can also return null, because then checking for null won't work to determine if we already tried to retrieve it.
+    ///
+    /// ATM used in the ResponsiveBase, but we should also use it in other places where we have a second toggle to determine if it had been retrieved already. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PropertyToRetrieveOnce<T>
+    {
+        public T Get(Func<T> generator)
+        {
+            if (_retrieved) return _value;
+            _retrieved = true;
+            return _value = generator();
+        }
+
+        private bool _retrieved;
+        private T _value;
+    }
+}
