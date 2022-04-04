9f27cc2be5e50a19d986021f5cf812995f6e181b
ijungleboy <daniel.mettler@2sic.com>
2022-04-04 22:01:41 +0200

    app can report lightspeed use to apps-management

---
+++ ToSic.Eav.WebApi/Dto/AppMetadataDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToSic.Eav.WebApi.Dto
+{
+    public class AppMetadataDto
+    {
+        public int Id { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public string Title { get; set; }
+    }
+}
