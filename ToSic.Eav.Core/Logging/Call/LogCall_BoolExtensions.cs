c218cd25685b03526bb95d135ea2fc2175071fb8
ijungleboy <daniel.mettler@2sic.com>
2022-05-23 23:54:38 +0200

    minor / plumbing

--- ToSic.Eav.Core/Logging/Call/LogCall_BoolExtensions.cs
+++ ToSic.Eav.Core/Logging/Call/LogCall_BoolExtensions.cs
@@ -5,13 +5,13 @@
 
         // TODO: @STV - pls use this instead of most calls where true/  false is in the result
 
-        public static bool ReturnTrue(this LogCall<bool> logCall) => logCall.ReturnTrue("true");
+        public static bool ReturnTrue(this LogCall<bool> logCall) => logCall.ReturnTrue("");
 
-        public static bool ReturnTrue(this LogCall<bool> logCall, string message) => logCall?.Return(true, message) ?? true;
+        public static bool ReturnTrue(this LogCall<bool> logCall, string message) => logCall?.Return(true, $"{true} {message}") ?? true;
 
-        public static bool ReturnFalse(this LogCall<bool> logCall) => logCall.ReturnFalse("false");
+        public static bool ReturnFalse(this LogCall<bool> logCall) => logCall.ReturnFalse("");
 
-        public static bool ReturnFalse(this LogCall<bool> logCall, string message) => logCall?.Return(false, message) ?? false;
+        public static bool ReturnFalse(this LogCall<bool> logCall, string message) => logCall?.Return(false, $"{false} {message}") ?? false;
 
     }
 }
