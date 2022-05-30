24e34f9d61d1513699b7918567bac06e612375e6
ijungleboy <daniel.mettler@2sic.com>
2022-05-30 15:18:45 +0200

    Create Base64.cs

---
+++ ToSic.Eav.Core/Serialization/Base64.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using ToSic.Eav.Documentation;
+
+namespace ToSic.Eav.Serialization
+{
+    /// <summary>
+    /// https://stackoverflow.com/questions/11743160/how-do-i-encode-and-decode-a-base64-string
+    /// </summary>
+    [PrivateApi]
+    public class Base64
+    {
+        public static string Encode(string plainText)
+        {
+            if(plainText == null) return null;
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Decode(string base64EncodedData)
+        {
+            if (base64EncodedData == null) return null;
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
