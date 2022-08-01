3c2fb5a196ded8115c825b5fb797ba45f9422d26
Tonći Vatavuk <tonci@sistemi.hr>
2022-08-01 07:07:30 +0200

    create a "Users" and "Roles" Data Source for VisualQuery

---
+++ ToSic.Eav.Core/Context/IRole.cs
@@ -0,0 +1,18 @@
+﻿using ToSic.Eav.Documentation;
+
+namespace ToSic.Eav.Context
+{
+    [PrivateApi]
+    public interface IRole
+    {
+        /// <summary>
+        /// Role Id as int. Works in DNN and Oqtane
+        /// </summary>
+        int Id { get; }
+
+        /// <summary>
+        /// Role name.
+        /// </summary>
+        string Name { get; }
+    }
+}
---
+++ ToSic.Eav.Core/Context/IRole_T.cs
@@ -0,0 +1,9 @@
+﻿using ToSic.Eav.Data;
+
+namespace ToSic.Eav.Context
+{
+    public interface IRole<out T>: IRole, IWrapper<T>
+    {
+
+    }
+}
