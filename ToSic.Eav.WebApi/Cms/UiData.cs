85010d47ecd3a833f68f62568b770da54b610e0e
ijungleboy <daniel.mettler@2sic.com>
2022-08-23 12:10:49 +0200

    minor

--- ToSic.Eav.WebApi/Cms/UiData.cs
+++ ToSic.Eav.WebApi/Cms/UiData.cs
@@ -16,7 +16,10 @@ namespace ToSic.Eav.WebApi.Cms
         /// if the user has full edit permissions, he may also get the un-public features
         /// otherwise just the public Ui features
         /// </summary>
-        public IList<FeatureState> Features(IContextOfApp appContext, IMultiPermissionCheck permCheck)
+        /// <remarks>
+        /// It's virtual, so other code can inherit from this. 
+        /// </remarks>
+        public virtual IList<FeatureState> Features(IContextOfApp appContext, IMultiPermissionCheck permCheck)
         {
             var userHasPublishRights = permCheck.UserMayOnAll(GrantSets.WritePublished);
             return appContext.User.IsAdmin
