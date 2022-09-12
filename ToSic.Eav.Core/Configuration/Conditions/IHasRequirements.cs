70b7f4eea910a9f6655a108acbb9e5b527593082
ijungleboy <daniel.mettler@2sic.com>
2022-09-12 19:30:53 +0200

    minor docs

--- ToSic.Eav.Core/Configuration/Conditions/IHasRequirements.cs
+++ ToSic.Eav.Core/Configuration/Conditions/IHasRequirements.cs
@@ -4,6 +4,9 @@ namespace ToSic.Eav.Configuration
 {
     public interface IHasRequirements
     {
+        /// <summary>
+        /// Optional requirements which are necessary for this feature to be used
+        /// </summary>
         List<Condition> Requirements { get; }
     }
 }
--- ToSic.Eav.Core/Data/IHasIdentityNameId.cs
+++ ToSic.Eav.Core/Data/IHasIdentityNameId.cs
@@ -2,6 +2,10 @@
 {
     public interface IHasIdentityNameId
     {
+        /// <summary>
+        /// Primary identifier of an object which has this property.
+        /// It will be unique and used as an ID where needed.
+        /// </summary>
         string NameId { get; }
     }
 }
