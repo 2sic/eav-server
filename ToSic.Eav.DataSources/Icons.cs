199c9279b688682abcc7eeb08e357bd3d09a40fc
ijungleboy <daniel.mettler@2sic.com>
2022-11-03 19:18:34 +0100

    sync data-source icons with updated UI

--- ToSic.Eav.DataSources/Icons.cs
+++ ToSic.Eav.DataSources/Icons.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using ToSic.Eav.Documentation;
+﻿using ToSic.Eav.Documentation;
 
 namespace ToSic.Eav.DataSources
 {
@@ -16,61 +15,61 @@ namespace ToSic.Eav.DataSources
 
         public const string BorderOuter = "border_outer";
 
-        public const string CopyAll = "copy-all";
+        public const string CopyAll = "copy_all";
 
-        public const string DateRange = "date-range";
+        public const string DateRange = "date_range";
 
-        public const string Delete = "delete-sweep";
+        public const string Delete = "delete_sweep";
 
         public const string Description = "description";
         public const string Dns = "dns";
 
-        public const string DynamicForm = "dynamic-form";
-        public const string EditAttributes = "edit-attributes";
+        public const string DynamicForm = "dynamic_form";
+        public const string EditAttributes = "edit_attributes";
         public const string Eye = "visibility";
         public const string Face = "face";
-        public const string Filter1 = "filter-1";
-        public const string FilterList = "filter-list";
+        public const string Filter1 = "filter_1";
+        public const string FilterList = "filter_list";
         public const string Fingerprint = "fingerprint";
-        public const string FormDyn = "dynamic-form";
+        public const string FormDyn = "dynamic_form";
 
-        public const string HistoryOff = "history-toggle-off";
+        public const string HistoryOff = "history_toggle_off";
 
-        public const string HtmlDotDotDot = "settings-ethernet";
+        public const string HtmlDotDotDot = "settings_ethernet";
 
         public const string Launch = "launch";
 
-        public const string Merge = "call-merge";
-        public const string MergeLeft = "merge-type";
+        public const string Merge = "call_merge";
+        public const string MergeLeft = "merge_type";
 
-        public const string OfferLocal = "local-offer";
+        public const string OfferLocal = "local_offer";
 
-        public const string PageFind = "find-in-page";
+        public const string PageFind = "find_in_page";
 
-        public const string ParentChild = "escalator-warning";
-        public const string Parents = "family-restroom";
+        public const string ParentChild = "escalator_warning";
+        public const string Parents = "family_restroom";
 
         public const string PersonCircled = "attribution";
 
-        public const string RecentActor = "recent-actors";
+        public const string RecentActor = "recent_actors";
 
-        public const string RouteAlt = "alt-route";
+        public const string RouteAlt = "alt_route";
 
         public const string Share = "share";
 
         public const string Shuffle = "shuffle";
         public const string Sort = "sort";
-        public const string Stories = "auto-stories";
+        public const string Stories = "auto_stories";
 
-        public const string TableChart = "table-chart";
+        public const string TableChart = "table_chart";
 
         public const string Translate = "translate";
 
-        public const string Tree = "account-tree";
+        public const string Tree = "account_tree";
 
         public const string Metadata = "loyalty";
 
-        public const string UserCircled = "account-circle";
+        public const string UserCircled = "account_circle";
 
         public const string Warning = "warning";
     }
