b3fb158145d8f6c2e6415de87c21d47f3a7ad9ff
Tonći Vatavuk <tonci@sistemi.hr>
2022-11-23 14:26:02 +0100

    remove obsolete ILog and Log methods

--- ToSic.Lib.Logging/Log/Log_Obsolete.cs
+++ ToSic.Lib.Logging/Log/Log_Obsolete.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿//using System;
+//using System.Runtime.CompilerServices;
 
-namespace ToSic.Lib.Logging
-{
-    public partial class Log
-    {
-        /// <inheritdoc />
-        [Obsolete("Will remove soon - probably v15 as it's probably internal only")]
-        public string Add(string message,
-            [CallerFilePath] string cPath = null,
-            [CallerMemberName] string cName = null,
-            [CallerLineNumber] int cLine = 0)
-        {
-            this.AddInternal(message, new CodeRef(cPath, cName, cLine));
-            return message;
-        }
+//namespace ToSic.Lib.Logging
+//{
+//    public partial class Log
+//    {
+//        /// <inheritdoc />
+//        [Obsolete("Will remove soon - probably v15 as it's probably internal only")]
+//        public string Add(string message,
+//            [CallerFilePath] string cPath = null,
+//            [CallerMemberName] string cName = null,
+//            [CallerLineNumber] int cLine = 0)
+//        {
+//            this.AddInternal(message, new CodeRef(cPath, cName, cLine));
+//            return message;
+//        }
 
-        public void Exception(Exception ex) => this?.Ex(ex);
+//        public void Exception(Exception ex) => this?.Ex(ex);
 
-        /// <inheritdoc />
-        [Obsolete("Do not use any more! Use extension method Fn instead")]
-        public Action<string> Call(string parameters = null, string message = null,
-            bool useTimer = false,
-            [CallerFilePath] string cPath = null,
-            [CallerMemberName] string cName = null,
-            [CallerLineNumber] int cLine = 0)
-            => Wrapper(
-                $"{cName}({parameters}) {message}",
-                useTimer,
-                new CodeRef(cPath, cName, cLine)
-            );
+//        /// <inheritdoc />
+//        [Obsolete("Do not use any more! Use extension method Fn instead")]
+//        public Action<string> Call(string parameters = null, string message = null,
+//            bool useTimer = false,
+//            [CallerFilePath] string cPath = null,
+//            [CallerMemberName] string cName = null,
+//            [CallerLineNumber] int cLine = 0)
+//            => Wrapper(
+//                $"{cName}({parameters}) {message}",
+//                useTimer,
+//                new CodeRef(cPath, cName, cLine)
+//            );
 
-    }
-}
+//    }
+//}
