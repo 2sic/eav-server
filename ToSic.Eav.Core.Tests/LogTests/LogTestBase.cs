eba088b9cc4c34f02d58dffac892db33736acbd7
Tonći Vatavuk <tonci@sistemi.hr>
2022-11-25 14:25:31 +0100

    minor

--- ToSic.Eav.Core.Tests/LogTests/LogTestBase.cs
+++ ToSic.Eav.Core.Tests/LogTests/LogTestBase.cs
@@ -5,7 +5,7 @@ namespace ToSic.Eav.Core.Tests.LogTests
 {
     public class LogTestBase
     {
-        public Logging.Simple.Log SL(string l) => new Logging.Simple.Log(l);
+        //public Logging.Simple.Log SL(string l) => new Logging.Simple.Log(l);
         public Log L(string l) => new Log(l);
         public LogAdapter LA(string l) => new LogAdapter(L(l));
     }
