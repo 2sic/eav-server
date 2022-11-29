f349e3fecef808a10966a4581fc3b12b3c9e7b60
ijungleboy <daniel.mettler@2sic.com>
2022-11-29 13:11:30 +0100

    Remove old Log History

--- ToSic.Eav.Core/Logging/History.cs
+++ ToSic.Eav.Core/Logging/History.cs
@@ -6,11 +6,12 @@ namespace ToSic.Eav.Logging
     [PrivateApi]
     public static class History
     {
-        [Obsolete("Will be removed in 2sxc 15 - just kept temporarily in case external DLLs are using this. Please get a LogHistory object using DI and call Add() on that")]
+        [Obsolete("Will be removed in 2sxc 16 - just kept temporarily in case external DLLs are using this. Please get a LogHistory object using DI and call Add() on that")]
         public static void Add(string key, ILog log)
         {
+            new Lib.Logging.LogHistory().Add(key, log.GetContents());
             // TODO: ADD Obsolete WARNING IF THIS IS CALLED
-            new LogHistory().Add(key, log);
+            //new LogHistory().Add(key, log);
         }
     }
 
