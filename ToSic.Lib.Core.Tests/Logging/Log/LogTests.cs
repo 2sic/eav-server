d5ec5b38bd2d9a44de2ddc0de81d6a380617d091
Tonći Vatavuk <tonci@sistemi.hr>
2022-12-19 16:27:49 +0100

    Create test project ToSic.Lib.Core.Tests

---
+++ ToSic.Lib.Core.Tests/Logging/Log/LogTests.cs
@@ -0,0 +1,16 @@
+﻿using ToSic.Lib.Logging;
+
+namespace ToSic.Lib.Core.Tests.Logging.Log
+{
+    [TestClass()]
+    public class LogTests
+    {
+        [TestMethod()]
+        public void LogTest()
+        {
+            var log = new Lib.Logging.Log("test");
+            log.A("test-add");
+            Assert.AreEqual(1, log.Entries.Count, "should have one entry");
+        }
+    }
+}
