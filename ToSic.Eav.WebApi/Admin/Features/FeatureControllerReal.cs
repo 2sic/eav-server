9b6fa356b3124ab20785e4a5ad3eeb565faf11e6
ijungleboy <daniel.mettler@2sic.com>
2022-12-22 22:02:28 +0100

    Update FeatureControllerReal.cs

--- ToSic.Eav.WebApi/Admin/Features/FeatureControllerReal.cs
+++ ToSic.Eav.WebApi/Admin/Features/FeatureControllerReal.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using ToSic.Eav.Configuration;
+using ToSic.Eav.Plumbing;
 using ToSic.Lib.DI;
 using ToSic.Lib.Logging;
 using ToSic.Lib.Services;
@@ -9,20 +10,27 @@ namespace ToSic.Eav.WebApi.Admin.Features
 {
     public class FeatureControllerReal : ServiceBase, IFeatureController
     {
+        /// <summary>
+        /// Must be lazy, to avoid log being filled with sys-loading infos when this service is being used
+        /// </summary>
+        private readonly LazySvc<EavSystemLoader> _systemLoaderLazy;
+        private readonly LazySvc<IFeaturesInternal> _featuresLazy;
         public const string LogSuffix = "Feats";
 
         #region Constructor / DI
 
         public FeatureControllerReal(
-            LazySvc<EavSystemLoader> systemLoaderLazy
-            ) : base("Bck.Feats") =>
-            ConnectServices(_systemLoaderLazy = systemLoaderLazy);
+            LazySvc<EavSystemLoader> systemLoaderLazy,
+            LazySvc<IFeaturesInternal> featuresLazy
+            ) : base("Bck.Feats")
+        {
+            ConnectServices(
+                _systemLoaderLazy = systemLoaderLazy,
+                _featuresLazy = featuresLazy
+            );
+        }
 
 
-        /// <summary>
-        /// Must be lazy, to avoid log being filled with sys-loading infos when this service is being used
-        /// </summary>
-        private readonly LazySvc<EavSystemLoader> _systemLoaderLazy;
 
         #endregion
 
@@ -35,6 +43,13 @@ namespace ToSic.Eav.WebApi.Admin.Features
                 return wrapLog.ReturnFalse("no features changes");
             
             return wrapLog.ReturnAsOk(_systemLoaderLazy.Value.UpdateFeatures(changes));
+        }
+
+        public FeatureState Details(string nameId)
+        {
+            var l = Log.Fn<FeatureState>(nameId);
+            var details = _featuresLazy.Value.All.FirstOrDefault(f => f.NameId.EqualsInsensitive(nameId));
+            return l.Return(details);
         }
     }
 }
