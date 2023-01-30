7f483865ea863099b32e564599c92d97f74d2d86
ijungleboy <daniel.mettler@2sic.com>
2023-01-30 19:01:00 +0100

    move api keys for UI to edit-load
    https://github.com/2sic/2sxc/issues/2990

--- ToSic.Eav.WebApi/Context/Ctx.cs
+++ ToSic.Eav.WebApi/Context/Ctx.cs
@@ -15,8 +15,7 @@ namespace ToSic.Eav.WebApi.Context
         System = 1 << 6,
         User = 1 << 7,
         Features = 1 << 8,
-        ApiKeys = 1 << 9,
-        All = AppBasic | AppEdit | AppAdvanced | Language | Page | Site | System | User | Features | ApiKeys,
+        All = AppBasic | AppEdit | AppAdvanced | Language | Page | Site | System | User | Features,
         General = AppBasic | AppEdit | AppAdvanced | Language | Page | Site | System | User | Features,
     }
 
