0d9c5b1c90a87786c9da8a1a8e9e1c2b6c2a2b99
ijungleboy <daniel.mettler@2sic.com>
2023-02-01 14:52:19 +0100

    Minor Log API clean-up

--- ToSic.Lib.Core/Logging/HasLog/IHasLogExtensions.cs
+++ ToSic.Lib.Core/Logging/HasLog/IHasLogExtensions.cs
@@ -13,16 +13,12 @@ namespace ToSic.Lib.Logging
         /// <summary>
         /// Null-Safe method to link logs together. Both the parent and the Log could be null. 
         /// </summary>
-        /// <returns>The same object as started this, to allow chaining</returns>
-        public static T LinkLog<T>(this T thingWithLog, ILog parentLog) where T: class, IHasLog 
-            => thingWithLog.LinkLog(parentLog, null, false);
-
-        /// <summary>
-        /// Null-Safe method to link logs together. Both the parent and the Log could be null. 
-        /// </summary>
+        /// <param name="thingWithLog">Object which is an IHasLog</param>
+        /// <param name="parentLog">Log to connect to</param>
+        /// <param name="forceConnect">Force connect the logs, even if it's an <see cref="ILogShouldNeverConnect"/></param>
         /// <returns>The same object as started this, to allow chaining</returns>
         [PrivateApi]
-        public static T LinkLog<T>(this T thingWithLog, ILog parentLog, string name, bool forceConnect) where T: class, IHasLog
+        public static T LinkLog<T>(this T thingWithLog, ILog parentLog, /*string name,*/ bool forceConnect = false) where T: class, IHasLog
         {
             if (thingWithLog == null) return null;
             if (thingWithLog is ILogShouldNeverConnect && !forceConnect)
@@ -33,7 +29,7 @@ namespace ToSic.Lib.Logging
             else
             {
                 // Connect if possible
-                (thingWithLog.Log as Log)?.LinkTo(parentLog, name);
+                (thingWithLog.Log as Log)?.LinkTo(parentLog/*, name*/);
 
                 // If the object needs a call back, give it...
                 (thingWithLog as ILogWasConnected)?.LogWasConnected();
