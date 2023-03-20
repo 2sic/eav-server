dd8708261e98f467b250b2a4226b85a20c1c73db
ijungleboy <daniel.mettler@2sic.com>
2023-03-20 21:17:29 +0100

    add simple typeName to DataSourceCatalog

--- ToSic.Eav.DataSources/Catalog/DataSourceCatalog_Cache.cs
+++ ToSic.Eav.DataSources/Catalog/DataSourceCatalog_Cache.cs
@@ -18,22 +18,24 @@ namespace ToSic.Eav.DataSources.Catalog
         /// Find a DataSource which may have changed it's name. Will look in the cached names list.
         /// </summary>
         /// <param name="name"></param>
+        /// <param name="appId"></param>
         /// <returns></returns>
         private static DataSourceInfo FindInCache(string name, int appId)
         {
             var list = GlobalCache;
-            var inGlobal = FindInCachedList(name, list);
+            var inGlobal = FindInCachedList(name, list, false);
             if (inGlobal != null) return inGlobal;
 
             // New v15.04 - also support app level data sources
             return !AppCache.TryGetValue(appId, out var appCache) 
                 ? null 
-                : FindInCachedList(name, appCache);
+                : FindInCachedList(name, appCache, true);
         }
 
-        private static DataSourceInfo FindInCachedList(string name, List<DataSourceInfo> list) =>
+        private static DataSourceInfo FindInCachedList(string name, List<DataSourceInfo> list, bool isLocal) =>
             // First check for normal type name
             list.FirstOrDefault(dst => dst.Name.EqualsInsensitive(name))
+            ?? (isLocal ? list.FirstOrDefault(dst => dst.TypeName.EqualsInsensitive(name)) : null)
             // Otherwise check for historical names in the VisualQuery Attribute
             ?? list.FirstOrDefault(dst =>
                 dst.VisualQuery?.PreviousNames.Any(pn => pn.EqualsInsensitive(name)) ?? false);
--- ToSic.Eav.DataSources/DataSourceInfo.cs
+++ ToSic.Eav.DataSources/DataSourceInfo.cs
@@ -7,14 +7,19 @@ namespace ToSic.Eav.DataSources
     public class DataSourceInfo: TypeWithMetadataBase<VisualQueryAttribute>
     {
         public VisualQueryAttribute VisualQuery { get; }
+
+        // By default the name is the global name of the VisualQuery.
         public override string Name => VisualQuery?.GlobalName;
 
         public bool IsGlobal { get; }
+
+        public string TypeName { get; }
 
         public DataSourceInfo(Type dsType, bool isGlobal): base(dsType)
         {
             VisualQuery = TypeMetadata;
             IsGlobal = isGlobal;
+            TypeName = dsType.Name;
         }
     }
 
