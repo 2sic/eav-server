33af3cd176174d80b75134b1865d74a644c0a5d2
Tonći Vatavuk <tonci@sistemi.hr>
2023-03-28 14:53:18 +0200

    upgrade Scopes DataSource to use CustomDataSource with RawEntity

--- ToSic.Eav.Apps/DataSources/System/Scopes.cs
+++ ToSic.Eav.Apps/DataSources/System/Scopes.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using ToSic.Eav.Apps;
 using ToSic.Eav.Data;
 using ToSic.Eav.Data.Build;
+using ToSic.Eav.Data.Raw;
 using ToSic.Eav.DataSource;
 using ToSic.Eav.DataSource.VisualQuery;
 using ToSic.Lib.Documentation;
 using ToSic.Lib.Logging;
-using IEntity = ToSic.Eav.Data.IEntity;
 
 // ReSharper disable once CheckNamespace
 namespace ToSic.Eav.DataSources.Sys
@@ -19,56 +18,40 @@ namespace ToSic.Eav.DataSources.Sys
     /// </summary>
     [InternalApi_DoNotUse_MayChangeWithoutNotice]
     [VisualQuery(
-        NiceName = "Data Scopes",
-        UiHint = "Data Scopes group Content-Types by topic",
-        Icon = Icons.Scopes,
-        Type = DataSourceType.System,
+        ConfigurationType = "",
         NameId = "f134e3c1-f09f-4fbc-85be-de43a64c6eed",
+        Icon = Icons.Scopes,
+        NiceName = "Data Scopes",
+        Type = DataSourceType.System,
+        UiHint = "Data Scopes group Content-Types by topic",
         Audience = Audience.Advanced,
         DynamicOut = false
     )]
     // ReSharper disable once UnusedMember.Global
-    public sealed class Scopes : Eav.DataSource.DataSourceBase
+    public sealed class Scopes : CustomDataSource
     {
-        private readonly IDataFactory _scopesFactory;
-
-        #region Configuration-properties (no config)
-
-        #endregion
-
         /// <inheritdoc />
         /// <summary>
         /// Constructs a new Scopes DS
         /// </summary>
         [PrivateApi]
-        public Scopes(MyServices services, IAppStates appStates, IDataFactory dataFactory) : base(services, $"{DataSourceConstants.LogPrefix}.Scopes")
+        public Scopes(MyServices services, IAppStates appStates) : base(services, $"{DataSourceConstants.LogPrefix}.Scopes")
         {
-            ConnectServices(
-                _appStates = appStates,
-                // Note: these are really the scopes of the current app, so we set the AppId
-                _scopesFactory = dataFactory.New(options: new DataFactoryOptions(appId: AppId, typeName: "Scope"))
-            );
-            ProvideOut(GetList);
+            ConnectServices(_appStates = appStates);
+            ProvideOutRaw(GetList, options: () => new DataFactoryOptions(typeName: "Scope", titleField: "Name", autoId: false));
         }
         private readonly IAppStates _appStates;
 
-        private IImmutableList<IEntity> GetList() => Log.Func(l =>
+        private IEnumerable<IRawEntity> GetList() => Log.Func(l =>
         {
-            Configuration.Parse();
-
-            var appId = AppId;
-
-            var scopes = _appStates.Get(appId).ContentTypes.GetAllScopesWithLabels();
-
-            var scopeBuilder = _scopesFactory;
+            var scopes = _appStates.Get(AppId).ContentTypes.GetAllScopesWithLabels();
             var list = scopes
-                .Select(s => scopeBuilder.Create(new Dictionary<string, object>
-                    {
-                        { Data.Attributes.NameIdNiceName, s.Key },
-                        { Data.Attributes.TitleNiceName, s.Value },
-                    }
-                ))
-                .ToImmutableList();
+                .Select(s => new RawEntity(new Dictionary<string, object>
+                {
+                    { Data.Attributes.NameIdNiceName, s.Key },
+                    { "Name", s.Value }
+                }))
+                .ToList();
 
             return (list, $"{list.Count}");
         });
