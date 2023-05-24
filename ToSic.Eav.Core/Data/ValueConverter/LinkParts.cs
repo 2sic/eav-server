d6fe539ba8adfea6fe2baa2458acfac92b3e5b79
ijungleboy <daniel.mettler@2sic.com>
2023-05-24 12:12:29 +0200

    Update LinkParts.cs

--- ToSic.Eav.Core/Data/ValueConverter/LinkParts.cs
+++ ToSic.Eav.Core/Data/ValueConverter/LinkParts.cs
@@ -8,25 +8,47 @@ namespace ToSic.Eav.Data
         {
             var regularExpression = Regex.Match(link, RegExToDetectConvertable, RegexOptions.IgnoreCase);
             IsMatch = regularExpression.Success;
-            if (IsMatch)
-            {
-                Type = regularExpression.Groups[RegExType].Value.ToLowerInvariant();
-                Id = int.Parse(regularExpression.Groups[RegExId].Value);
-                Params = regularExpression.Groups[RegExParams].Value ?? "";
-                IsPage = Type == ValueConverterBase.PrefixPage;
-            }
+            if (!IsMatch) return;
 
+            Type = regularExpression.Groups[RegExType].Value.ToLowerInvariant();
+            Id = int.Parse(regularExpression.Groups[RegExId].Value);
+            Params = regularExpression.Groups[RegExParams].Value ?? "";
+            IsPage = Type == ValueConverterBase.PrefixPage;
+
+        }
+
+        /// <summary>
+        /// This is a special temp overload to detect `file:filename.jpg` variations
+        /// </summary>
+        /// <param name="link"></param>
+        /// <param name="someOtherParameter"></param>
+        public LinkParts(string link, bool someOtherParameter)
+        {
+            var regularExpression = Regex.Match(link, RegExToDetectName, RegexOptions.IgnoreCase);
+            IsMatch = regularExpression.Success;
+            if (!IsMatch) return;
+
+            Type = regularExpression.Groups[RegExType].Value.ToLowerInvariant();
+            Name = regularExpression.Groups[RegExName].Value;
+            Params = regularExpression.Groups[RegExParams].Value ?? "";
+            IsPage = Type == ValueConverterBase.PrefixPage;
         }
 
         public readonly bool IsMatch;
         public readonly bool IsPage;
         public readonly string Type;
         public readonly int Id;
+        public readonly string Name;
+        public bool UseName => Name != null;
         public readonly string Params;
 
-        public const string RegExToDetectConvertable = @"^(?<type>(file|page)):(?<id>[0-9]+)(?<params>(\?|\#).*)?$";
-        public const string RegExType = "type";
-        public const string RegExId = "id";
-        public const string RegExParams = "params";
+        // language=regex
+        private const string RegExToDetectConvertable = @"^(?<type>(file|page)):(?<id>[0-9]+)(?<params>(\?|\#).*)?$";
+        // language=regex
+        private const string RegExToDetectName = @"^(?<type>(file|adam)):(?<name>[^\\^\/^?]+)(?<params>(\?|\#).*)?$";
+        private const string RegExType = "type";
+        private const string RegExId = "id";
+        private const string RegExName = "name";
+        private const string RegExParams = "params";
     }
 }
