dc651868f1d9a374577238e7b7818852e3c15d26
ijungleboy <daniel.mettler@2sic.com>
2023-08-10 07:54:56 +0200

    Fix some cases of wrapper equality checks for dyn

--- ToSic.Eav.Core/Data/Shared/WrapperEquality.cs
+++ ToSic.Eav.Core/Data/Shared/WrapperEquality.cs
@@ -21,12 +21,17 @@ namespace ToSic.Eav.Data
         /// <returns></returns>
         public static bool EqualsWrapper<T>(IMultiWrapper<T> parent, IMultiWrapper<T> dynObj) where T : class
         {
+            // Check the root objects first and independently
+            // Since if either is null and the other is not, then they can't be equal
+            // 2023-08-08 previously this was different, but IMHO wrong
+            if (parent is null) return dynObj is null;
+            if (dynObj is null) return false;
             // first ensure that neither parent nor the sub-item are null
             // ReSharper disable once ConvertIfStatementToReturnStatement
-            if (parent?.RootContentsForEqualityCheck is null) return false;
+            if (parent.RootContentsForEqualityCheck is null) return dynObj.RootContentsForEqualityCheck is null;
 
             // then verify that the underlying entities have the same reference
-            return ReferenceEquals(parent.RootContentsForEqualityCheck, dynObj?.RootContentsForEqualityCheck);
+            return ReferenceEquals(parent.RootContentsForEqualityCheck, dynObj.RootContentsForEqualityCheck);
         }
 
         /// <summary>
@@ -39,7 +44,7 @@ namespace ToSic.Eav.Data
         /// <returns></returns>
         public static bool EqualsObj<T>(IMultiWrapper<T> parent, object obj) where T : class
         {
-            if (obj is null) return false;
+            if (obj is null) return parent is null; // 2023-08-08 previously returned false every time, which caused problems on null-checks;
             if (ReferenceEquals(parent, obj)) return true;
             // This is a check if it's exactly that type, but it could be an inherited type, so false must fall to the next check
             if (obj is T && parent.RootContentsForEqualityCheck.Equals(obj)) return true;
@@ -57,7 +62,10 @@ namespace ToSic.Eav.Data
         /// </remarks>
         /// <returns></returns>
         public static bool IsEqual<T>(IMultiWrapper<T> d1, IMultiWrapper<T> d2) where T : class
-            // check most basic case - they are really the same object or both null
-            => !(d1 is null) && (ReferenceEquals(d1, d2) || Equals(d1.RootContentsForEqualityCheck, d2?.RootContentsForEqualityCheck));
+            => d1 is null
+                // 2023-08-08 previously returned false if d1 was null (without comparing d2)
+                ? d2 is null
+                // check most basic case - they are really the same object or both null - or the same applies to their inner contents
+                : !(d2 is null) && (ReferenceEquals(d1, d2) || Equals(d1.RootContentsForEqualityCheck, d2?.RootContentsForEqualityCheck));
     }
 }
