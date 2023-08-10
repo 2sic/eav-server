1f88d88929a783662ec25f11fab30a3bc3ac29d9
ijungleboy <daniel.mettler@2sic.com>
2023-08-10 07:21:43 +0200

    Create LazyLike.cs

---
+++ ToSic.Lib.Core/DI/LazyLike.cs
@@ -0,0 +1,9 @@
+﻿namespace ToSic.Lib.DI
+{
+    public class LazyLike<TService>: ILazyLike<TService>
+    {
+        public LazyLike(TService value) => Value = value;
+        public TService Value { get; }
+        public bool IsValueCreated => true;
+    }
+}
