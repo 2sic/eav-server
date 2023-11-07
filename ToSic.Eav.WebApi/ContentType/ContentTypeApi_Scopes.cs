a979b74b82e668d97f5774ec457b6e58845a1f29
iJungleboy <daniel.mettler@2sic.com>
2023-11-07 10:43:07 +0100

    Update ContentTypeApi_Scopes.cs
    minor

--- ToSic.Eav.WebApi/ContentType/ContentTypeApi_Scopes.cs
+++ ToSic.Eav.WebApi/ContentType/ContentTypeApi_Scopes.cs
@@ -11,9 +11,7 @@ namespace ToSic.Eav.WebApi
         public IDictionary<string, string> Scopes()
         {
             var wrapLog = Log.Fn<IDictionary<string, string>>();
-            var appState = _appStates.Get(_appId);
-
-            var results = appState.ContentTypes.GetAllScopesWithLabels();
+            var results = _appCtxPlus.AppState.ContentTypes.GetAllScopesWithLabels();
             return wrapLog.Return(results);
         }
 
