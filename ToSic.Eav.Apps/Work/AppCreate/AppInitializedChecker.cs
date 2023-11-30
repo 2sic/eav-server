fdee1bdd5d2cb53d924ed6e28a4529cee72c0a54
iJungleboy <daniel.mettler@2sic.com>
2023-11-30 08:08:32 +0100

    Update AppInitializedChecker.cs
    https://github.com/2sic/2sxc/issues/3203

--- ToSic.Eav.Apps/Work/AppCreate/AppInitializedChecker.cs
+++ ToSic.Eav.Apps/Work/AppCreate/AppInitializedChecker.cs
@@ -30,7 +30,7 @@ public class AppInitializedChecker : ServiceBase, IAppInitializedChecker
 
         var l = log.Fn<bool>($"..., {appName}");
 
-        if (CheckIfAllPartsExist(appState, out _, out _, out _, log))
+        if (CheckIfAllPartsExist(appState, codeRefTrail, out _, out _, out _, log))
             return l.ReturnFalse("ok");
 
         // something is missing, so we must build them
@@ -46,24 +46,40 @@ public class AppInitializedChecker : ServiceBase, IAppInitializedChecker
     /// This should remain static, because it's used 2x
     /// </remarks>
     /// <param name="appState"></param>
+    /// <param name="codeRefTrail"></param>
     /// <param name="appConfig"></param>
     /// <param name="appResources"></param>
     /// <param name="appSettings"></param>
     /// <param name="log"></param>
     /// <returns></returns>
-    internal static bool CheckIfAllPartsExist(IAppState appState, out IEntity appConfig, out IEntity appResources, out IEntity appSettings, ILog log)
+    internal static bool CheckIfAllPartsExist(IAppState appState, CodeRefTrail codeRefTrail, out IEntity appConfig, out IEntity appResources, out IEntity appSettings, ILog log)
     {
         var l = log.Fn<bool>();
         appConfig = appState.GetMetadata(TargetTypes.App, appState.AppId, TypeAppConfig).FirstOrDefault();
         appResources = appState.GetMetadata(TargetTypes.App, appState.AppId, TypeAppResources).FirstOrDefault();
         appSettings = appState.GetMetadata(TargetTypes.App, appState.AppId, TypeAppSettings).FirstOrDefault();
 
+        var allOk = appConfig != null && appResources != null && appSettings != null;
+        var msg = $"Config: {appConfig != null}, Resources: {appResources != null}, Settings: {appSettings != null}; AllOk: {allOk}";
+        codeRefTrail.AddMessage(msg);
+        l.A(msg);
 
-        // if nothing must be done, return now
-        if (appConfig != null && appResources != null && appSettings != null)
-            return l.ReturnTrue("all ok");
+        if (allOk) return l.ReturnTrue("all ok");
 
-        l.A($"Config: {appConfig != null}, Resources: {appResources != null}, Settings: {appSettings != null}");
+        // if something is missing, add more information to better find out what really went wrong.
+        // Because ATM it randomly adds them again when they are already there!
+        // Must try catch, to be really sure we don't break anything
+        // https://github.com/2sic/2sxc/issues/3203
+        try
+        {
+            codeRefTrail.AddMessage($"App Entity Count: {appState.List.Count}");
+            codeRefTrail.AddMessage($"App ContentType Count: {appState.ContentTypes.Count()}");
+            codeRefTrail.AddMessage($"App Metadata Count: {appState.Metadata?.Count()}");
+            codeRefTrail.AddMessage($"Has Type App Config: {appState.GetContentType(TypeAppConfig)}");
+            codeRefTrail.AddMessage($"Has Type App Resources: {appState.GetContentType(TypeAppResources)}");
+            codeRefTrail.AddMessage($"Has Type App Settings: {appState.GetContentType(TypeAppSettings)}");
+        }
+        catch { /* ignore */ }
 
         return l.ReturnFalse("some missing");
     }
