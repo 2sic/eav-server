9230ac702cf9e404f0fe75d14ded0d3a9d8bbfaf
iJungleboy <daniel.mettler@2sic.com>
2023-12-08 16:11:14 +0100

    minor docs

--- ToSic.Lib.Core/Coding/StableApi.cs
+++ ToSic.Lib.Core/Coding/StableApi.cs
@@ -6,12 +6,14 @@ namespace ToSic.Lib.Coding;
 /// Special placeholder to indicate that all parameters following this should be named.
 /// It is really important that all parameters following this are named, since the API won't guarantee the order of the parameter names.
 ///
-/// Example:
-///
-/// * This signature: `...Save(string name, NoParamOrder noParamOrder = default, string title = title, string description = default)`
-/// * allows a) `Save("MyName", title: "MyTitle")` and b) `Save("MyName", description: "MyDescription")`
-/// * or c) Save("MyName") - without additional parameters
-/// See [Convention: Named Parameters(https://go.2sxc.org/named-params).
+/// Example: This signature: `...Save(string name, NoParamOrder noParamOrder = default, string title = title, string description = default)`
+/// allows:
+/// 
+/// 1. `Save("MyName", title: "MyTitle")`
+/// 2. `Save("MyName", description: "MyDescription")`
+/// 3. Save("MyName") - without additional parameters
+/// 
+/// See [Convention: Named Parameters](https://go.2sxc.org/named-params).
 /// </summary>
 [PublicApi]
 [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
