b8efd9bb484b7e18459478a4e386bb1e93604a54
iJungleboy <daniel.mettler@2sic.com>
2024-01-11 14:04:42 +0100

    minor harden

--- ToSic.Eav/Integration/StartupEav.cs
+++ ToSic.Eav/Integration/StartupEav.cs
@@ -1,27 +1,27 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using ToSic.Eav.Apps;
 using ToSic.Eav.Apps.Integration;
 using ToSic.Eav.DataFormats.EavLight;
 using ToSic.Eav.DataSources;
 using ToSic.Eav.ImportExport;
 using ToSic.Eav.Repository.Efc;
+using ToSic.Eav.StartUp;
 using ToSic.Eav.WebApi;
 using ToSic.Lib;
 
-namespace ToSic.Eav.StartUp;
+namespace ToSic.Eav.Integration;
 
 /// <summary>
 /// Global Eav Configuration
 /// </summary>
 [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
-public static class StartUpEav
+public static class StartupEav
 {
     /// <summary>
     /// Use this to setup the new DI container
     /// </summary>
     /// <param name="services"></param>
-    public static IServiceCollection AddEav(this IServiceCollection services)
+    public static IServiceCollection AddEavEverything(this IServiceCollection services)
     {
         // standard IEntity conversion
         // not sure where to put it, interface is in Core but the implementation in Web, also used by DataSources for json errors
