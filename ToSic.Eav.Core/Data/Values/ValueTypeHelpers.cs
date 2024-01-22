c80ba491c67bfcc77b51df32444e0f468377c4d4
iJungleboy <daniel.mettler@2sic.com>
2024-01-22 12:25:19 +0100

    Update ValueTypeHelpers.cs

--- ToSic.Eav.Core/Data/Values/ValueTypeHelpers.cs
+++ ToSic.Eav.Core/Data/Values/ValueTypeHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ToSic.Lib.Documentation;
 
 namespace ToSic.Eav.Data;
@@ -16,4 +17,19 @@ public class ValueTypeHelpers
     [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
     public static ValueTypes Get(string typeName) 
         => Enum.TryParse<ValueTypes>(typeName ?? "", true, out var code) ? code : ValueTypes.Undefined;
+
+    public static Dictionary<ValueTypes, Type> TypeMap = new()
+    {
+        {ValueTypes.Boolean, typeof(bool)},
+        {ValueTypes.DateTime, typeof(DateTime)},
+        {ValueTypes.Entity, typeof(int)},
+        {ValueTypes.Hyperlink, typeof(string)},
+        {ValueTypes.Number, typeof(double)},
+        {ValueTypes.String, typeof(string)},
+        {ValueTypes.Empty, typeof(string)},
+        {ValueTypes.Custom, typeof(string)},
+        {ValueTypes.Json, typeof(string)}
+    };
+
+    public static Type GetType(ValueTypes type) => TypeMap[type];
 }
