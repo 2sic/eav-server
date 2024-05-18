2433e9b06eaec308925b676f4f3d6ede2e287413
iJungleboy <daniel.mettler@2sic.com>
2024-05-18 15:03:41 +0200

    first draft of FunFact (Functional Factories) and tests
    part of https://github.com/2sic/2sxc/issues/3390

---
+++ ToSic.Lib.Core/FunFact/FunFactFunctionBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToSic.Lib.Logging;
+using ToSic.Lib.Services;
+
+namespace ToSic.Lib.FunFact;
+
+/// <summary>
+/// WIP fluid functional object factory.
+/// The idea is to have a fluent API that allows you to create objects and apply actions to them in a functional way.
+///
+/// So every method will return a new object, basically queueing up what should be done to the object once created.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="parentLog"></param>
+/// <param name="functions"></param>
+/// <param name="logName"></param>
+[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+public abstract class FunFactFunctionBase<T>(ILog parentLog, IEnumerable<Func<T, T>> functions, string logName) : HelperBase(parentLog, logName ?? "Eav.FnOExp")
+{
+    /// <summary>
+    /// List of actions to apply to the object
+    /// </summary>
+    protected internal List<Func<T, T>> Actions { get; } = functions?.ToList() ?? [];
+
+    protected List<Func<T, T>> CloneActions(Func<T, T> addition) => [..functions, addition];
+
+    protected T Apply(T initial) => Actions.Aggregate(initial, (current, action) => action(current));
+
+    public abstract T CreateResult();
+}
---
+++ ToSic.Lib.Core/FunFact/FunFactString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ToSic.Lib.Logging;
+
+namespace ToSic.Lib.FunFact;
+
+/// <summary>
+/// WIP fluid functional object factory.
+/// With strings, it probably doesn't offer any benefits, so this is mostly for demonstration / unit-testing purposes.
+/// </summary>
+/// <param name="parentLog"></param>
+/// <param name="actions"></param>
+internal class FunFactString(ILog parentLog, IEnumerable<Func<string, string>> actions) : FunFactFunctionBase<string>(parentLog, actions, "Lib.FunStr")
+{
+    public override string CreateResult() => Apply("");
+
+    private FunFactString Next(Func<string, string> addition) => new((Log as Log)?.Parent, CloneActions(addition));
+
+    public FunFactString Set(string value) => Next(s => value);
+
+    public FunFactString Append(string text) => Next(s => s + text);
+
+    public FunFactString Prepend(string text) => Next(s => text + s);
+
+    public FunFactString Trim() => Next(s => s.Trim());
+
+    public FunFactString Replace(string oldValue, string newValue) => Next(s => s.Replace(oldValue, newValue));
+}
