e549edd0fc3d9b10f1ea9a591550ef46deab5e6a
iJungleboy <daniel.mettler@2sic.com>
2024-05-31 17:38:52 +0200

    Update EavFeaturesService.cs
    minor code style

--- ToSic.Eav.Core/Internal/Features/EavFeaturesService.cs
+++ ToSic.Eav.Core/Internal/Features/EavFeaturesService.cs
@@ -9,28 +9,17 @@ namespace ToSic.Eav.Internal.Features;
 
 /// <summary>
 /// WARNING: this is used as a singleton / static
-/// not quite sure any more why, either the name is hard-coded in some apps or we felt like it's a performance improvement
+/// not quite sure anymore why, either the name is hard-coded in some apps or we felt like it's a performance improvement
 /// </summary>
+/// <remarks>
+/// WARNING: singleton - don't use any complex services/dependencies here
+/// REASON is probably cache connection, since it should notify the system to release caches as settings change?
+/// </remarks>
 [PrivateApi("hide implementation")]
-public class EavFeaturesService: IEavFeaturesService
+[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+public class EavFeaturesService(FeaturesCatalog featuresCatalog) : IEavFeaturesService
 {
-    #region Constructor
-
-    /// <summary>
-    /// warning: singleton - don't use any complex services/dependencies here
-    /// </summary>
-    /// <param name="featuresCatalog"></param>
-    public EavFeaturesService(FeaturesCatalog featuresCatalog)
-    {
-        _featuresCatalog = featuresCatalog;
-    }
-
-    private readonly FeaturesCatalog _featuresCatalog;
-
-    #endregion
-
-
-    public IEnumerable<FeatureState> All => AllStaticCache.Get(() => Merge(Stored, _featuresCatalog.List, _staticSysFeatures));
+    public IEnumerable<FeatureState> All => AllStaticCache.Get(() => Merge(Stored, featuresCatalog.List, _staticSysFeatures));
     private static readonly GetOnce<List<FeatureState>> AllStaticCache = new();
 
     /// <summary>
@@ -43,11 +32,14 @@ public class EavFeaturesService: IEavFeaturesService
         InvariantCultureIgnoreCase);
     private HashSet<string> _enabledFeatures; // had to step back from GetOnce, because of "Error Unable to marshal host object to interpreter space"
 
-    public IEnumerable<FeatureState> UiFeaturesForEditors => All.Where(f => f.IsEnabled && f.IsForEditUi);
+    public IEnumerable<FeatureState> UiFeaturesForEditors
+        => All.Where(f => f.IsEnabled && f.IsForEditUi);
 
-    public bool IsEnabled(Guid guid) => All.Any(f => f.Aspect.Guid == guid && f.IsEnabled);
+    public bool IsEnabled(Guid guid)
+        => All.Any(f => f.Aspect.Guid == guid && f.IsEnabled);
         
-    public bool IsEnabled(IEnumerable<Guid> guids) => guids.All(IsEnabled);
+    public bool IsEnabled(IEnumerable<Guid> guids)
+        => guids.All(IsEnabled);
 
     public bool IsEnabled(params string[] nameIds)
     {
@@ -55,7 +47,8 @@ public class EavFeaturesService: IEavFeaturesService
         return nameIds.All(name => EnabledFeatures.Contains(name?.Trim()));
     }
 
-    public FeatureState Get(string nameId) => All.FirstOrDefault(f => f.Aspect.Name == nameId || f.NameId == nameId);
+    public FeatureState Get(string nameId)
+        => All.FirstOrDefault(f => f.Aspect.Name == nameId || f.NameId == nameId);
 
     public bool IsEnabled(params Feature[] features) 
         => IsEnabled(features?.Select(f => f.NameId).ToArray());
@@ -67,7 +60,9 @@ public class EavFeaturesService: IEavFeaturesService
     {
         // ReSharper disable PossibleMultipleEnumeration
         var enabled = IsEnabled(features);
-        exception = enabled ? null : new FeaturesDisabledException(message + " - " + MsgMissingSome(features.ToArray()));
+        exception = enabled
+            ? null
+            : new FeaturesDisabledException(message + " - " + MsgMissingSome(features.ToArray()));
         // ReSharper restore PossibleMultipleEnumeration
         return enabled;
     }
@@ -77,16 +72,15 @@ public class EavFeaturesService: IEavFeaturesService
     {
         var missing = ids
             .Where(i => !IsEnabled(i))
-            .Select(id =>
+            .Select(id => new
             {
-                var feat = All.FirstOrDefault(f => f.Aspect.Guid == id);
-                return new { Id = id, feat?.NameId };
+                Id = id,
+                All.FirstOrDefault(f => f.Aspect.Guid == id)?.NameId
             });
 
         var messages = missing.Select(f => $"'{f.NameId}'");
 
-        return
-            $"Features {string.Join(", ", messages)} not enabled - see also https://go.2sxc.org/features";
+        return $"Features {string.Join(", ", messages)} not enabled - see also https://go.2sxc.org/features";
     }
 
 
@@ -114,49 +108,67 @@ public class EavFeaturesService: IEavFeaturesService
     {
         var licService = new LicenseService();
 
-        var allFeats = featuresCat.Select(f =>
-        {
-            var enabled = false;
-            var licenseEnabled = false;
-            var msgShort = "default";
-            var message = " by default";
-            var expiry = DateTime.MinValue;
-
-            // Check if the required license is active
-            var enabledRule = f.LicenseRules.FirstOrDefault(lr => licService.IsEnabled(lr.FeatureSet));
-            if (enabledRule != null)
+        var allFeats = featuresCat
+            .Select(f =>
             {
-                licService.Enabled.TryGetValue(enabledRule.FeatureSet.Guid, out var licenseState);
-                var specialExpiry = licenseState?.Expiration;
-                enabled = enabledRule.EnableFeatureByDefault;
-                licenseEnabled = true; // The license is active, so it's allowed to enable this
-                msgShort = enabledRule.FeatureSet.Name;
-                message = $" by default with license {enabledRule.FeatureSet.Name}";
-                expiry = specialExpiry ?? BuiltInLicenses.UnlimitedExpiry;
-            }
+                var enabled = false;
+                var licenseEnabled = false;
+                var msgShort = "default";
+                var message = " by default";
+                var expiry = DateTime.MinValue;
 
-            // Check if the configuration would enable this feature
-            var inConfig = config?.Features.FirstOrDefault(cf => cf.Id == f.Guid);
-            if (inConfig != null)
-            {
-                enabled = licenseEnabled && inConfig.Enabled;
-                if (expiry == DateTime.MinValue) expiry = inConfig.Expires; // set expiry by configuration (when is not set by license)
-                msgShort = licenseEnabled ? "configuration" : "unlicensed";
-                message = licenseEnabled ? " by configuration" : " - requires license";
-            }
+                // Check if the required license is active
+                var enabledRule = f.LicenseRules.FirstOrDefault(lr => licService.IsEnabled(lr.FeatureSet));
+                if (enabledRule != null)
+                {
+                    licService.Enabled.TryGetValue(enabledRule.FeatureSet.Guid, out var licenseState);
+                    var specialExpiry = licenseState?.Expiration;
+                    enabled = enabledRule.EnableFeatureByDefault;
+                    licenseEnabled = true; // The license is active, so it's allowed to enable this
+                    msgShort = enabledRule.FeatureSet.Name;
+                    message = $" by default with license {enabledRule.FeatureSet.Name}";
+                    expiry = specialExpiry ?? BuiltInLicenses.UnlimitedExpiry;
+                }
 
-            return new FeatureState(f, expiry, enabled, msgShort, (enabled ? "Enabled" : "Disabled") + message,
-                licenseEnabled, enabledByDefault: enabledRule?.EnableFeatureByDefault ?? false, enabledInConfiguration: inConfig?.Enabled);
-        }).ToList();
+                // Check if the configuration would enable this feature
+                var inConfig = config?.Features.FirstOrDefault(cf => cf.Id == f.Guid);
+                if (inConfig != null)
+                {
+                    enabled = licenseEnabled && inConfig.Enabled;
+                    if (expiry == DateTime.MinValue)
+                        expiry = inConfig.Expires; // set expiry by configuration (when is not set by license)
+                    msgShort = licenseEnabled ? "configuration" : "unlicensed";
+                    message = licenseEnabled ? " by configuration" : " - requires license";
+                }
+
+                return new FeatureState(f, expiry, enabled, msgShort, (enabled ? "Enabled" : "Disabled") + message,
+                    licenseEnabled, enabledByDefault: enabledRule?.EnableFeatureByDefault ?? false,
+                    enabledInConfiguration: inConfig?.Enabled);
+            })
+            .ToList();
 
         // Find additional, un matching features which are not known in the catalog
         var missingFeatures = config?.Features
             .Where(f => featuresCat.All(fd => fd.Guid != f.Id))
-            .Select(f => new FeatureState(new(f.Id), f.Expires, f.Enabled, "configuration", "Configured manually", 
-                allowedByLicense: false, enabledByDefault: false,  enabledInConfiguration: f.Enabled));
+            .Select(f => new FeatureState(
+                    new(f.Id),
+                    f.Expires,
+                    f.Enabled,
+                    "configuration",
+                    "Configured manually",
+                    allowedByLicense: false,
+                    enabledByDefault: false,
+                    enabledInConfiguration: f.Enabled
+                )
+            );
 
-        var final = (missingFeatures == null ? allFeats : allFeats.Union(missingFeatures)).ToList();
-        if (sysFeatureStates.SafeAny()) final = final.Union(sysFeatureStates).ToList();
+        var final = (missingFeatures == null
+                ? allFeats
+                : allFeats.Union(missingFeatures))
+            .ToList();
+
+        if (sysFeatureStates.SafeAny())
+            final = final.Union(sysFeatureStates).ToList();
         return final;
     }
 
@@ -164,7 +176,8 @@ public class EavFeaturesService: IEavFeaturesService
     [PrivateApi]
     public long CacheTimestamp { get; private set; }
 
-    public bool CacheChanged(long dependentTimeStamp) => CacheTimestamp != dependentTimeStamp;
+    public bool CacheChanged(long dependentTimeStamp)
+        => CacheTimestamp != dependentTimeStamp;
 
     // Custom event for LightSpeed
     // TODO: @STV 2024-05-28 - this looks wrong, since it's only on the instance.
