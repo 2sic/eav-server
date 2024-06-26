93155c9e10a5e7eff86642e17cff76e59b507f81
iJungleboy <daniel.mettler@2sic.com>
2024-06-26 10:41:45 +0200

    Update FeatureDto.cs

--- ToSic.Eav.WebApi/Context/FeatureDto.cs
+++ ToSic.Eav.WebApi/Context/FeatureDto.cs
@@ -4,6 +4,7 @@ namespace ToSic.Eav.WebApi.Context;
 
 public class FeatureDto(FeatureState state)
 {
+    [JsonPropertyName("nameId")]
     public string NameId => state.NameId;
 
     [JsonPropertyName("isEnabled")]
