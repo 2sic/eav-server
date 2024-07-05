82448c139285a5e2f93f111135cfc60952dd8d6a
iJungleboy <daniel.mettler@2sic.com>
2024-07-05 17:52:41 +0200

    Update ExtensionToListOfOne.cs
    minor

--- ToSic.Eav.Core/Plumbing/Linq/ExtensionToListOfOne.cs
+++ ToSic.Eav.Core/Plumbing/Linq/ExtensionToListOfOne.cs
@@ -5,4 +5,7 @@ public static class ExtensionToListOfOne
 {
     [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
     public static List<T> ToListOfOne<T>(this T original) => [original];
+
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    public static List<T> ToListOfOneOrNone<T>(this T original) => original == null ? [] : [original];
 }
