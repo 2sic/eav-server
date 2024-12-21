8d388cc8ccfe5b091bda838a05978a2052bfd497
iJungleboy <daniel.mettler@2sic.com>
2024-12-21 11:00:03 +0100

    docs IFile.cs

--- ToSic.Eav.Apps/Eav.Apps/Assets/IFile.cs
+++ ToSic.Eav.Apps/Eav.Apps/Assets/IFile.cs
@@ -1,48 +1,63 @@
 ﻿namespace ToSic.Eav.Apps.Assets;
 
 /// <summary>
-/// Describes a file asset as provided by the underlying environment (like DNN)
+/// Describes a file asset as provided by the underlying environment (like DNN and Oqtane).
 /// </summary>
+/// <remarks>
+/// All APIs retrieving such a file always begin in either an App or ADAM context.
+/// So any relative path information begins there, as these paths are usually meant to be used either relative to that location,
+/// or for generating URLs.
+/// </remarks>
 [PublicApi]
 public interface IFile: IAsset
 {
     /// <summary>
-    /// The file extension of the real underlying file.
+    /// The file extension of the real underlying file, without the leading dot.
     /// </summary>
     /// <returns>
-    /// The extension, like "pdf" or "jpg"
+    /// The extension, like "pdf" or "jpg".
+    /// `pdf` for `C:\Inetpub\wwwroot\www.2sic.com\Portals\0\2sxc\content\assets\docs\terms\file.pdf`
     /// </returns>
     string Extension { get; }
 
     /// <summary>
-    /// The folder of the file
+    /// The full folder of the file beginning from the root (App or ADAMA), with trailing slash.
     /// </summary>
-    /// <returns>The folder name </returns>
-    // ReSharper disable once UnusedMemberInSuper.Global
+    /// <returns>
+    /// The folder name.
+    /// `/assets/docs/terms/` for `C:\Inetpub\wwwroot\www.2sic.com\Portals\0\2sxc\content\assets\docs\terms\file.pdf`
+    /// </returns>
     string Folder { get; }
 
     /// <summary>
     /// The folder ID of the file, if the underlying environment uses int IDs
     /// </summary>
-    /// <returns>an int with the id used by the environment to track this item</returns>
-    // ReSharper disable once UnusedMemberInSuper.Global
+    /// <returns>
+    /// The id used by the environment to track this item.
+    /// `19350` for `C:\Inetpub\wwwroot\www.2sic.com\Portals\0\2sxc\content\assets\docs\terms\file.pdf`
+    /// </returns>
     int FolderId { get; }
 
     /// <summary>
     /// The full file name of the original file
     /// </summary>
-    /// <returns>The full file name with extension.</returns>
+    /// <returns>
+    /// The full file name with extension.
+    /// `file.pdf` for `C:\Inetpub\wwwroot\www.2sic.com\Portals\0\2sxc\content\assets\docs\terms\file.pdf`
+    /// </returns>
     string FullName { get; }
 
     /// <summary>
     /// The file size of the file, IF the underlying environment provides this.
     /// </summary>
-    /// <returns>the size in bytes</returns>
-    // ReSharper disable once UnusedMemberInSuper.Global
+    /// <returns>
+    /// The size in bytes.
+    /// `18273` for `C:\Inetpub\wwwroot\www.2sic.com\Portals\0\2sxc\content\assets\docs\terms\file.pdf`
+    /// </returns>
     int Size { get; }
 
     /// <summary>
-    /// Size information for files
+    /// Size information object for files with specific properties to get the size in bytes, kilobytes, megabytes, etc.
     /// </summary>
     /// <remarks>
     /// Added in v14.04
