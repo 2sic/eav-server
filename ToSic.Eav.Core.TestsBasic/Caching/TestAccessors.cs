2d35d3a44261f74d0c37963495d178c8604d608c
iJungleboy <daniel.mettler@2sic.com>
2025-03-06 10:29:17 +0100

    refactoring unit tests to xunit

---
+++ ToSic.Eav.Core.TestsBasic/Caching/TestAccessors.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Caching;
+using ToSic.Eav.Caching;
+
+namespace ToSic.Eav.Tests.Caching;
+
+internal static class TestAccessors
+{
+    internal static CacheItemPolicy CreateResultTac(this IPolicyMaker policyMaker)
+        => policyMaker.CreateResult();
+}
