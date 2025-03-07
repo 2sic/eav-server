924aaaad74daffc1ed836841b84592819667df5a
iJungleboy <daniel.mettler@2sic.com>
2025-03-07 11:26:52 +0100

    minor move tests

---
+++ ToSic.Eav.ImportExport.Tests/Persistence.File/TypesBase.cs
@@ -0,0 +1,8 @@
+﻿namespace ToSic.Eav.Core.Tests.Types;
+
+/// <summary>
+/// Note: this seem like an old test which tried to implement code-based-content types.
+/// As of 2025-02 and probably 2 years already, it probably doesn't do anything.
+/// We moved it near the only place it's used, but the test is probably obsolete by all means.
+/// </summary>
+public abstract class TypesBase;
