6a9e85f9bb90bda876f95dc7f6d8297aae3ebe01
iJungleboy <daniel.mettler@2sic.com>
2025-03-07 11:15:32 +0100

    fix sha1 test

--- ToSic.Eav.Core.TestsBasic/Security.Encryption/SignatureHashSha1.cs
+++ ToSic.Eav.Core.TestsBasic/Security.Encryption/SignatureHashSha1.cs
@@ -2,49 +2,48 @@
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Xunit.Abstractions;
 
 namespace ToSic.Eav.Core.Tests.Signature;
 
-public class SignatureHashSha1
+public class SignatureHashSha1(ITestOutputHelper output)
 {
-
-    [Fact]
-    public void SignatureSha1ValidationTest()
+    [Theory]
+    [InlineData(TestData.Json)]
+    public void SignatureSha1ValidationTest(string json)
     {
-        var Json = TestData.Json;
-
         var encoding = new UnicodeEncoding();
-        var data = encoding.GetBytes(Json);
+        var data = encoding.GetBytes(json);
 
         // get hash (SHA1 IS LESS SECURE)
-        var sha1 = new SHA1Managed();
-        var hash = sha1.ComputeHash(data);
+        var hash = SHA1.Create().ComputeHash(data);
 
         // sign hash
         var privateCert = new X509Certificate2(Convert.FromBase64String(TestKeys.SelfSigned2.Private), "", X509KeyStorageFlags.PersistKeySet);
-        var csp = (RSACryptoServiceProvider)privateCert.PrivateKey;
-        var signature = csp.SignHash(hash, "SHA1");
+
+        var csp = privateCert.GetRSAPrivateKey();
+        var signature = csp.SignHash(hash, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
         var signatureBase64 = Convert.ToBase64String(signature);
-        Trace.WriteLine($"server: signature Base64 {signatureBase64}");
+        output.WriteLine($"server: signature Base64 {signatureBase64}");
 
 
-        Trace.WriteLine("client:  Verify signature... SHA1 IS LESS SECURE");
+        output.WriteLine("client:  Verify signature... SHA1 IS LESS SECURE");
 
 
         var encodingClient = new UnicodeEncoding();
-        var dataClient = encodingClient.GetBytes(Json);
+        var dataClient = encodingClient.GetBytes(json);
         var signatureClient = Convert.FromBase64String(signatureBase64);
 
         // get hash
-        var sha1Client = new SHA1Managed();
-        var hashClient = sha1Client.ComputeHash(dataClient);
+        var hashClient = SHA1.Create().ComputeHash(dataClient);
 
         // verify the signature with the hash
         var publicCert = new X509Certificate2(Convert.FromBase64String(TestKeys.SelfSigned2.Public), "");
-        var cspPublic = (RSACryptoServiceProvider)publicCert.PublicKey.Key;
-        var isValidSignature = cspPublic.VerifyHash(hashClient, "SHA1", signatureClient);
-        Trace.WriteLine($"client:  Signature valid {isValidSignature}");
 
-        Assert.Equal(true, isValidSignature);
+        var cspPublic = publicCert.GetRSAPublicKey();
+        var isValidSignature = cspPublic.VerifyHash(hashClient, signatureClient, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
+        output.WriteLine($"client:  Signature valid {isValidSignature}");
+
+        Assert.True(isValidSignature);
     }
 }
