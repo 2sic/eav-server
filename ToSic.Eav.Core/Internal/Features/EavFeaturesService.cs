d14aa3eb730a8881f041daefe46e638e930784e4
Tonći Vatavuk <tonci@sistemi.hr>
2025-03-11 13:30:28 +0100

    fix https://github.com/2sic/2sxc/issues/3597

--- ToSic.Eav.Core/Internal/Features/EavFeaturesService.cs
+++ ToSic.Eav.Core/Internal/Features/EavFeaturesService.cs
@@ -17,7 +17,8 @@ namespace ToSic.Eav.Internal.Features;
 /// REASON is probably cache connection, since it should notify the system to release caches as settings change?
 ///
 /// 2024-05-31 changing to non-singleton, must monitor if all is ok...
-/// 2025-03-11 updated the `CacheTimestamp` property to use a backing field `_staticCacheTimestamp`, so WebFarmCache is working when feature is enabled
+/// 2025-03-11 updated the `CacheTimestamp` property to use a backing field `_staticCacheTimestamp`, 
+///            so WebFarmCache is working when feature is enabled https://github.com/2sic/2sxc/issues/3597
 /// </remarks>
 [PrivateApi("hide implementation")]
 [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
@@ -181,7 +182,7 @@ public class EavFeaturesService(FeaturesCatalog featuresCatalog) : IEavFeaturesS
     }
 
     [PrivateApi]
-    public long CacheTimestamp => _staticCacheTimestamp;
+    public long CacheTimestamp => _staticCacheTimestamp; // fix https://github.com/2sic/2sxc/issues/3597
     private static long _staticCacheTimestamp; // need static to preserve value across transient instances
 
     public bool CacheChanged(long dependentTimeStamp)
