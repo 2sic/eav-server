edadec8504ba46d37b7093538d9c3ac3e60fed92
iJungleboy <daniel.mettler@2sic.com>
2025-03-11 15:33:46 +0100

    xunit move another test

--- ToSic.Eav.DataSource.Tests/DataSources.DataTable/DataTableTest.cs
+++ ToSic.Eav.DataSource.Tests/DataSources.DataTable/DataTableTest.cs
@@ -1,21 +1,22 @@
 ﻿using ToSic.Eav.Data.Build;
-using ToSic.Eav.TestData;
+using ToSic.Eav.DataSourceTests;
+using ToSic.Eav.StartupTests;
 
-namespace ToSic.Eav.DataSourceTests.ExternalData;
+namespace ToSic.Eav.DataSources.DataTable;
 
-[TestClass]
-public class DataTableTst: TestBaseEavDataSource
+[Startup(typeof(StartupTestsEavCoreAndDataSources))]
+public class DataTableTest(DataTablePerson dataTablePerson, DataSourcesTstBuilder dsSvc, DataBuilder dataBuilder)
 {
 
-    [TestMethod]
+    [Fact]
     public void DataSource_Create_GeneralTest()
     {
         const int itemsToGenerate = 499;
-        var ds = new DataTablePerson(this).Generate(itemsToGenerate);
-        IsTrue(ds.InTac().Count == 0, "In count should be 0");
-        IsTrue(ds.Out.Count == 1, "Out count should be 1");
+        var ds = dataTablePerson.Generate(itemsToGenerate);
+        True(ds.InTac().Count == 0, "In count should be 0");
+        True(ds.Out.Count == 1, "Out count should be 1");
         var defaultOut = ds[DataSourceConstants.StreamDefaultName];
-        IsTrue(defaultOut != null);
+        True(defaultOut != null);
         try
         {
             // ReSharper disable once UnusedVariable
@@ -23,32 +24,32 @@ public class DataTableTst: TestBaseEavDataSource
             Fail("Access to another out should fail");
         }
         catch { }
-        IsTrue(defaultOut.ListTac().Count() == itemsToGenerate);
+        True(defaultOut.ListTac().Count() == itemsToGenerate);
     }
 
-    [TestMethod]
+    [Fact]
     public void DataTable_CacheKey()
     {
         const int itemsToGenerate = 499;
-        var ds = new DataTablePerson(this).Generate(itemsToGenerate);
+        var ds = dataTablePerson.Generate(itemsToGenerate);
 
         //var expKey = "DataTable:NoGuid&TitleField=FullName&EntityIdField=EntityId&ModifiedField=InternalModified&ContentType=Person";
         var expKey = "DataTable:NoGuid&ContentType=Person&EntityIdField=entityid&ModifiedField=InternalModified&TitleField=FullName";
-        AreEqual(expKey, ds.CachePartialKey);
-        AreEqual(expKey, ds.CacheFullKey);
+        Equal(expKey, ds.CachePartialKey);
+        Equal(expKey, ds.CacheFullKey);
         var lastRefresh = ds.CacheTimestamp; // get this before comparison, because sometimes slow execution will get strange results
-        IsTrue(DateTime.Now.Ticks >= lastRefresh, "Date-check of cache refresh");
+        True(DateTime.Now.Ticks >= lastRefresh, "Date-check of cache refresh");
     }
 
-    [TestMethod]
+    [Fact]
     public void DataTable_DefaultTitleField()
     {
         const int itemsToGenerate = 25;
-        var ds = new DataTableTrivial(this.GetService<DataSourcesTstBuilder>(), GetService<DataBuilder>()).Generate(itemsToGenerate);
+        var ds = new DataTableTrivial(dsSvc, dataBuilder).Generate(itemsToGenerate);
 
-        AreEqual(25, ds.ListTac().Count());
+        Equal(25, ds.ListTac().Count());
         var first = ds.ListTac().FirstOrDefault();
-        AreEqual("Daniel Mettler", first.GetBestTitle());
+        Equal("Daniel Mettler", first.GetBestTitle());
     }
 
 
