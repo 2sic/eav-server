1e508c7105da7bd59f7697b371469e54c3ea1ff2
iJungleboy <daniel.mettler@2sic.com>
2025-03-11 10:29:15 +0100

    move fingerprint tests

---
+++ ToSic.Eav.StartupTests/Fingerprint/FingerprintTest.cs
@@ -0,0 +1,18 @@
+﻿using ToSic.Eav.Security.Fingerprint;
+using ToSic.Eav.Testing;
+
+namespace ToSic.Eav.Fingerprint;
+
+[Startup(typeof(StartupTestFullWithDb))]
+public class FingerprintTest(SystemFingerprint fingerprintSvc, SystemFingerprint fingerprintSvc2) : IClassFixture<FullDbFixtureScenarioBasic>
+{
+    [Fact]
+    public void HasFingerprint() => NotNull(fingerprintSvc.GetFingerprint());
+
+    [Fact]
+    public void FingerprintIsLong() => True(fingerprintSvc.GetFingerprint().Length > 10);
+
+    [Fact]
+    public void FingerprintsMatch() => Equal(fingerprintSvc.GetFingerprint(), fingerprintSvc2.GetFingerprint());
+
+}
