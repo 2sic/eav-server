4d1f38e48fb1f54529c753946dd32eab5de09d6d
Tonći Vatavuk <tonci@sistemi.hr>
2025-03-19 10:35:35 +0100

    Modified the `GetTestPath` method to use `CodeBase` for .NET Framework and `Location` for other frameworks, ensuring compatibility across different .NET environments.

--- ToSic.Eav.Core.TestHelpers/Testing/TestFiles.cs
+++ ToSic.Eav.Core.TestHelpers/Testing/TestFiles.cs
@@ -6,7 +6,11 @@ public static class TestFiles
 {
     public static string GetTestPath(string relativePath)
     {
-        var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().Location);
+#if NETFRAMEWORK
+        var codeBaseUrl = new Uri(uriString: Assembly.GetExecutingAssembly().CodeBase);
+#else
+        var codeBaseUrl = new Uri(uriString: Assembly.GetExecutingAssembly().Location);
+#endif
         var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
         var dirPath = Path.GetDirectoryName(codeBasePath);
         return Path.Combine(dirPath, relativePath);
