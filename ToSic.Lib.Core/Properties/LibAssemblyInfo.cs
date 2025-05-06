5e08939db6dd61d218df6ab2ba747875e0d9f9f9
iJungleboy <daniel.mettler@2sic.com>
2025-05-06 12:10:37 +0200

    Eav.Lib - add assembly info, copyright etc.

---
+++ ToSic.Lib.Core/Properties/LibAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace ToSic.Lib.Properties;
+
+/// <summary>
+/// Contains information for all assemblies to use
+/// </summary>
+public static class LibAssemblyInfo
+{
+    // Important: These must be constants!
+    // This is because the version etc. may be compiled into Oqtane DLLs
+    // which will run in the browser, and shouldn't have to include these DLLs to work
+    // If it's a constant, the value will be added to the compiled code, so no real dependency will exist at runtime
+    public const string AssemblyVersion = "19.99.00";
+    public const string Company = "2sic internet solutions GmbH, Switzerland";
+    public const string EavProduct = "2sic Lib Foundation";
+    public const string EavCopyright = "Copyright MIT © 2sic 2025";
+}
