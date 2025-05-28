ff16bcb5cca761fde7926559eca42d04f8ba73e5
iJungleboy <daniel.mettler@2sic.com>
2025-05-28 07:46:32 +0200

    Move everything from old ImportExport to Eav.Persistence

--- ToSic.Eav.Data.Persistence/GlobalUsings.cs
+++ ToSic.Eav.Data.Persistence/GlobalUsings.cs
@@ -1,4 +1,17 @@
 // Global using directives
 
+global using System;
+global using System.Collections.Generic;
+global using System.Text.Json.Serialization;
+global using ToSic.Eav.Apps;
+global using ToSic.Eav.Data;
+global using ToSic.Eav.Metadata;
+global using ToSic.Eav.Plumbing;
+global using ToSic.Eav.Serialization;
+global using ToSic.Lib.Documentation;
 global using ToSic.Lib.Documentation;
 global using ToSic.Lib.Logging;
+global using ToSic.Lib.Logging;
+global using ToSic.Lib.Services;
+global using ToSic.Sys.Utils;
+global using static System.Text.Json.Serialization.JsonIgnoreCondition;
---
+++ ToSic.Eav.Data.Persistence/Properties/Eav.Persistence.InternalsVisibleTo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ToSic.Eav.Apps")]
+[assembly: InternalsVisibleTo("ToSic.Eav.ImportExport.Tests")]
+[assembly: InternalsVisibleTo("ToSic.Eav.ImportExport.Tests19")]
