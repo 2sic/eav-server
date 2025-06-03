455494ab44295fc826991bcfc76d192c5b167802
iJungleboy <daniel.mettler@2sic.com>
2025-06-03 18:18:40 +0200

    Move all Core.Data.Raw to Eav.DataBuilder project

--- ToSic.Eav.Data.Build/Data.Raw/Sys/IHasRawEntity.cs
+++ ToSic.Eav.Data.Build/Data.Raw/Sys/IHasRawEntity.cs
@@ -7,6 +7,9 @@
 /// <typeparam name="T"></typeparam>
 /// <remarks>
 /// Added in 15.04
+/// Note: because of changes in v19 and v20, this doesn't seem to be in use ATM.
+/// There is still code which would convert objects having this, but ATM it seems to be unused.
+/// It's unclear if we will pick this up again, for now better keep it internal as we may remove it again. 
 /// </remarks>
 [InternalApi_DoNotUse_MayChangeWithoutNotice("WIP for DataSources")]
 [ShowApiWhenReleased(ShowApiMode.Never)]
