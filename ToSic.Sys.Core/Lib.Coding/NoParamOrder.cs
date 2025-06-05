c4ab248cf234d537058a89e6087c81c6f0b33063
iJungleboy <daniel.mettler@2sic.com>
2025-06-05 06:27:09 +0200

    Update NoParamOrder.cs
    minor docs

--- ToSic.Sys.Core/Lib.Coding/NoParamOrder.cs
+++ ToSic.Sys.Core/Lib.Coding/NoParamOrder.cs
@@ -2,17 +2,22 @@
 
 /// <summary>
 /// Special placeholder to indicate that all parameters following this should be named.
+/// </summary>
+/// <remarks>
 /// It is really important that all parameters following this are named, since the API won't guarantee the order of the parameter names.
 ///
-/// Example: This signature: `...Save(string name, NoParamOrder noParamOrder = default, string title = title, string description = default)`
+/// Example: This signature:
+///
+/// `Something.Save(string name, NoParamOrder noParamOrder = default, string title = title, string description = default)`
+/// 
 /// allows:
 /// 
-/// 1. `Save("MyName", title: "MyTitle")`
-/// 2. `Save("MyName", description: "MyDescription")`
-/// 3. Save("MyName") - without additional parameters
+/// 1. `Something.Save("MyName", title: "MyTitle")`
+/// 2. `Something.Save("MyName", description: "MyDescription")`
+/// 3. `Something.Save("MyName")` - without additional parameters
 /// 
 /// See [Convention: Named Parameters](https://go.2sxc.org/named-params).
-/// </summary>
+/// </remarks>
 [PublicApi]
 [ShowApiWhenReleased(ShowApiMode.Never)]
 public struct NoParamOrder
