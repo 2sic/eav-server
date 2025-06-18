9cd0b85b13c3da955ca09dfb8e62fdaff49caf66
iJungleboy <daniel.mettler@2sic.com>
2025-06-18 08:22:33 +0200

    Update Retry.cs

--- ToSic.Sys.Utils/Utils/Retry.cs
+++ ToSic.Sys.Utils/Utils/Retry.cs
@@ -1,7 +1,9 @@
 ﻿namespace ToSic.Sys.Utils;
+
 public static class Retry
 {
-    public static bool RetryOnException(Action action, ILogCall logCall, int repeat = 10, int delay = 100, bool silent = false)
+    public static bool RetryOnException(Action action, ILogCall logCall, int repeat = 10, int delay = 100,
+        bool silent = false)
     {
         var l = logCall.Fn<bool>($"{nameof(repeat)}: {repeat}; {nameof(delay)}: {delay}; {nameof(silent)}: {silent}");
         for (var i = 0; i < repeat; i++)
@@ -24,10 +26,13 @@ public static class Retry
                     l.Done("Failed after 10 tries, giving up.");
                     throw;
                 }
+
                 // otherwise do a short delay to try again
                 l.A($"Attempt {i} failed, delay and try again.");
                 Thread.Sleep(i * delay);
             }
         }
 
+        return l.ReturnFalse("unclear what happened, this should not occur");
     }
+}
