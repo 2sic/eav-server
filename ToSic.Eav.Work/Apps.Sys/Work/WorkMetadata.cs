16de7df0b78f1fb022ce013ba0dd5465eac0c491
iJungleboy <daniel.mettler@2sic.com>
2025-06-28 12:29:24 +0200

    fix minor bug updating the wrong metadata when input-type changes

--- ToSic.Eav.Work/Apps.Sys/Work/WorkMetadata.cs
+++ ToSic.Eav.Work/Apps.Sys/Work/WorkMetadata.cs
@@ -1,4 +1,5 @@
 ﻿using ToSic.Eav.Data.Build;
+using ToSic.Eav.Data.Sys.Entities;
 using ToSic.Eav.Metadata;
 using ToSic.Eav.Metadata.Targets;
 
@@ -13,15 +14,20 @@ public class WorkMetadata(
 {
     public void SaveMetadata(Target target, string typeName, Dictionary<string, object> values)
     {
-        var l = Log.Fn($"target:{target.KeyNumber}/{target.KeyGuid}, values count:{values.Count}");
+        var l = Log.Fn($"target:{target.KeyNumber}/{target.KeyGuid}, {nameof(typeName)}: '{typeName}', values count:{values.Count}");
         if (target.TargetType != (int)TargetTypes.Attribute || target.KeyNumber == null || target.KeyNumber == 0)
             throw new NotSupportedException("atm this command only creates metadata for entities with id-keys");
 
         // see if a metadata already exists which we would update
         var existingEntity = AppWorkCtx.AppReader.List
-            .FirstOrDefault(e => e.MetadataFor?.TargetType == target.TargetType && e.MetadataFor?.KeyNumber == target.KeyNumber);
+            .OfType(typeName)
+            .FirstOrDefault(e => e.MetadataFor.TargetType == target.TargetType && e.MetadataFor.KeyNumber == target.KeyNumber);
+
         if (existingEntity != null)
+        {
+            l.A($"Found and will update: {existingEntity.EntityId}");
             entityUpdate.New(AppWorkCtx).UpdateParts(existingEntity.EntityId, values, new());
+        }
         else
         {
             var appState = AppWorkCtx.AppReader;
