4aad6cb4d784f1eff0f01ed7e684108a7557a5f9
iJungleboy <daniel.mettler@2sic.com>
2025-07-30 14:43:24 +0200

    EF: minor code flow optimization

--- ToSic.Eav.Repository.Efc/Sys.DbEntityProcess/Process3New1LastChecks.cs
+++ ToSic.Eav.Repository.Efc/Sys.DbEntityProcess/Process3New1LastChecks.cs
@@ -8,17 +8,16 @@ internal class Process3New1LastChecks() : Process0Base("DB.EPr3n1")
         if (!data.IsNew)
             return l.Return(data, "not new, not my job");
 
-        if (data.NewEntity.EntityGuid == Guid.Empty)
-        {
-            if (data.LogDetails)
-                l.A("New entity guid was null, will throw exception");
-            return l.Return(data with
-            {
-                Exception = new ArgumentException("can't create entity in DB with guid null - entities must be fully prepared before sending to save"),
-            });
-        }
+        if (data.NewEntity.EntityGuid != Guid.Empty)
+            return l.Return(data);
 
-        return l.Return(data);
+        if (data.LogDetails)
+            l.A("New entity guid was null, will throw exception");
+        return l.Return(data with
+        {
+            Exception = new ArgumentException("can't create entity in DB with guid null - entities must be fully prepared before sending to save"),
+        });
+
     }
 
 }
