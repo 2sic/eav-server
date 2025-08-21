2374c79d6a03795a3a2f97375a11e1fe50cd52d0
iJungleboy <daniel.mettler@2sic.com>
2025-08-21 13:14:53 +0200

    Update System.Runtime.CompilerServices.cs
    minor code style / comments / grouping

--- SharedImports/Code/System.Runtime.CompilerServices.cs
+++ SharedImports/Code/System.Runtime.CompilerServices.cs
@@ -1,21 +1,28 @@
 ﻿// ReSharper disable once CheckNamespace
+
+#region Enable Init Properties
+
 namespace System.Runtime.CompilerServices
 {
 
     // enable C# 9 init-only properties
     // https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
     internal static class IsExternalInit;
+}
+
+#endregion
 
 
-    #region required feature (for records / init)
+#region required feature (for records / init)
 
+namespace System.Runtime.CompilerServices
+{
     public class RequiredMemberAttribute : Attribute;
 
     public class CompilerFeatureRequiredAttribute : Attribute
     {
         public CompilerFeatureRequiredAttribute(string name)
-        {
-        }
+        { }
     }
 }
 
