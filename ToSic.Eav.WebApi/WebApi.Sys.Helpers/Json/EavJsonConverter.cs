d3e893776d08464a23faad7692927c8d1c73d53f
iJungleboy <daniel.mettler@2sic.com>
2025-11-03 22:50:28 +0100

    json serialization problem - needs help @tvatavuk

--- ToSic.Eav.WebApi/WebApi.Sys.Helpers/Json/EavJsonConverter.cs
+++ ToSic.Eav.WebApi/WebApi.Sys.Helpers/Json/EavJsonConverter.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using System.Web;
 using ToSic.Eav.DataFormats.EavLight;
 
 namespace ToSic.Eav.WebApi.Sys.Helpers.Json;
@@ -14,4 +16,40 @@ public class EavJsonConverter(IConvertToEavLight convertToEavLight) : JsonConver
         var eavLightEntity = convertToEavLight.Convert(entity);
         JsonSerializer.Serialize(writer, eavLightEntity, eavLightEntity.GetType(), options);
     }
+}
+
+[ShowApiWhenReleased(ShowApiMode.Never)]
+public class EavJsonConverterHttpEntry(IConvertToEavLight convertToEavLight) : JsonConverter<IEntity>
+{
+    public override IEntity Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => throw new NotImplementedException();
+
+    public override void Write(Utf8JsonWriter writer, IEntity entity, JsonSerializerOptions options)
+    {
+        var eavLightEntity = GetCurrentConverter().Convert(entity);
+        JsonSerializer.Serialize(writer, eavLightEntity, eavLightEntity!.GetType(), options);
+    }
+
+    private IConvertToEavLight GetCurrentConverter()
+    {
+        return convertToEavLight;
+
+        // @STV need your help here
+#if !NETCOREAPP
+        if (_currentConverter != null)
+            return _currentConverter;
+        // 1. Access the current HttpContext
+        var httpContext = HttpContext.Current;
+        if (httpContext == null)
+            return _currentConverter = convertToEavLight;
+
+        var scope = httpContext.Items[typeof(IServiceScope)] as IServiceScope;
+        var sp = scope?.ServiceProvider;
+        if (sp == null)
+            return _currentConverter = convertToEavLight;
+        return _currentConverter = sp.Build<IConvertToEavLight>();
+#endif
+    }
+
+    private IConvertToEavLight? _currentConverter;
 }
--- ToSic.Eav.WebApi/WebApi.Sys.Helpers/Json/EavJsonConverterFactory.cs
+++ ToSic.Eav.WebApi/WebApi.Sys.Helpers/Json/EavJsonConverterFactory.cs
@@ -1,20 +1,51 @@
 ﻿using System.Collections;
 using System.Text.Json;
+using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+
+#if NETCOREAPP
+using Microsoft.AspNetCore.Http;
+#endif
 
 namespace ToSic.Eav.WebApi.Sys.Helpers.Json;
 
 [ShowApiWhenReleased(ShowApiMode.Never)]
 public class EavJsonConverterFactory(
     EavJsonConverter eavJsonConverter,
-    EavCollectionJsonConverter eavCollectionJsonConverter)
+    EavCollectionJsonConverter eavCollectionJsonConverter
+#if NETCOREAPP
+    , IHttpContextAccessor htppContextAccessor
+#endif
+        )
     : JsonConverterFactory
 {
     public override bool CanConvert(Type typeToConvert) =>
         typeof(IEntity).IsAssignableFrom(typeToConvert)
         || typeof(IEnumerable<IEntity>).IsAssignableFrom(typeToConvert);
 
-    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) =>
-        typeof(IEnumerable).IsAssignableFrom(typeToConvert)
+    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        // @STV need your help here
+//#if !NETCOREAPP
+//        // 1. Access the current HttpContext
+//        var httpContext = HttpContext.Current;
+
+//        // TEMP - should move to DNN if this works
+//        // 2. Use the HttpContext to resolve dependencies - if possible
+//        if (httpContext != null)
+//        {
+//            var scope = httpContext.Items[typeof(IServiceScope)] as IServiceScope;
+//            var sp = scope?.ServiceProvider;
+//            if (sp != null)
+//                return typeof(IEnumerable).IsAssignableFrom(typeToConvert)
+//                    ? sp.Build<EavCollectionJsonConverter>()
+//                    //: sp.Build<EavJsonConverter>();
+//                    : sp.Build<EavJsonConverterHttpEntry>();
+//        }
+//#endif
+
+        return typeof(IEnumerable).IsAssignableFrom(typeToConvert)
             ? eavCollectionJsonConverter
             : eavJsonConverter;
+    }
 }
