c05d0605b395c226de068a8db434f7fa7b7ffd03
iJungleboy <daniel.mettler@2sic.com>
2025-11-11 19:05:50 +0100

    minor

--- ToSic.Sys.Utils/Utils/Generic Extensions/DictionaryExtensions.cs
+++ ToSic.Sys.Utils/Utils/Generic Extensions/DictionaryExtensions.cs
@@ -70,7 +70,8 @@ public static class DictionaryExtensions
         if (!source.TryGetValue(key, out var innerResult))
             return false;
         //if (innerResult == null) return false;
-        if (innerResult is not TResult typed) return false;
+        if (innerResult is not TResult typed)
+            return false;
         result = typed;
         return true;
     }
