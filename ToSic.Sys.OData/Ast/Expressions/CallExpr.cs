1a7218a1d56d1cb2e7ffee76d294a8a853499b74
iJungleboy <daniel.mettler@2sic.com>
2025-12-02 12:54:46 +0100

    OData code style

--- ToSic.Sys.OData/Ast/Expressions/CallExpr.cs
+++ ToSic.Sys.OData/Ast/Expressions/CallExpr.cs
@@ -1,14 +1,10 @@
 namespace ToSic.Sys.OData.Ast;
 
-public sealed class CallExpr : Expr
+public sealed class CallExpr(string name, IReadOnlyList<Expr> args) : Expr
 {
-    public string Name { get; }
-    public IReadOnlyList<Expr> Arguments { get; }
+    public string Name { get; } = name;
 
-    public CallExpr(string name, IReadOnlyList<Expr> args)
-    {
-        Name = name; Arguments = args;
-    }
+    public IReadOnlyList<Expr> Arguments { get; } = args;
 
     public override string ToString() => $"{Name}({string.Join(", ", Arguments)})";
 }
