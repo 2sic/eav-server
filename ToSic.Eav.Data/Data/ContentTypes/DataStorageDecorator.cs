f6fc38116eb87835b12f024e499ec4af2a24c9c0
iJungleboy <daniel.mettler@2sic.com>
2026-02-12 20:08:48 +0100

    Create DataStorageDecorator.cs
    To configure how data of content-types can be stored

---
+++ ToSic.Eav.Data/Data/ContentTypes/DataStorageDecorator.cs
@@ -0,0 +1,14 @@
+﻿using ToSic.Eav.Models;
+
+namespace ToSic.Eav.Data.ContentTypes;
+
+public record DataStorageDecorator: ModelOfEntityCore
+{
+    public string StoreType => GetThis("");
+
+    public bool SaveIsDisabled => GetThis(false);
+
+    public long ItemsMax => GetThis(long.MaxValue);
+
+    public string DataProcessingHandler => GetThis("");
+}
