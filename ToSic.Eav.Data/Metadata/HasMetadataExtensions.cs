ad0943a7abe6a8c090243e89111e1d6b39b11089
iJungleboy <daniel.mettler@2sic.com>
2026-02-12 20:05:01 +0100

    Introduce Model retrieval off of metadata

---
+++ ToSic.Eav.Data/Metadata/HasMetadataExtensions.cs
@@ -0,0 +1,15 @@
+﻿using ToSic.Eav.Models;
+
+namespace ToSic.Eav.Metadata;
+
+[PrivateApi]
+public static class HasMetadataExtensions
+{
+    public static TModel? TryGetMetadata<TModel>(
+        this IHasMetadata parent,
+        NoParamOrder npo = default,
+        ModelNullHandling nullHandling = ModelNullHandling.Undefined
+    )
+        where TModel : class, IModelSetup<IEntity>, new() =>
+        parent.Metadata.First<TModel>(nullHandling: nullHandling);
+}
