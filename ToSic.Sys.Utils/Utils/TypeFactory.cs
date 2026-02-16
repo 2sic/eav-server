22374ddecf5e9956f4ab41797f19176fb40e9eef
iJungleboy <daniel.mettler@2sic.com>
2026-02-16 15:59:49 +0100

    Introduce Sys.Utils.TypeFactory for faster type creation

---
+++ ToSic.Sys.Utils/Utils/TypeFactory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+using System.Linq.Expressions;
+
+namespace ToSic.Sys.Utils;
+
+/// <summary>
+/// Special helper to create objects from a type.
+/// </summary>
+/// <remarks>
+/// The type must have a parameterless constructor, otherwise it will throw an error.
+/// </remarks>
+[InternalApi_DoNotUse_MayChangeWithoutNotice]
+public static class TypeFactory
+{
+    /// <summary>
+    /// A thread-safe cache to store our compiled delegates.
+    /// These are important for fast creation without ongoing reflection.
+    /// </summary>
+    internal static readonly ConcurrentDictionary<Type, Func<object>> Cache = new();
+
+    /// <summary>
+    /// The Generic Wrapper: Uses the central cache but returns the correct type.
+    /// </summary>
+    public static T CreateInstance<T>() where T: class =>
+        (T)CreateInstance(typeof(T));
+
+    public static object CreateInstance(Type type)
+    {
+        // GetOrAdd ensures we only compile the expression once
+        var factory = Cache.GetOrAdd(type, t =>
+        {
+            // If it's a value type (struct), we use Activator or a specialized expression
+            // but for simplicity, here is the standard class implementation:
+            if (t.IsValueType)
+                return () => Activator.CreateInstance(t)!;
+
+            var constructor = t.GetConstructor(Type.EmptyTypes);
+            if (constructor == null)
+                throw new InvalidOperationException($"Type {t.Name} must have a public parameterless constructor.");
+
+            // Generate the IL equivalent of: () => new T()
+            var newExp = Expression.New(t);
+            var lambda = Expression.Lambda<Func<object>>(newExp);
+            return lambda.Compile();
+        });
+
+        return factory();
+    }
+}
