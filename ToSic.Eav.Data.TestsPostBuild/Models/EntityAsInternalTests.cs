c8b26e7e5b6ab25083537dcd05497b39743ba5e6
iJungleboy <daniel.mettler@2sic.com>
2026-02-16 16:27:35 +0100

    Model.AsInternal - unit tests with constructor check

---
+++ ToSic.Eav.Data.TestsPostBuild/Models/EntityAsInternalTests.cs
@@ -0,0 +1,27 @@
+﻿using ToSic.Eav.Metadata;
+using ToSic.Eav.Models.TestData;
+
+namespace ToSic.Eav.Models;
+
+public class EntityAsInternalTests(TestDataGenerator generator)
+{
+    [Fact]
+    public void AsInternalBasic()
+    {
+        var entity = generator.CreateMetadataForDecorator();
+        var model = entity.AsInternalTac<TestModelMetadataForDecorator>();
+        NotNull(model);
+        Equal((int)TargetTypes.Entity, model.TargetType);
+    }
+
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private record WithConstructor(string something) : TestModelMetadataForDecorator;
+
+    [Fact]
+    public void ThrowsWithoutEmptyConstructor() =>
+        Throws<InvalidOperationException>(() =>
+        {
+            var entity = generator.CreateMetadataForDecorator();
+            entity.AsInternalTac<WithConstructor>(skipTypeCheck: true);
+        });
+}
--- ToSic.Eav.Data.TestsPostBuild/Models/TestAccessors.cs
+++ ToSic.Eav.Data.TestsPostBuild/Models/TestAccessors.cs
@@ -5,6 +5,10 @@ namespace ToSic.Eav.Models;
 
 public static class TestAccessors
 {
+    internal static TModel? AsTac<TModel>(this IEntity? entity)
+        where TModel : class, IModelSetup<IEntity>, new()
+        => entity.As<TModel>();
+
     internal static TModel? AsTac<TModel>(
         this IEntity? entity,
         NoParamOrder npo = default,
@@ -15,6 +19,15 @@ public static class TestAccessors
         where TModel : class, IModelSetup<IEntity>, new()
         => entity.As<TModel>(npo, skipTypeCheck, nullHandling: nullHandling);
 
+    internal static TModel? AsInternalTac<TModel>(
+        this IEntity? entity,
+        NoParamOrder npo = default,
+        bool skipTypeCheck = false,
+        //bool nullIfNull = false
+        ModelNullHandling nullHandling = ModelNullHandling.Undefined
+    )
+        where TModel : class, IModelSetup<IEntity>//, new()
+        => entity.AsInternal<TModel>(npo, skipTypeCheck, nullHandling: nullHandling);
 
     internal static TModel? FirstTac<TModel>(
         this IEnumerable<IEntity>? list,
