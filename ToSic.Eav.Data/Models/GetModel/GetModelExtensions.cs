30c9f34593acb8aaa2099a976852728d29c441a1
iJungleboy <daniel.mettler@2sic.com>
2026-02-20 00:43:21 +0100

    models - minor

---
+++ ToSic.Eav.Data/Models/GetModel/GetModelExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ToSic.Eav.Metadata;
+
+namespace ToSic.Eav.Models;
+
+[WorkInProgressApi("beta v21")]
+public static class GetModelExtensions
+{
+    /// <summary>
+    /// Get a typed metadata from an object which has metadata. Will return `null` if no data found.
+    /// </summary>
+    /// <typeparam name="TModel">Any model object or interface.</typeparam>
+    /// <param name="parent">An object which has metadata.</param>
+    /// <param name="npo">see [](xref:NetCode.Conventions.NamedParameters)</param>
+    /// <returns></returns>
+    public static TModel? GetMetadata<TModel>(
+        this IHasMetadata parent,
+        NoParamOrder npo = default
+        //ModelNullHandling nullHandling = ModelNullHandling.Undefined
+    )
+        where TModel : class, IModelFromEntity
+    {
+        return parent.Metadata.First<TModel>(/*nullHandling: nullHandling*/);
+    }
+}
