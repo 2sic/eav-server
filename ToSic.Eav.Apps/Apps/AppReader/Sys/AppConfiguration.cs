7beecc71976c2ecfad72904cadad0dd3fbe3c66b
iJungleboy <daniel.mettler@2sic.com>
2026-03-12 07:35:34 +0100

    minor move app configuration constants

--- ToSic.Eav.Apps/Apps/AppReader/Sys/AppConfiguration.cs
+++ ToSic.Eav.Apps/Apps/AppReader/Sys/AppConfiguration.cs
@@ -9,39 +9,29 @@ namespace ToSic.Eav.Apps.AppReader.Sys;
 [ShowApiWhenReleased(ShowApiMode.Never)]
 internal record AppConfiguration : ModelFromEntityBasic, IAppConfiguration
 {
-    //public AppConfiguration(IEntity entity) : base(entity) { }
-
-    // todo: probably move most to Eav.Apps.AppConstants
-    [PrivateApi] public const string FieldAllowRazor = "AllowRazorTemplates";
-    [PrivateApi] public const string FieldAllowToken = "AllowTokenTemplates";
-    [PrivateApi] public const string FieldRequiredSxcVersion = "RequiredVersion";
-    [PrivateApi] public const string FieldRequiredDnnVersion = "RequiredDnnVersion";
-    [PrivateApi] public const string FieldRequiredOqtaneVersion = "RequiredOqtaneVersion";
-    [PrivateApi] public const string FieldSupportsAjax = "SupportsAjaxReload";
-
     public Version Version => GetVersionOrDefault(nameof(Version));
 
-    public string Name => Get(AppLoadConstants.FieldName, EavConstants.NullNameId);
+    public string Name => Get(AppConfigurationFields.FieldName, EavConstants.NullNameId);
 
     public string Description => GetThis("");
 
     public string Folder => GetThis("");
 
-    public bool EnableRazor => Get(FieldAllowRazor, false);
+    public bool EnableRazor => Get(AppConfigurationFields.FieldAllowRazor, false);
 
-    public bool EnableToken => Get(FieldAllowToken, false);
+    public bool EnableToken => Get(AppConfigurationFields.FieldAllowToken, false);
 
     public bool IsHidden => Get(AppLoadConstants.FieldHidden, false);
 
-    public bool EnableAjax => Get(FieldSupportsAjax, false);
+    public bool EnableAjax => Get(AppConfigurationFields.FieldSupportsAjax, false);
 
     public Guid OriginalId => Guid.TryParse(GetThis(""), out var result) ? result : Guid.Empty;
 
-    public Version RequiredSxc => GetVersionOrDefault(FieldRequiredSxcVersion);
+    public Version RequiredSxc => GetVersionOrDefault(AppConfigurationFields.FieldRequiredSxcVersion);
 
-    public Version RequiredDnn => GetVersionOrDefault(FieldRequiredDnnVersion);
+    public Version RequiredDnn => GetVersionOrDefault(AppConfigurationFields.FieldRequiredDnnVersion);
 
-    public Version RequiredOqtane => GetVersionOrDefault(FieldRequiredOqtaneVersion);
+    public Version RequiredOqtane => GetVersionOrDefault(AppConfigurationFields.FieldRequiredOqtaneVersion);
 
     private Version GetVersionOrDefault(string name) =>
         Version.TryParse(Get(name, ""), out var version) ? version : new();
