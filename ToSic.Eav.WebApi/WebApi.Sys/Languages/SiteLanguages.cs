73e299e83c9cba28a07a37f70875a00b8f640bac
giovi <npro9021@gmail.com>
2026-04-22 11:37:21 +0200

    feat: add SiteLanguages SysData datasource

---
+++ ToSic.Eav.WebApi/WebApi.Sys/Languages/SiteLanguages.cs
@@ -0,0 +1,51 @@
+﻿
+using ToSic.Eav.Data.Raw.Sys;
+using ToSic.Eav.DataSource;
+using ToSic.Eav.DataSource.VisualQuery;
+using ToSic.Eav.WebApi.Sys.Dto;
+
+namespace ToSic.Eav.WebApi.Sys.Languages;
+
+[PrivateApi]
+[VisualQuery(
+    NiceName = "Site Languages",
+    NameId = "9981db56-6d4e-4b29-914c-4a223c063eb4",
+    NameIds = ["System.SiteLanguages"], // Internal name for the system, used in some entity-pickers. Can change at any time.
+    Type = DataSourceType.System,
+    Audience = Audience.System,
+    DataConfidentiality = DataConfidentiality.Confidential,
+    UiHint = "Languages of the current site"
+)]
+public class SiteLanguages : CustomDataSource
+{
+    public SiteLanguages(Dependencies services, LanguagesBackend languagesBackend)
+        : base(services, logName: "Sxc.SiteLangs", connect: [languagesBackend])
+    {
+        ProvideOutRaw(
+            () => GetLanguages(languagesBackend),
+            options: () => new()
+            {
+                AutoId = true,
+                TitleField = nameof(SiteLanguageDto.Culture),
+                TypeName = "SiteLanguage",
+            });
+    }
+
+    private IEnumerable<IRawEntity> GetLanguages(LanguagesBackend languagesBackend)
+    {
+        var l = Log.Fn<IEnumerable<IRawEntity>>();
+
+        var list = languagesBackend
+            .GetLanguages()
+            .Select(language => new RawEntity(new()
+            {
+                { nameof(SiteLanguageDto.Code), language.Code },
+                { nameof(SiteLanguageDto.Culture), language.Culture },
+                { nameof(SiteLanguageDto.IsEnabled), language.IsEnabled },
+                { nameof(SiteLanguageDto.NameId), language.NameId },
+            }))
+            .ToList<IRawEntity>();
+
+        return l.Return(list, $"{list.Count}");
+    }
+}
