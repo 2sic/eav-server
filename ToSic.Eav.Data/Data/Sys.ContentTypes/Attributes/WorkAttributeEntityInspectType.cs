b8492ffcab8f7326acb0787ea926deb24465af0e
iJungleboy <daniel.mettler@2sic.com>
2026-06-16 06:47:17 +0200

    minor logging

--- ToSic.Eav.Data/Data/Sys.ContentTypes/Attributes/WorkAttributeEntityInspectType.cs
+++ ToSic.Eav.Data/Data/Sys.ContentTypes/Attributes/WorkAttributeEntityInspectType.cs
@@ -6,14 +6,16 @@ public class WorkAttributeEntityInspectType(): ServiceBase("Eav.AtInTy")
 {
     public string PrimaryTypeName(IContentTypeAttribute definition, bool create = true)
     {
+        var l = Log.Fn<string>($"attribute: {definition.Name}, create: {create}");
         // Make sure it's the right initial type
         if (!definition.IsEntity())
-            return "";
+            return l.Return("", "empty");
         
         // First check if it's a picker
         // In this case, the values are stored differently for create vs. data
         if (definition.IsPicker())
         {
+            l.A("is picker");
             var sources = definition
                 .GetPickerDataSources();
             var list = create
@@ -21,13 +23,13 @@ public class WorkAttributeEntityInspectType(): ServiceBase("Eav.AtInTy")
                 : sources.GetPickerDataTypeNames();
 
             if (list.Any())
-                return list.First();
+                return l.Return(list.First(), $"returning first: {list.First()}");
         }
         
         // Do basic check for non-pickers
         var itemTypeName = definition.Metadata.Get<string>(AttributeNames.EntityFieldType) ?? "";
         var typeName = itemTypeName.CsvToArrayWithoutEmpty().FirstOrDefault() ?? "";
-        return typeName;
+        return l.Return(typeName, $"{typeName}");
     }
 
 }
