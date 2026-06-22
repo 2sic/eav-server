a2efe65f3c2dee487b8cc72e74a7e6f617ad04d8
iJungleboy <daniel.mettler@2sic.com>
2026-06-22 14:08:56 +0200

    Improve get primary type of picker - with tryOtherModes

--- ToSic.Eav.Data/Data/Sys.ContentTypes/Attributes/WorkAttributeEntityInspectType.cs
+++ ToSic.Eav.Data/Data/Sys.ContentTypes/Attributes/WorkAttributeEntityInspectType.cs
@@ -4,9 +4,9 @@
 [ShowApiWhenReleased(ShowApiMode.Never)]
 public class WorkAttributeEntityInspectType(): ServiceBase("Eav.AtInTy")
 {
-    public string PrimaryTypeName(IContentTypeAttribute definition, bool create = true)
+    public string PrimaryTypeName(IContentTypeAttribute definition, bool modeCreate, bool tryOtherModes = false)
     {
-        var l = Log.Fn<string>($"attribute: {definition.Name}, create: {create}");
+        var l = Log.Fn<string>($"attribute: {definition.Name}, {nameof(modeCreate)}: {modeCreate}");
         // Make sure it's the right initial type
         if (!definition.IsEntity())
             return l.Return("", "empty");
@@ -17,13 +17,25 @@ public class WorkAttributeEntityInspectType(): ServiceBase("Eav.AtInTy")
         {
             l.A("is picker");
             var sources = definition
-                .GetPickerDataSources();
-            var list = create
+                .GetPickerDataSources()
+                .ToList();
+            var list = modeCreate
                 ? sources.GetPickerCreateTypeNames()
                 : sources.GetPickerDataTypeNames();
 
             if (list.Any())
-                return l.Return(list.First(), $"returning first: {list.First()}");
+                return l.Return(list.First(), $"first on {nameof(modeCreate)} {modeCreate}: {list.First()}");
+
+            if (tryOtherModes)
+            {
+                l.A("Found nothing, try other mode.");
+                list = !modeCreate
+                    ? sources.GetPickerCreateTypeNames()
+                    : sources.GetPickerDataTypeNames();
+
+                if (list.Any())
+                    return l.Return(list.First(), $"first on {nameof(modeCreate)} {!modeCreate}: {list.First()}");
+            }
         }
         
         // Do basic check for non-pickers
