8034f7d7ed8dbead3fe207d6d76e2d34a0cd71ba
iJungleboy <daniel.mettler@2sic.com>
2026-06-23 16:54:30 +0200

    finish migration of ListController

---
+++ ToSic.Eav.WebApi/WebApi.Sys/Cms/EntityInListDto.cs
@@ -0,0 +1,23 @@
+﻿namespace ToSic.Eav.WebApi.Sys.Cms;
+
+public record EntityInListDto()
+{
+    public int Index { get; init; }
+    public int Id { get; init; }
+    public Guid Guid { get; init; }
+    public string? Title { get; init; }
+    public string? Type { get; init; }
+    // 2026-06-22 2dm - seems unused
+    //public JsonType? TypeWip;
+
+    public EntityInListDto(IEntity? c, int index): this()
+    {
+        Index = index;
+        Id = c?.EntityId ?? 0;
+        Guid = c?.EntityGuid ?? Guid.Empty;
+        Title = c?.GetBestTitle() ?? "";
+        Type = c?.Type.NameId;
+        // 2026-06-22 2dm - seems unused
+        //TypeWip = c?.Type.NameId == null ? null : new JsonType(c)
+    }
+}
