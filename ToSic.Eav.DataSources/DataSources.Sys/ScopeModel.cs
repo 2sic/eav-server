523f228388a9c554942fe5aeda2bfd58c1a16f9f
giovi <npro9021@gmail.com>
2026-07-03 10:26:09 +0200

    Add typed model for System.Scopes datasource

---
+++ ToSic.Eav.DataSources/DataSources.Sys/ScopeModel.cs
@@ -0,0 +1,33 @@
+using ToSic.Eav.Data.Raw;
+using ToSic.Eav.Data.Raw.Sys;
+using ToSic.Eav.Data.Sys;
+using ToSic.Eav.Data.Sys.ContentTypes;
+
+namespace ToSic.Eav.DataSources.Sys;
+
+[ContentTypeSpecs(
+    Name = "Scope",
+    Guid = "f134e3c1-f09f-4fbc-85be-de43a64c6eed",
+    Description = "Data Scope",
+    Scope = "System"
+)]
+public class ScopeModel : RawEntity
+{
+    public required string NameId { get; init; }
+
+    [ContentTypeAttributeSpecs(IsTitle = true)]
+    public required string Name { get; init; }
+    public required int TypesTotal { get; init; }
+    public required int TypesInherited { get; init; }
+    public required int TypesOfApp { get; init; }
+
+    public override IDictionary<string, object?> Attributes(RawConvertOptions options) =>
+        new Dictionary<string, object?>
+        {
+            { AttributeNames.NameIdNiceName, NameId },
+            { nameof(Name), Name },
+            { nameof(TypesTotal), TypesTotal },
+            { nameof(TypesInherited), TypesInherited },
+            { nameof(TypesOfApp), TypesOfApp },
+        };
+}
