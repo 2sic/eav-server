997b1d106c7b7ff3e13ed08964088ebdb89d3838
giovi <npro9021@gmail.com>
2026-08-04 15:19:59 +0200

    Fix rebased data source models for new RawEntity API

--- ToSic.Eav.WebApi/WebApi.Sys/Languages/AppLanguageModel.cs
+++ ToSic.Eav.WebApi/WebApi.Sys/Languages/AppLanguageModel.cs
@@ -1,22 +1,21 @@
-﻿using ToSic.Eav.Data.Raw;
 using ToSic.Eav.Data.Raw.Sys;
-using ToSic.Eav.Data.Sys.ContentTypes;
+using ToSic.Eav.Data.ContentTypes;
 using ToSic.Eav.WebApi.Sys.Dto;
 
 namespace ToSic.Eav.WebApi.Sys.Languages;
 
-[ContentTypeSpecs(
-    Name = "AppLanguage", 
-    Guid = "c8676078-b904-4412-bf4e-aa83d48b63e7", 
-    Description = "Language enabled for an app", 
+[ContentType(
+    Name = "AppLanguage",
+    Guid = "c8676078-b904-4412-bf4e-aa83d48b63e7",
+    Description = "Language enabled for an app",
     Scope = "System"
-    )]
-public class AppLanguageModel(SiteLanguageDto language) : RawEntity
+)]
+public record AppLanguageModel(SiteLanguageDto language) : RawEntity
 {
-    [ContentTypeAttributeSpecs(IsTitle = true)]
+    [ContentTypeField(IsTitle = true)]
     public string Culture => language.Culture;
 
-    public override IDictionary<string, object?> Attributes(RawConvertOptions options) => new Dictionary<string, object?>
+    protected override IDictionary<string, object?> GetValues() => new Dictionary<string, object?>
     {
         { nameof(SiteLanguageDto.Code), language.Code },
         { nameof(SiteLanguageDto.Culture), language.Culture },
