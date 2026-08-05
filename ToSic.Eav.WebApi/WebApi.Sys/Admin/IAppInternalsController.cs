86306c8b3e4c922605e86ab0806b473ac6126131
iJungleboy <daniel.mettler@2sic.com>
2026-08-05 18:09:45 +0200

    Comment out internal code Endpoints for AppInternals replaced by AppEnhancements

--- ToSic.Eav.WebApi/WebApi.Sys/Admin/IAppInternalsController.cs
+++ ToSic.Eav.WebApi/WebApi.Sys/Admin/IAppInternalsController.cs
@@ -1,24 +1,24 @@
-﻿using ToSic.Eav.WebApi.Sys.Dto;
+﻿//using ToSic.Eav.WebApi.Sys.Dto;
 
-namespace ToSic.Eav.WebApi.Sys.Admin;
+//namespace ToSic.Eav.WebApi.Sys.Admin;
 
-public interface IAppInternalsController
-{
-    /// <summary>
-    /// Get all app internals:
-    /// - all content-types in the app
-    /// - app configuration
-    /// - app settings
-    /// - app resources
-    /// - metadata
-    /// ...
-    /// Consolidates 9 related requests in one request.
-    /// </summary>
-    /// <param name="appId"></param>
-    /// <returns></returns>
-    /// <remarks>
-    /// Needs edit-permissions, as the item-list can also be accessed from the toolbar in certain cases.
-    /// Will do permission checks internally.
-    /// </remarks>
-    AppInternalsDto Get(int appId);
-}
+//public interface IAppInternalsController
+//{
+//    /// <summary>
+//    /// Get all app internals:
+//    /// - all content-types in the app
+//    /// - app configuration
+//    /// - app settings
+//    /// - app resources
+//    /// - metadata
+//    /// ...
+//    /// Consolidates 9 related requests in one request.
+//    /// </summary>
+//    /// <param name="appId"></param>
+//    /// <returns></returns>
+//    /// <remarks>
+//    /// Needs edit-permissions, as the item-list can also be accessed from the toolbar in certain cases.
+//    /// Will do permission checks internally.
+//    /// </remarks>
+//    AppInternalsDto Get(int appId);
+//}
--- ToSic.Eav.WebApi/WebApi.Sys/Dto/AppInternalsDto.cs
+++ ToSic.Eav.WebApi/WebApi.Sys/Dto/AppInternalsDto.cs
@@ -1,10 +1,10 @@
-﻿using ToSic.Eav.WebApi.Sys.Admin.Metadata;
+﻿//using ToSic.Eav.WebApi.Sys.Admin.Metadata;
 
-namespace ToSic.Eav.WebApi.Sys.Dto;
+//namespace ToSic.Eav.WebApi.Sys.Dto;
 
-public class AppInternalsDto
-{
-    public required IDictionary<string, IEnumerable<IDictionary<string, object>>?> EntityLists { get; init; }
-    public required IDictionary<string, IEnumerable<ContentTypeFieldDto>?> FieldAll { get; init; }
-    public required MetadataListDto MetadataList { get; init; }
-}
+//public class AppInternalsDto
+//{
+//    public required IDictionary<string, IEnumerable<IDictionary<string, object>>?> EntityLists { get; init; }
+//    public required IDictionary<string, IEnumerable<ContentTypeFieldDto>?> FieldAll { get; init; }
+//    public required MetadataListDto MetadataList { get; init; }
+//}
