6e82006b03188fe4bcaea5735fc3718c7bdfe555
iJungleboy <daniel.mettler@2sic.com>
2026-08-06 12:28:40 +0200

    Minor extract LazyGetAndLog

--- ToSic.Sys.Core/Sys.Data/GetOnce.cs
+++ ToSic.Sys.Core/Sys.Data/GetOnce.cs
@@ -8,7 +8,7 @@ namespace ToSic.Sys.Data;
 /// Simple helper class to use on object properties which should be generated once.
 /// Similar to Lazy(), but the generator function is passed in on the Get() call, not on the constructor.
 /// </summary>
-/// <typeparam name="TResult"></typeparam>
+/// <typeparam name="TValue"></typeparam>
 /// <remarks>
 /// Important for properties which can also return null, because then checking for null won't work to determine if we already tried to retrieve it.
 /// 
@@ -28,16 +28,16 @@ namespace ToSic.Sys.Data;
 /// 4. Properties which have non-nullable types like tuples, which can't self-detect if they are still empty
 /// 5. Methods which behave like properties but as methods (typically to prevent serialization), so they don't have a backing `field`, needing additional code
 /// </remarks>
-[InternalApi_DoNotUse_MayChangeWithoutNotice]
+[PrivateApi]
 [ShowApiWhenReleased(ShowApiMode.Never)]
-public class GetOnce<TResult>
+public class GetOnce<TValue>
 {
     /// <summary>
     /// Get the value. If not yet retrieved, use the generator function (but only once). 
     /// </summary>
     /// <param name="generator">Function which will generate the value on first use.</param>
     /// <returns></returns>
-    public TResult? Get(Func<TResult> generator)
+    public TValue? Get(Func<TValue> generator)
     {
         if (IsValueCreated)
             return Value;
@@ -49,65 +49,16 @@ public class GetOnce<TResult>
         IsValueCreated = true;
         return Value;
     }
-
-    /// <summary>
-    /// Getter with will log its actions when it retrieves the property the first time.
-    /// </summary>
-    /// <param name="log">Log object to use when logging</param>
-    /// <param name="generator">
-    /// Function which will generate the value on first use.
-    /// The function must return the expected value/type.
-    /// </param>
-    /// <param name="timer">enable a timer from call/close</param>
-    /// <param name="enabled">can be set to false if you want to disable logging</param>
-    /// <param name="message"></param>
-    /// <param name="cPath">auto pre-filled by the compiler - the path to the code file</param>
-    /// <param name="cName">auto pre-filled by the compiler - the method name</param>
-    /// <param name="cLine">auto pre-filled by the compiler - the code line</param>
-    /// <param name="parameters"></param>
-    /// <returns></returns>
-    public TResult? Get(ILog log, Func<TResult> generator,
-        bool timer = default,
-        bool enabled = true,
-        string? parameters = default,
-        string? message = default,
-        [CallerFilePath] string? cPath = default,
-        [CallerMemberName] string? cName = default,
-        [CallerLineNumber] int cLine = default
-    )
-    {
-        if (IsValueCreated)
-            return Value;
-        IsValueCreated = true;
-        // ReSharper disable ExplicitCallerInfoArgument
-        return Value = log.Getter(generator, timer: timer, enabled: enabled, parameters: parameters, message: message, cPath: cPath, cName: cName, cLine: cLine);
-        // ReSharper restore ExplicitCallerInfoArgument
-    }
     
-    ///// <summary>
-    ///// Reset the state and value so it will be re-generated next time it's needed.
-    ///// </summary>
-    //public void Reset()
-    //    => IsValueCreated = false;
-
-    ///// <summary>
-    ///// Reset the state and value so it will be re-generated next time it's needed.
-    ///// </summary>
-    //public void Reset(TResult newValue)
-    //{
-    //    Value = newValue;
-    //    IsValueCreated = true;
-    //}
-
     /// <summary>
     /// Determines if value has been created.
     /// The name `IsValueCreated` is the same as in a Lazy() object
     /// </summary>
     public bool IsValueCreated { get; protected set; }
-    protected TResult? Value;
+    protected TValue? Value;
 }
 
-[InternalApi_DoNotUse_MayChangeWithoutNotice]
+[PrivateApi]
 [ShowApiWhenReleased(ShowApiMode.Never)]
 public class LazyGetAndReset<TValue>: GetOnce<TValue>
 {
@@ -125,5 +76,50 @@ public class LazyGetAndReset<TValue>: GetOnce<TValue>
         Value = newValue;
         IsValueCreated = true;
     }
-    
+}
+
+[PrivateApi]
+[ShowApiWhenReleased(ShowApiMode.Never)]
+public class LazyGetAndLog<TValue>
+{
+    /// <summary>
+    /// Getter with will log its actions when it retrieves the property the first time.
+    /// </summary>
+    /// <param name="log">Log object to use when logging</param>
+    /// <param name="generator">
+    /// Function which will generate the value on first use.
+    /// The function must return the expected value/type.
+    /// </param>
+    /// <param name="timer">enable a timer from call/close</param>
+    /// <param name="enabled">can be set to false if you want to disable logging</param>
+    /// <param name="message"></param>
+    /// <param name="cPath">auto pre-filled by the compiler - the path to the code file</param>
+    /// <param name="cName">auto pre-filled by the compiler - the method name</param>
+    /// <param name="cLine">auto pre-filled by the compiler - the code line</param>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
+    public TValue? Get(ILog log, Func<TValue> generator,
+        bool timer = default,
+        bool enabled = true,
+        string? parameters = default,
+        string? message = default,
+        [CallerFilePath] string? cPath = default,
+        [CallerMemberName] string? cName = default,
+        [CallerLineNumber] int cLine = default
+    )
+    {
+        if (IsValueCreated)
+            return Value;
+        IsValueCreated = true;
+        // ReSharper disable ExplicitCallerInfoArgument
+        return Value = log.Getter(generator, timer: timer, enabled: enabled, parameters: parameters, message: message, cPath: cPath, cName: cName, cLine: cLine);
+        // ReSharper restore ExplicitCallerInfoArgument
+    }
+
+    /// <summary>
+    /// Determines if value has been created.
+    /// The name `IsValueCreated` is the same as in a Lazy() object
+    /// </summary>
+    public bool IsValueCreated { get; protected set; }
+    protected TValue? Value;
 }
--- ToSic.Sys.Utils/Utils/ValueType Extensions/NumberExtensions.cs
+++ ToSic.Sys.Utils/Utils/ValueType Extensions/NumberExtensions.cs
@@ -3,16 +3,14 @@
 [ShowApiWhenReleased(ShowApiMode.Never)]
 public static class NumberExtensions
 {
-    public static NumberFormatInfo NumberSeparator =>
-        _numberSeparator.Get(() =>
-        {
-            var nfi = (NumberFormatInfo)CultureInfo.InvariantCulture.NumberFormat.Clone();
-            nfi.NumberGroupSeparator = "`";
-            return nfi;
-        })!;
+    public static readonly NumberFormatInfo NumberSeparator = Create();
 
-    private static readonly GetOnce<NumberFormatInfo> _numberSeparator = new();
-
+    private static NumberFormatInfo Create()
+    {
+        var nfi = (NumberFormatInfo)CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = "`";
+        return nfi;
+    }
 
     [ShowApiWhenReleased(ShowApiMode.Never)]
     public static string ToAposString(this double value) => value.ToString("N2", NumberSeparator);
