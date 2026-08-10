f360410fb363334c09a96e33c9b52fffce099559
iJungleboy <daniel.mettler@2sic.com>
2026-08-10 08:17:03 +0200

    Update ContentTypesFromCodeAttributeHelper.cs
    minor naming

--- ToSic.Eav.Data.Build/Data.Build.Sys/ContentTypes/ContentTypesFromCodeAttributeHelper.cs
+++ ToSic.Eav.Data.Build/Data.Build.Sys/ContentTypes/ContentTypesFromCodeAttributeHelper.cs
@@ -23,31 +23,31 @@ internal class ContentTypesFromCodeAttributeHelper(ContentTypesFromCodeBuilder.D
     /// </summary>
     internal (IList<IContentTypeField> attributes, IDecorator<IContentType>? additionalDecorators) Process(Type type)
     {
-        var (attributes, builtInAttributes) = GenerateAttributes(type);
+        var (fields, systemFields) = ExtractFields(type);
 
-        IDecorator<IContentType>? builtInAttributeDecorator =
-            builtInAttributes == null || builtInAttributes.Count == 0
-                ? null
-                : new ContentTypeBuiltInAttributesDecorator(builtInAttributes
-                    .ToDictionary(
-                        va => va.Name,
-                        va => va
-                    )
-                );
+        // if we have system fields, generate a decorator
+        IDecorator<IContentType>? sysFieldsDecorator = systemFields.SafeNone()
+            ? null
+            : new ContentTypeBuiltInAttributesDecorator(systemFields
+                .ToDictionary(
+                    va => va.Name,
+                    va => va
+                )
+            );
 
-        return (attributes, builtInAttributeDecorator);
+        return (fields, sysFieldsDecorator);
     }
 
 
-    private (IList<IContentTypeField> attributes, IList<IContentTypeField>? builtInAttributes) GenerateAttributes(Type type)
+    private (IList<IContentTypeField> fields, IList<IContentTypeField> systemFields) ExtractFields(Type type)
     {
-        var l = Log.Fn<(IList<IContentTypeField>, IList<IContentTypeField>?)>(timer: true);
+        var l = Log.Fn<(IList<IContentTypeField>, IList<IContentTypeField>)>(timer: true);
 
         // 1. Get all properties of the type; exit early if none
         var properties = type.GetProperties();
 
         if (properties.Length == 0)
-            return l.Return(([], null), "no properties");
+            return l.Return(([], []), "no properties");
 
         // 2. Group by how it should be processed afterward
         var propsGrouped = properties
@@ -55,63 +55,62 @@ internal class ContentTypesFromCodeAttributeHelper(ContentTypesFromCodeBuilder.D
             .ToListOpt();
 
         // 3. Handle normal / general properties
-        var gDefault = propsGrouped
-            .FirstOrDefault(g => g.Key == TempCategory.General)
+        var normalProps = propsGrouped
+            .FirstOrDefault(g => g.Key == TempCategory.Normal)
             ?.ToListOpt();
 
-        var attributes = gDefault == null || !gDefault.Any()
+        var normalFields = normalProps.SafeNone()
             ? []
-            : PropertiesToFields(gDefault, false);
+            : PropertiesToFields(normalProps, keepEvenIfNoAttribute: true);
 
         // 4. Generate list of virtual attributes
-        var gSystem = propsGrouped
+        var systemProps = propsGrouped
             .FirstOrDefault(g => g.Key == TempCategory.System)
             ?.ToListOpt();
-        var vAttributes = gSystem == null || !gSystem.Any()
-            ? null
-            : PropertiesToFields(gSystem, true);
+        var systemFields = systemProps.SafeNone()
+            ? []
+            : PropertiesToFields(systemProps, keepEvenIfNoAttribute: false);
 
         // Return everything
-        return l.Return((attributes, vAttributes), $"real: {attributes.Count}, virtual: {vAttributes?.Count}");
+        return l.Return((normalFields, systemFields), $"real: {normalFields.Count}, virtual: {systemFields.Count}");
 
 
 
 
-        static TempCategory CategorizePropertyInfo(PropertyInfo p) =>
-            p.Name switch
-            {
-                AttributeNames.IdNiceName
-                    or AttributeNames.GuidNiceName
-                    or AttributeNames.CreatedNiceName
-                    or AttributeNames.ModifiedNiceName
-                    => TempCategory.System,
+        static TempCategory CategorizePropertyInfo(PropertyInfo p) => p.Name switch
+        {
+            AttributeNames.IdNiceName
+                or AttributeNames.GuidNiceName
+                or AttributeNames.CreatedNiceName
+                or AttributeNames.ModifiedNiceName
+                => TempCategory.System,
 
-                // Standard built-in properties which are almost certainly never used otherwise
-                nameof(IHasMetadata.Metadata)
-                    or nameof(IRelationshipKeys.RelationshipKeys)
-                    => TempCategory.Ignore,
+            // Standard built-in properties which are almost certainly never used otherwise
+            nameof(IHasMetadata.Metadata)
+                or nameof(IRelationshipKeys.RelationshipKeys)
+                => TempCategory.Ignore,
 
-                // Values property. which could be used otherwise as well, so we'll only skip if it's the 
-                nameof(IRawEntity.Values)
-                    //when typeof(IDictionary<string, object?>).IsAssignableFrom(p.PropertyType)
-                    when typeof(IDictionary<string, object?>) == p.PropertyType
-                    => TempCategory.Ignore,
+            // Values property. which could be used otherwise as well, so we'll only skip if it's the 
+            nameof(IRawEntity.Values)
+                //when typeof(IDictionary<string, object?>).IsAssignableFrom(p.PropertyType)
+                when typeof(IDictionary<string, object?>) == p.PropertyType
+                => TempCategory.Ignore,
 
-                _ => p.GetCustomAttribute<ContentTypeIgnoreAttribute>() != null
-                    ? TempCategory.Ignore
-                    : TempCategory.General,
-            };
+            _ => p.GetCustomAttribute<ContentTypeIgnoreAttribute>() != null
+                ? TempCategory.Ignore
+                : TempCategory.Normal,
+        };
     }
 
     private enum TempCategory
     {
-        General,
+        Normal,
         System,
         Ignore
     }
 
 
-    private IList<IContentTypeField> PropertiesToFields(IList<PropertyInfo> propsFiltered, bool skipNoMetadata)
+    private IList<IContentTypeField> PropertiesToFields(IList<PropertyInfo> propsFiltered, bool keepEvenIfNoAttribute)
     {
         var pairs = propsFiltered
             .Select(p =>
@@ -120,10 +119,10 @@ internal class ContentTypesFromCodeAttributeHelper(ContentTypesFromCodeBuilder.D
                     PropertyInfo = p,
                     Specs = p.GetCustomAttributes<ContentTypeFieldAttribute>().FirstOrDefault(),
                 })
-            .Where(pair => !skipNoMetadata || pair.Specs != null)
+            .Where(pair => pair.Specs != null || keepEvenIfNoAttribute)
             .ToListOpt();
 
-        var attributes = pairs
+        var fields = pairs
             .Select(pair =>
             {
                 var specs = pair.Specs;
@@ -148,7 +147,7 @@ internal class ContentTypesFromCodeAttributeHelper(ContentTypesFromCodeBuilder.D
                 );
             })
             .ToListOpt();
-        return attributes;
+        return fields;
     }
 
 }
