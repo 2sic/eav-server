4968c1e3359ee14283655542970a51660ebecfbd
giovi <npro9021@gmail.com>
2026-08-10 08:40:58 +0200

    use ContentTypeTitle for sys data

--- ToSic.Eav.WebApi/WebApi.Sys/Dto/DataSourceDto.cs
+++ ToSic.Eav.WebApi/WebApi.Sys/Dto/DataSourceDto.cs
@@ -1,10 +1,12 @@
-﻿using ToSic.Eav.DataSource;
+using ToSic.Eav.Data.ContentTypes;
+using ToSic.Eav.Data.Raw;
+using ToSic.Eav.DataSource;
 using ToSic.Eav.DataSource.VisualQuery;
 using ToSic.Eav.DataSource.VisualQuery.Sys;
 
 namespace ToSic.Eav.WebApi.Sys.Dto;
 
-public class DataSourceDto
+public class DataSourceDto : IRawEntityAutoConvert
 {
     public DataSourceDto(DataSourceInfo dsInfo, ICollection<string>? outNameList)
     {
@@ -61,6 +63,7 @@ public class DataSourceDto
     public bool DynamicIn { get; }
     public string? HelpLink { get; }
     public bool EnableConfig { get; }
+    [ContentTypeTitle]
     public string Name { get; }
     public string? UiHint { get; }
 
