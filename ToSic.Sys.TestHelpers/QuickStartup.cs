44d5055f493ca548d7ba0691f9e7412d3920c3e2
iJungleboy <daniel.mettler@2sic.com>
2026-08-15 16:19:31 +0200

    Create QuickStartup.cs

---
+++ ToSic.Sys.TestHelpers/QuickStartup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ToSic.Sys;
+
+/// <summary>
+/// Quick helper to create a startup class with a single action to configure services.
+/// This is mostly used for startup classes underneath a test class.
+/// </summary>
+/// <param name="configureServices">The action to configure services.</param>
+public abstract class QuickStartup(Action<IServiceCollection> configureServices)
+{
+    public virtual void ConfigureServices(IServiceCollection services) =>
+        configureServices(services);
+
+}
