9788109bdd2bba2b53f35297ccd38709662224d6
iJungleboy <daniel.mettler@2sic.com>
2026-08-17 17:10:53 +0200

    DI Override - reorg / rename

--- ToSic.Sys.DI.Tests/Mock.LifetimeServices/Setup.cs
+++ ToSic.Sys.DI.Tests/Mock.LifetimeServices/Setup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using ToSic.Sys.DI;
-using ToSic.Sys.DI.ChildScope;
 
 namespace ToSic.Mock.LifetimeServices;
 
@@ -9,8 +8,10 @@ internal static class Setup
 {
     public static IServiceCollection AddMockLifetimes(this IServiceCollection services)
     {
+        
         services.TryAddTransient<MockTransientStandalone>();
-        services.TryAddTransient(OverrideContext<IMockTransientStandalone>.Register<MockTransientStandalone>());
+        services.TryAddTransient(OverrideService<IMockTransientStandalone>.Register<MockTransientStandalone>());
+        
         services.TryAddTransient<MockTransientRequiringTransient>();
         services.TryAddTransient<MockTransientRequiringScoped>();
 
@@ -23,15 +24,16 @@ internal static class Setup
         // Note: any scoped which we would ever want to override
         // must be registered as transient, and just created once.
         // so we could have the implementation as scoped
-        //services.TryAddScoped(OverrideContext<IMockScopedToReRegisterReqITransient>.Register<MockScopedToReRegisterReqITransient>());
+        //services.TryAddScoped(OverrideService<IMockScopedToReRegisterReqITransient>.Register<MockScopedToReRegisterReqITransient>());
         services.TryAddTransient<MockScopedToReRegisterReqITransient>();
-        services.TryAddTransient(
-            OverrideContext<IMockScopedToReRegisterReqITransient>.RegisterScoped<MockScopedToReRegisterReqITransient>());
+        services.TryAddTransient(OverrideService<IMockScopedToReRegisterReqITransient>.RegisterScoped<MockScopedToReRegisterReqITransient>());
         
         services.TryAddTransient<MockChildScopeOnlyTransientBasic>();
         
         return services;
     }
+    
+    
 
     public static IServiceCollection AddMockPreRegisterChildInstances(this IServiceCollection services)
     {
@@ -40,14 +42,4 @@ internal static class Setup
         services.TryAddTransient<MockChildScopeOnlyTransientPreRegistered>();
         return services;
     }
-    
-    public static IServiceCollection AddMockInitialsWhichThrow(this IServiceCollection services)
-    {
-        // Register the MockChildScopeOnlyTransientPreRegistered as a transient service
-        // we'll later access it through the interface, but
-        services.TryAddTransient(OverrideContext<IMockChildScopeOnlyScopedBasic>.Register(() => new MockChildScopeOnlyScopedBasicInitialThrows()));
-        services.TryAddTransient<MockChildScopeOnlyScopedBasic>();  // make sure the underlying real implementation can be resolved
-        return services;
-    }
-    
 }
---
+++ ToSic.Sys.DI.Tests/Sys.DI.Override/ChildScopeSetup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToSic.Mock.LifetimeServices;
+
+namespace ToSic.Sys.DI.Override;
+
+public class ChildScopeSetup
+{
+    internal static IServiceProvider BuildSps()
+    {
+        var parentSp = new ServiceCollection()
+            .AddMockLifetimes()
+            .AddMockPreRegisterChildInstances()
+            .BuildServiceProvider();
+        return parentSp;
+    }
+
+    internal static T? GetService<T>() where T : class => BuildSps().GetService<T>();
+}
