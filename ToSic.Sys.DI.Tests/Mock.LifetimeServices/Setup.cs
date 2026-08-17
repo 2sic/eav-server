ffcce24cf6305dbf7251eb04cd09eff1b9f6d376
iJungleboy <daniel.mettler@2sic.com>
2026-08-17 15:35:39 +0200

    improve override registration

--- ToSic.Sys.DI.Tests/Mock.LifetimeServices/Setup.cs
+++ ToSic.Sys.DI.Tests/Mock.LifetimeServices/Setup.cs
@@ -9,7 +9,7 @@ internal static class Setup
     public static IServiceCollection AddMockLifetimes(this IServiceCollection services)
     {
         services.TryAddTransient<MockTransientStandalone>();
-        services.TryAddOverrideableTransient<IMockTransientStandalone, MockTransientStandalone>();
+        services.TryAddTransient(OverrideContext<IMockTransientStandalone>.Register<MockTransientStandalone>());
         services.TryAddTransient<MockTransientRequiringTransient>();
         services.TryAddTransient<MockTransientRequiringScoped>();
 
@@ -33,41 +33,55 @@ internal static class Setup
 
 public static class ServiceCollectionExtensions
 {
-    //public static IServiceCollection TryAddOverrideableTransient<TService>(this IServiceCollection services)
-    //    where TService : class
-    //{
-    //    services.TryAddTransient<TService>(sp
-    //        // 1. Check if an override is active in the current async context
-    //        => OverrideContext<TService>.Current
-    //           // 2. Fall back to the default implementation in the SAME scope
-    //           ?? sp.GetRequiredService<TService>());
-    //    return services;
-    //}
     
-    //public static IServiceCollection TryAddOverrideableTransient<TService, TImplementation>(this IServiceCollection services)
-    //    where TService : class
-    //    where TImplementation : class, TService
-    //{
-    //    services.TryAddTransient<TService>(sp
-    //        // 1. Check if an override is active in the current async context
-    //        => OverrideContext<TService>.Current
-    //           // 2. Fall back to the default implementation in the SAME scope
-    //           ?? sp.GetRequiredService<TImplementation>());
-    //    return services;
-    //}
+    public static IServiceCollection AddOverrideableScoped<TService>(this IServiceCollection services)
+        where TService : class
+        => services.AddScoped(OverrideContext<TService>.Register<TService>());
+
+    public static IServiceCollection AddOverrideableScoped<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+        => services.AddScoped(OverrideContext<TService>.Register<TImplementation>());
     
+    public static IServiceCollection AddOverrideableTransient<TService>(this IServiceCollection services)
+        where TService : class
+        => services.AddTransient(OverrideContext<TService>.Register<TService>());
+
+    public static IServiceCollection AddOverrideableTransient<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+        => services.AddTransient(OverrideContext<TService>.Register<TImplementation>());
+
+    public static IServiceCollection TryAddOverrideableTransient<TService>(this IServiceCollection services)
+        where TService : class
+    {
+        services.TryAddTransient(OverrideContext<TService>.Register<TService>());
+        return services;
+    }
     
     public static IServiceCollection TryAddOverrideableTransient<TService, TImplementation>(this IServiceCollection services)
         where TService : class
         where TImplementation : class, TService
     {
-        services.TryAddTransient<TService>(sp
-            // 1. Check if an override is active in the current async context
-            => OverrideContext<TService>.CurrentFactory is { } overrideFactory
-                ? overrideFactory(sp)
-                // 2. Fall back to the default implementation in the SAME scope
-                : sp.GetRequiredService<TImplementation>()
-        );
+        services.TryAddTransient(OverrideContext<TService>.Register<TImplementation>());
         return services;
     }
+
+    ///// <summary>
+    ///// Generate the factory function to either get from the override, or from the underlying service provider.
+    ///// </summary>
+    ///// <typeparam name="TService">Service type or interface</typeparam>
+    ///// <typeparam name="TImplementation">Implementation type</typeparam>
+    ///// <returns>A factory function to create the service instance</returns>
+    //private static Func<IServiceProvider, TService> Overrideable<TService, TImplementation>()
+    //    where TService : class
+    //    where TImplementation : class, TService
+    //{
+    //    // Check if an override is active in the current async context
+    //    return sp => OverrideContext<TService>.CurrentFactory is { } overrideFactory
+    //        // 1. Return from new factory
+    //        ? overrideFactory(sp)
+    //        // 2. Fall back to the default implementation in the SAME scope
+    //        : sp.GetRequiredService<TImplementation>();
+    //}
 }
