225d7ea3688f255ca73eef7c05b15660fbb7c555
giovi <npro9021@gmail.com>
2026-09-07 13:44:02 +0200

    Migrate MetadataTargetTypes to auto-converting raw records

---
+++ ToSic.Eav.DataSources/DataSources.Sys/MetadataTargetTypeRaw.cs
@@ -0,0 +1,15 @@
+﻿using ToSic.Eav.Data.ContentTypes;
+using ToSic.Eav.Data.Raw;
+
+namespace ToSic.Eav.DataSources.Sys;
+
+[ContentType(
+    Guid = "52c96a68-c791-439f-a154-7c46c1bfa0d9",
+    Description = "Metadata target type",
+    Name = "MetadataTargetTypes"
+)]
+internal sealed record MetadataTargetTypeRaw(
+    int Id,
+    [property: ContentTypeTitle] string Title,
+    string NameId
+) : IRawEntityAutoConvert;
