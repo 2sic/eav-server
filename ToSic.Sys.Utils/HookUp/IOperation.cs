018cb4d82c505d27bbfabcd8df8b8d29082fe385
iJungleboy <daniel.mettler@2sic.com>
2026-09-25 17:23:17 +0200

    Create IOperation.cs

---
+++ ToSic.Sys.Utils/HookUp/IOperation.cs
@@ -0,0 +1,13 @@
+﻿namespace ToSic.Sys.HookUp;
+
+[ShowApiWhenReleased(ShowApiMode.Never)]
+public interface IOperation;
+
+[ShowApiWhenReleased(ShowApiMode.Never)]
+public interface IOperation<in TIn, out TOut> : IOperation
+{
+    TOut Process(TIn input);
+}
+
+[ShowApiWhenReleased(ShowApiMode.Never)]
+public interface IOperation<T>: IOperation<T, T>;
